0182c52de280117ee26b680457a0e49f4b567b0a
Dirk Groeneveld <dirkg@allenai.org>
2023-03-08 09:33:45 +0200

    Inching the lumi script closer to working

--- lumi/slurm-batch-example.sh
+++ lumi/slurm-batch-example.sh
@@ -17,8 +17,6 @@ module load LUMI/22.08 partition/G
 module load singularity-bindings
 module load aws-ofi-rccl
 
-module load MyApp/1.2.3
-
 CPU_BIND="mask_cpu:7e000000000000,7e00000000000000"
 CPU_BIND="${CPU_BIND},7e0000,7e000000"
 CPU_BIND="${CPU_BIND},7e,7e00"
@@ -26,6 +24,18 @@ CPU_BIND="${CPU_BIND},7e00000000,7e0000000000"
 
 export OMP_NUM_THREADS=2
 export MPICH_GPU_SUPPORT_ENABLED=1
+export NCCL_SOCKET_IFNAME=hsn
+export NCCL_NET_GDR_LEVEL=3
+export MIOPEN_USER_DB_PATH=/tmp/${USER}-miopen-cache-${SLURM_JOB_ID}
+export MIOPEN_CUSTOM_CACHE_DIR=${MIOPEN_USER_DB_PATH}
+export CXI_FORK_SAFE=1
+export CXI_FORK_SAFE_HP=1
+export FI_CXI_DISABLE_CQ_HUGETLB=1
+export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:${EBROOTAWSMINOFIMINRCCL}/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:${SINGULARITYENV_LD_LIBRARY_PATH}
+export NCCL_DEBUG=INFO
 
-srun --cpu-bind=${CPU_BIND} --distribution=block:block ./run_with_slurm_device.sh <executable> <args>
+srun \
+  --cpu-bind=${CPU_BIND} \
+  --distribution=block:block \
+  ./run_with_slurm_device.sh singularity exec -B"$SCRATCH:$SCRATCH" /project/project_462000229/containers/llm-lumi_latest.sif python <args>
 
