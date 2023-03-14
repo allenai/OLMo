d5f32da6cff6508a5d32ff0ed73196c24d1444b0
Dirk Groeneveld <dirkg@allenai.org>
2023-03-15 01:55:19 +0200

    Run on a whole node, with wandb

--- scripts/c4-on-lumi.sh
+++ scripts/c4-on-lumi.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=c4-1.2b
 #SBATCH --account=project_462000229
-#SBATCH --output=/users/dgroeneveld/logs/%j.log
+#SBATCH --output=/pfs/lustref1/flash/project_462000229/logs/%j.log
 #SBATCH --nodes=1               # Total number of nodes 
-#SBATCH --ntasks-per-node=2
-#SBATCH --gpus-per-node=2       # Allocate one gpu per MPI rank
-#SBATCH --cpus-per-task=2       # 2 threads per ranks
+#SBATCH --ntasks-per-node=8
+#SBATCH --gpus-per-node=8       # Allocate one gpu per MPI rank
+#SBATCH --cpus-per-task=6       # 6 threads per ranks
 #SBATCH --time=00:15:00
-#SBATCH --mail-type=all         # Send email at begin and end of job
-#SBATCH --mail-user=dirkg@allenai.org
-#SBATCH --mem=64G
+#SBATCH --mem=0			# All memory on the node
 #SBATCH --partition=small-g
 
 module load LUMI/22.08 partition/G
@@ -19,7 +17,7 @@ CPU_BIND="${CPU_BIND},7e0000,7e000000"
 CPU_BIND="${CPU_BIND},7e,7e00"
 CPU_BIND="${CPU_BIND},7e00000000,7e0000000000"
 
-export OMP_NUM_THREADS=2
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export MPICH_GPU_SUPPORT_ENABLED=1
 export NCCL_SOCKET_IFNAME=hsn
 export NCCL_NET_GDR_LEVEL=3
@@ -30,16 +28,17 @@ export CXI_FORK_SAFE_HP=1
 export FI_CXI_DISABLE_CQ_HUGETLB=1
 export NCCL_DEBUG=INFO
 export PYTHONPATH=.:${PYTHONPATH}
+export WANDB_PROJECT=lumi-${SLURM_JOB_PARTITION}
 
 srun \
   --cpu-bind=${CPU_BIND} \
   --distribution=block:block \
   --kill-on-bad-exit \
-  singularity exec \
+  scripts/run_with_environment.sh \
+    singularity exec \
     -B"$PROJECT_DIR:$PROJECT_DIR" \
     -B"$SCRATCH_DIR:$SCRATCH_DIR" \
     -B"$FLASH_DIR:$FLASH_DIR" \
     $PROJECT_DIR/containers/llm-lumi_latest.sif \
-    scripts/run_with_environment.sh \
-    python scripts/train.py configs/1.2b-c4-lumi.yaml
+    python scripts/train.py configs/1.2b-c4.yaml --run_name=${SLURM_JOB_ID}
 
