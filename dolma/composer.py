857fcf553d58f4b4dbadbd33bce7c9aa65bb14f0
epwalsh <petew@allenai.org>
2023-03-28 13:50:21 -0700

    avoid computing loss twice during training

--- dolma/composer.py
+++ dolma/composer.py
@@ -45,16 +45,10 @@ class ComposerDolmaGPT(ComposerModel):
         self.config = self.model.config
         self.num_fwd_flops = self.model.num_fwd_flops
 
-        from composer.metrics.nlp import LanguageCrossEntropy, Perplexity
+        from composer.metrics.nlp import Perplexity
 
-        self.train_metrics = {
-            "LanguageCrossEntropy": LanguageCrossEntropy(self.config.vocab_size),
-            "Perplexity": Perplexity(),
-        }
-        self.eval_metrics = {
-            "LanguageCrossEntropy": LanguageCrossEntropy(self.config.vocab_size),
-            "Perplexity": Perplexity(),
-        }
+        self.train_metrics: Dict[str, Metric] = {}
+        self.eval_metrics: Dict[str, Metric] = {"Perplexity": Perplexity()}
 
     def get_labels(self, batch: BatchDict) -> torch.Tensor:
         # Labels are just input IDs shifted to the left (first item is ignored).
