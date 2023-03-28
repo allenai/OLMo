fe1061b35f960698e380887020a37514833a0185
epwalsh <petew@allenai.org>
2023-03-28 14:13:28 -0700

    fix use of embedding seize

--- dolma/model.py
+++ dolma/model.py
@@ -269,7 +269,9 @@ class DolmaGPT(nn.Module):
 
         self.transformer = nn.ModuleDict(
             dict(
-                wte=nn.Embedding(config.vocab_size, config.d_model, device=config.init_device),
+                wte=nn.Embedding(
+                    config.embedding_size or config.vocab_size, config.d_model, device=config.init_device
+                ),
                 emb_drop=nn.Dropout(config.embedding_dropout),
                 blocks=nn.ModuleList([GPTBlock(config) for _ in range(config.n_layers)]),
                 ln_f=LPLayerNorm(config),
