e97f2958cd854dd51d02a659a70c4dc2b7644e7b
kyleclo <kyleclo@uw.edu>
2023-03-29 09:36:32 -0700

    black

--- pretrain_data/api.py
+++ pretrain_data/api.py
@@ -19,31 +19,32 @@ from smashed.utils.io_utils import recursively_list_files, MultiPath, open_file_
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(message)s',
+    format="%(asctime)s %(levelname)s %(message)s",
     handlers=[
         logging.StreamHandler(sys.stdout),
-        logging.FileHandler('_pretrain_data_api.log'),
-    ])
+        logging.FileHandler("_pretrain_data_api.log"),
+    ],
+)
 
 SOURCE_TO_AVAILABLE_VERSIONS = {
-    'common-crawl': ['v0'],
-    'reddit': [],
-    's2': ['v2_hard_dedup'],
-    'stack-dedup': ['raw'],
-    'wikipedia': []
+    "common-crawl": ["v0"],
+    "reddit": [],
+    "s2": ["v2_hard_dedup"],
+    "stack-dedup": ["raw"],
+    "wikipedia": [],
 }
 
 SOURCE_TO_PATH = {
-    'common-crawl': f"s3://ai2-llm/pretraining-data/sources/common-crawl/{SOURCE_TO_AVAILABLE_VERSIONS['common-crawl']}/documents/mined_split/*/*/*.json.gz",
-    'reddit': None,
-    's2': f"s3://ai2-llm/pretraining-data/sources/s2/{SOURCE_TO_AVAILABLE_VERSIONS['s2']}/*/*/*.gz",
-    'stack-dedup': f"s3://ai2-llm/pretraining-data/sources/stack-dedup/{SOURCE_TO_AVAILABLE_VERSIONS['stack-dedup']}/*/*.jsonl.gz",
-    'wikipedia': None
+    "common-crawl": f"s3://ai2-llm/pretraining-data/sources/common-crawl/{SOURCE_TO_AVAILABLE_VERSIONS['common-crawl']}/documents/mined_split/*/*/*.json.gz",
+    "reddit": None,
+    "s2": f"s3://ai2-llm/pretraining-data/sources/s2/{SOURCE_TO_AVAILABLE_VERSIONS['s2']}/*/*/*.gz",
+    "stack-dedup": f"s3://ai2-llm/pretraining-data/sources/stack-dedup/{SOURCE_TO_AVAILABLE_VERSIONS['stack-dedup']}/*/*.jsonl.gz",
+    "wikipedia": None,
 }
 
 
 class Example:
-    REQUIRED_FIELDS = ['source', 'id', 'text', 'added', 'created', 'metadata']
+    REQUIRED_FIELDS = ["source", "id", "text", "added", "created", "metadata"]
 
     def __init__(self, source: str, id: str, text: str, added: str, created: str, metadata: Dict):
         self.source = source
@@ -52,7 +53,7 @@ class Example:
         self.added = added
         self.created = created
         self.metadata = metadata
-        self._global_id = f'{self.source}::{self.id}'
+        self._global_id = f"{self.source}::{self.id}"
         self._s3_filepath: Optional[str] = None
         self._s3_fileline: Optional[int] = None
 
@@ -77,14 +78,14 @@ class Example:
         self._s3_fileline = s3_fileline
 
     @classmethod
-    def from_json(cls, example_json: Dict) -> 'Example':
+    def from_json(cls, example_json: Dict) -> "Example":
         example = Example(
-            source=example_json['source'],
-            id=example_json['id'],
-            text=example_json['text'],
-            added=example_json['added'],
-            created=example_json['created'],
-            metadata=example_json['metadata']
+            source=example_json["source"],
+            id=example_json["id"],
+            text=example_json["text"],
+            added=example_json["added"],
+            created=example_json["created"],
+            metadata=example_json["metadata"],
         )
         extra_keys = [key for key in example_json.keys() if key not in cls.REQUIRED_FIELDS]
         if extra_keys:
@@ -93,29 +94,29 @@ class Example:
 
     def to_json(self) -> Dict:
         return {
-            'source': self.source,
-            'id': self.id,
-            'text': self.text,
-            'added': self.added,
-            'created': self.created,
-            'metadata': self.metadata
+            "source": self.source,
+            "id": self.id,
+            "text": self.text,
+            "added": self.added,
+            "created": self.created,
+            "metadata": self.metadata,
         }
 
 
 class Dataset:
     def __init__(self, source: str, version: str, attributes: List[str]):
-        logging.info(f'Creating Dataset from S3: source={source} version={version}')
+        logging.info(f"Creating Dataset from S3: source={source} version={version}")
         if source not in SOURCE_TO_PATH:
-            raise FileNotFoundError(f'{source} not one of the available sources')
+            raise FileNotFoundError(f"{source} not one of the available sources")
         if version not in SOURCE_TO_AVAILABLE_VERSIONS[source]:
-            raise FileNotFoundError(f'{version} does not exist for {source}')
+            raise FileNotFoundError(f"{version} does not exist for {source}")
 
         self.source = source
         self.version = version
         self.attributes = attributes
 
         path_str = SOURCE_TO_PATH[source]
-        i = path_str.index('*')
+        i = path_str.index("*")
         dir_path = path_str[:i]
         self.s3_dirpath = dir_path
 
@@ -159,21 +160,21 @@ class Dataset:
             seen.add(example.global_id)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--source", type=str, required=True)
     parser.add_argument("--version", type=str, required=True)
-    parser.add_argument("--attributes", nargs='*', required=False)
+    parser.add_argument("--attributes", nargs="*", required=False)
     args = parser.parse_args()
 
     dataset = Dataset(source=args.source, version=args.version, attributes=args.attributes)
-    logging.info(f'Found one dataset from source={args.source} version={args.version}')
+    logging.info(f"Found one dataset from source={args.source} version={args.version}")
 
     first_s3_filepath = next(dataset.s3_filepaths, None)
     if first_s3_filepath:
-        logging.info(f'Inspecting first file at {first_s3_filepath}')
+        logging.info(f"Inspecting first file at {first_s3_filepath}")
 
         dataset.verify_one_file(s3_filepath=first_s3_filepath)
-        logging.info(f'Finished verifying format of file {first_s3_filepath}')
+        logging.info(f"Finished verifying format of file {first_s3_filepath}")
     else:
-        raise FileNotFoundError(f'No files found for source={args.source} version={args.version}')
+        raise FileNotFoundError(f"No files found for source={args.source} version={args.version}")
