bc9339a399800276fd7592d5df67bb554df61967
Luca Soldaini <lucas@allenai.org>
2023-04-01 14:02:02 -0700

    setting up download for doab/gutemberg

---
+++ pretrain_data/books/doab.py
@@ -0,0 +1,189 @@
+import json
+import tempfile
+from urllib.parse import urljoin
+from pathlib import Path
+import shutil
+from typing import Optional, Tuple, List
+
+from fake_useragent import UserAgent
+import pandas as pd
+from cached_path import cached_path
+import springs as sp
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+import tqdm
+
+UA = UserAgent()
+
+
+def is_pdf_link(tag: Tag) -> bool:
+    # Check if the tag is a link and has 'href' attribute
+    if not (tag.name == 'a' and 'href' in tag.attrs):
+        return False
+
+    href = str(tag['href']).lower()
+    if href.startswith('#'):
+        return False
+
+    # Check if the URL is for a PDF file
+    if href.endswith('.pdf') or 'pdf' in href.split('?')[-1].split('&')[-1]:
+        return True
+
+    # Check if the link text is PDF or contains PDF
+    if 'pdf' in tag.get_text(strip=True, separator=' ').lower():
+        return True
+
+    # Check if the link has a type attribute with a value of “application/pdf”:
+    if 'type' in tag.attrs and str(tag['type']).lower() == "application/pdf":
+        return True
+
+    # Check if the link has a parent element with a class or an ID that
+    # suggests it is a PDF link. For example, sometimes websites may wrap the
+    # PDF link in a <div> or other elements with a class or ID like
+    # “pdf-download”, “pdf-viewer”, etc.:
+    c = tag.parent.get('class', []) if tag.parent else []
+    parent_classes = [c] if isinstance(c, str) else ([] if not c else c)
+    parent_id = tag.parent.get('id', '') if tag.parent else ''
+    parent_attr = [*parent_classes, parent_id]
+    if any(('pdf' in attr.lower()) for attr in parent_attr):
+        return True
+
+    return False
+
+
+@sp.dataclass
+class Config:
+    metadata: str = sp.field(
+        default=(
+            's3://ai2-llm/pretraining-data/sources/books/raw/'
+            'doabooks_repository-export_20230331.csv'
+        ),
+        help='URL or path to metadata file.'
+    )
+    destination: str = sp.field(
+        default=sp.MISSING,
+        help='Path to output directory.'
+    )
+    debug: Optional[str] = sp.field(
+        default=None,
+        help='Provide url to download debug for.'
+    )
+
+
+def process_url(
+    url: str,
+    id_: str,
+    base_path: Path,
+    _depth: int = 1
+) -> Tuple[str, bool, List[str]]:
+    if _depth < 0:
+        return 'depth exceeded', False, []
+
+    base_path.mkdir(parents=True, exist_ok=True)
+
+    if '||' in url:
+        success = False
+        urls = set(u.strip() for u in url.split('||') if u.strip())
+        sub_base_path = base_path / id_
+        for i, sub_url in enumerate(sorted(urls)):
+            _, sub_success, sub_urls = process_url(
+                url=sub_url,
+                id_=str(i),
+                base_path=sub_base_path,
+                _depth=_depth     # do not decrement depth
+            )
+            success = success or sub_success
+            urls.update(sub_urls)
+
+        if not success and sub_base_path.exists():
+            # no pdfs found, remove directory
+            shutil.rmtree(sub_base_path)
+
+        return 'url list', success, sorted(urls)
+
+    try:
+        response = requests.get(url, headers={'User-Agent': UA.random})
+    except Exception:
+        return 'request error', False, []
+
+    if response.status_code >= 400:
+        return f'status code {response.status_code}', False, []
+
+    if response.headers["content-type"].startswith("application/pdf"):
+        with open(base_path / f'{id_}.pdf', 'wb') as f:
+            f.write(response.content)
+        return 'pdf', True, [url]
+
+    if response.headers["content-type"].startswith("text/html"):
+        soup = BeautifulSoup(response.content, 'html.parser')
+        pdf_links = set(
+            url for a in soup.find_all(is_pdf_link) if (url := a.get('href'))
+        )
+        sub_base_path = base_path / id_
+        success = False
+        for i, pdf_link in enumerate(sorted(pdf_links)):
+            if pdf_link.startswith('/'):
+                # combine urls
+                pdf_link = urljoin(url, pdf_link)
+
+            _, sub_success, sub_urls = process_url(
+                url=pdf_link,
+                id_=str(i),
+                base_path=sub_base_path,
+                _depth=_depth - 1
+            )
+            success = success or sub_success
+            pdf_links.update(sub_urls)
+
+        if not success and sub_base_path.exists():
+            # no pdfs found, remove directory
+            shutil.rmtree(sub_base_path)
+
+        return 'pdf list', success, sorted(pdf_links)
+
+    return 'unknown', False, []
+
+
+@sp.cli(Config)
+def main(config: Config):
+    if config.debug:
+        url = config.debug
+        base_path = Path(tempfile.gettempdir())
+        print(process_url(url, 'debug', base_path))
+        return
+
+    df = pd.read_csv(cached_path(config.metadata))
+    data_path = Path(config.destination) / 'data'
+    meta_path = Path(config.destination) / 'metadata'
+    data_path.mkdir(parents=True, exist_ok=True)
+    meta_path.mkdir(parents=True, exist_ok=True)
+
+    it = tqdm.tqdm(df.iterrows(), total=len(df), desc='Downloading Books')
+
+    for _, elem in it:
+        elem = elem.to_dict()
+        id_ = elem.pop('id', None)
+        base_url = elem.pop('BITSTREAM Download URL', None)
+        content_type = "unknown"
+        success = False
+
+        properties = {k: v for k, v in elem.items() if not pd.isna(v) and v}
+        metadata = {'properties': properties, 'extra': {}, 'id': id_, 'url': base_url}
+
+        if base_url and id_:
+            content_type, success, urls = process_url(
+                url=base_url,
+                id_=id_,
+                base_path=data_path
+            )
+            metadata['extra']['urls'] = urls
+
+        metadata.update({'type': content_type, 'success': success})
+
+        with open(meta_path / f'{id_}.json', 'w') as f:
+            json.dump(metadata, f)
+
+
+if __name__ == '__main__':
+    main()
