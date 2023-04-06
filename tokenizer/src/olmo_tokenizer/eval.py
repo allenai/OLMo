e9d6c62384590d8af9d762ea8ed25c2066e40785
Luca Soldaini <lucas@allenai.org>
2023-04-06 15:13:01 -0700

    tokenizer eval code

---
+++ tokenizer/src/olmo_tokenizer/eval.py
@@ -0,0 +1,50 @@
+from collections import Counter
+import itertools
+from cached_path import cached_path
+from transformers import PreTrainedTokenizerFast, AutoTokenizer
+from .llama_tokenizer import LlamaTokenizer
+from datasets import load_dataset
+import numpy as np
+
+NAME = 'v1'  # Choose between v1, v1_small, or v1_tiny
+BASE_PATH = 's3://ai2-llm/tokenizer/model'
+LLAMA_PATH = 's3://ai2-s2-lucas/llama/tokenizer.model'
+
+
+tokenizers = {
+    'gpt': AutoTokenizer.from_pretrained('gpt2'),
+    'our': PreTrainedTokenizerFast(tokenizer_file=str(cached_path(f'{BASE_PATH}/{NAME}.json'))),
+    'llama': LlamaTokenizer(vocab_file=str(cached_path(LLAMA_PATH)))
+}
+
+
+copa = load_dataset('super_glue', 'copa', split='test')
+lambada = load_dataset("EleutherAI/lambada_openai", 'default', split='test')
+
+def join_copa(row):
+    return {'text': f'{row["premise"]} {row["choice1"]} {row["choice2"]} {row["label"]}'}
+
+copa = copa.map(join_copa, remove_columns=copa.column_names)
+
+datasets = {
+    'copa': copa,
+    'lambada_openai': lambada
+}
+
+
+for dn, dataset in datasets.items():
+    for tn, tokenizer in tokenizers.items():
+        print(f'{dn}/{tn}')
+
+        tokens = tokenizer(dataset['text'], add_special_tokens=False).input_ids
+        lengths = [len(r) for r in tokens]
+        counts = Counter(itertools.chain.from_iterable(tokens))
+        avg = float(np.mean(lengths))
+        std = float(np.std(lengths))
+
+        print(f'avg: {avg:.2f} std: {std:.2f}')
+        print('Most common tokens:')
+        for k, v in counts.most_common(10):
+            print(f'{repr(tokenizer.decode([k]))}: {v / sum(lengths):.2%}')
+
+        print('\n')
