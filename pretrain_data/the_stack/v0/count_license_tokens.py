dc9c35d6ed13b9ed1b4f304ee92f46271191011f
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-04-10 19:48:34 +0000

    license info

--- pretrain_data/the_stack/v0/count_license_tokens.py
+++ pretrain_data/the_stack/v0/count_license_tokens.py
@@ -16,12 +16,24 @@ def count_license_tokens(token_lengths, licenses):
     return result
 
 
+def count_license_documents(licenses):
+    result = {}
+    for i in range(len(licenses)):
+        for li in licenses[i]:
+            if li in result:
+                result[li] += 1
+            else:
+                result[li] = 1
+    return result
+
+
 def run(path: str):
     converters = {"document_licenses": literal_eval, "document_token_length": literal_eval}
     df = pd.read_csv(path, sep="\t", converters=converters)
     df["license_tokens"] = [
         df.apply(lambda x: count_license_tokens(x.document_token_length, x.document_licenses), axis=1).values[0]
     ]
+    df["license_counts"] = [df.apply(lambda x: count_license_documents(x.document_licenses), axis=1).values[0]]
     df.to_csv(path, sep="\t", index=False)
 
 
