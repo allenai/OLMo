8ac6bcaa3e791ea1dfd07bd800128568730b5f18
epwalsh <petew@allenai.org>
2023-04-25 11:00:00 -0700

    note about PYTORCH_HIP_ALLOC_CONF

--- scripts/c4-medium-on-lumi.sh
+++ scripts/c4-medium-on-lumi.sh
@@ -26,6 +26,8 @@ export PYTHONPATH=.:${PYTHONPATH}
 export WANDB_PROJECT=lumi-${SLURM_JOB_PARTITION}
 export ROCM_PATH=/opt/rocm
 export SINGULARITYENV_LD_LIBRARY_PATH=/usr/local/lib:/opt/cray/libfabric/1.15.2.0/lib64
+# Try playing with max_split_size_mb if you run into OOM errors.
+#export 'PYTORCH_HIP_ALLOC_CONF=max_split_size_mb:512'
 
 srun \
   --cpus-per-task=$SLURM_CPUS_PER_TASK \
