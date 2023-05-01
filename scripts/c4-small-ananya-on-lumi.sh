3dca997584bbf02742de551d1b8dab50b8152223
epwalsh <petew@allenai.org>
2023-05-01 15:25:27 -0700

    tweak

--- scripts/c4-small-ananya-on-lumi.sh
+++ scripts/c4-small-ananya-on-lumi.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=c4-small
 #SBATCH --account=project_462000229
 #SBATCH --output=/pfs/lustref1/flash/project_462000229/logs/%j.log
-#SBATCH --nodes=8               # Total number of nodes 
+#SBATCH --nodes=16              # Total number of nodes 
 #SBATCH --ntasks-per-node=8
 #SBATCH --gpus-per-node=8       # Allocate one gpu per MPI rank
 #SBATCH --cpus-per-task=6
