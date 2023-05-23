41019aa91aaac69bf1b580835a5ab50f77d1dbe0
epwalsh <petew@allenai.org>
2023-05-23 11:54:17 -0700

    generalize for LUMI

--- scripts/prepare_ppl_eval_data.sh
+++ scripts/prepare_ppl_eval_data.sh
@@ -3,9 +3,14 @@
 set -eo pipefail
 
 data_path=eval-data/perplexity/v2_small
-output_dir=/net/nfs/allennlp/llm-data/${data_path}
 eval_set=val
 tokenizer=gpt2
+
+if [ -d "/net/nfs/allennlp/" ]; then
+  output_dir=/net/nfs/allennlp/llm-data/${data_path}
+elif [ -v "$SCRATCH_DIR" ] & [ -d "$SCRATCH_DIR" ]; then
+  output_dir=$SCRATCH_DIR/${data_path}
+fi
 
 # Copy files from S3.
 mkdir -p ${output_dir}
