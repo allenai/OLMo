d3379ef8d47d2c5e93749b176f40be5679a6c5e5
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-05-24 10:38:36 +0000

    fix order

--- pretrain_data/filters/src/ai2_llm_filters/taggers/__init__.py
+++ pretrain_data/filters/src/ai2_llm_filters/taggers/__init__.py
@@ -1,6 +1,6 @@
+from .code import *  # noqa: F403
 from .jigsaw import *  # noqa: F403
 from .language import *  # noqa: F403
 from .length import *  # noqa: F403
 from .pii import *  # noqa: F403
 from .sampling import *  # noqa: F403
-from .code import *  # noqa: F403
