b9f032403ba9ef0a146a874d521948b9218e7da3
epwalsh <petew@allenai.org>
2023-05-25 16:11:33 -0700

    test restarts with different world size

--- tests/data/iterable_dataset_test.py
+++ tests/data/iterable_dataset_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from olmo.data import IterableDataset
 
 
@@ -32,3 +34,17 @@ def test_iterable_dataset_start_step():
         list(range(20)), world_size=2, rank=0, shuffle=False, start_index=2 * device_batch_size * 3
     )
     assert list(dataset) == [12, 14, 16, 18]
+
+
+@pytest.mark.parametrize("world_size", [2, 4])
+def test_iterable_dataset_restart_different_world_size(world_size: int):
+    start_index = 4
+    all_indices = set()
+    for rank in range(world_size):
+        dataset = IterableDataset(
+            list(range(20)), world_size=world_size, rank=rank, shuffle=False, start_index=start_index
+        )
+        indices = set(dataset)
+        assert len(all_indices & indices) == 0
+        all_indices.update(indices)
+    assert all_indices == set(range(4, 20))
