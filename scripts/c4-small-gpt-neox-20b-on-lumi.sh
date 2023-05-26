055b69ede77345294f5136e13abb202e63856e1b
Matthew Peters <matthewp@allenai.org>
2023-05-26 19:03:38 +0300

    gpt neo tokenizer c4 baseline

--- scripts/c4-small-gpt-neox-20b-on-lumi.sh
+++ scripts/c4-small-gpt-neox-20b-on-lumi.sh
@@ -12,7 +12,9 @@
 
 module load LUMI/22.08 partition/G
 
-export OLMO_CONTAINER=llm-lumi-rocm55_latest.sif
+#export OLMO_CONTAINER=llm-lumi-rocm55_latest.sif
+export OLMO_CONTAINER=llm-lumi-rocm550_latest.sif
+#export OLMO_CONTAINER=llm-lumi_latest.sif
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export MPICH_GPU_SUPPORT_ENABLED=1
