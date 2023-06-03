fe6227bee9209f362870eae0e759f81c099746b2
Luca Soldaini <luca@soldaini.net>
2023-06-02 17:26:23 -0700

    ignoring empty lines

--- scripts/prepare_memmap_dataset.py
+++ scripts/prepare_memmap_dataset.py
@@ -88,7 +88,8 @@ def tokenize_file(tokenizer: Tokenizer, path: str, safe_mode: bool = False) -> G
         try:
             for line in input_stream:
                 row = decoder.decode(line)
-                yield tokenizer.encode(row.text, add_special_tokens=True)
+                if row.text.strip():
+                    yield tokenizer.encode(row.text, add_special_tokens=True)
                 i += 1
         except Exception as e:
             log.error(f"Error processing {path}:{i:,} -> {e}")
