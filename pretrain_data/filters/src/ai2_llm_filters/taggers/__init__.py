fb725d2167d22f891bce13d42e26fd7ed471543b
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-06-05 14:29:07 -0700

    fix formatting

--- pretrain_data/filters/src/ai2_llm_filters/taggers/__init__.py
+++ pretrain_data/filters/src/ai2_llm_filters/taggers/__init__.py
@@ -1,5 +1,5 @@
-from .code import *  # noqa: F403
 from .c4 import *  # noqa: F403
+from .code import *  # noqa: F403
 from .gopher import *  # noqa: F403
 from .jigsaw import *  # noqa: F403
 from .language import *  # noqa: F403
