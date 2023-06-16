7ebd617bc1e13165b22999a604627edf6b9369f0
epwalsh <petew@allenai.org>
2023-06-16 09:12:42 -0700

    use nice trick from @soldni

--- scripts/c4-medium-on-lumi.sh
+++ scripts/c4-medium-on-lumi.sh
@@ -49,4 +49,4 @@ srun \
     -B /usr/lib64/libcxi.so.1:/usr/lib64/libcxi.so.1 \
     -B /usr/lib64/libjson-c.so.3:/usr/lib64/libjson-c.so.3 \
     $PROJECT_DIR/containers/$OLMO_CONTAINER \
-    python scripts/train.py configs/c4-medium.yaml --run_name=${SLURM_JOB_ID}
+    python scripts/train.py configs/c4-medium.yaml --run_name=${SLURM_JOB_ID} ${@}
