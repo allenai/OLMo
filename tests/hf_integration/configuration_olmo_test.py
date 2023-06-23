c92f25e6ac0b48c7426f06270297dc84a92afcfb
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-06-22 22:40:54 -0700

    add test fixture

--- tests/hf_integration/configuration_olmo_test.py
+++ tests/hf_integration/configuration_olmo_test.py
@@ -2,12 +2,12 @@ from hf_integration.configuration_olmo import OLMoConfig
 from olmo.config import ModelConfig
 
 
-def test_config_save(path: str):
+def test_config_save(model_path: str):
     config = ModelConfig(alibi=True)  # default is False
     hf_config = OLMoConfig(**config.asdict())
 
-    hf_config.save_pretrained(path)
-    loaded_hf_config = OLMoConfig.from_pretrained(path)
+    hf_config.save_pretrained(model_path)
+    loaded_hf_config = OLMoConfig.from_pretrained(model_path)
 
     assert hf_config.to_dict() == loaded_hf_config.to_dict()
 
