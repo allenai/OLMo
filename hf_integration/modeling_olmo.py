615909721193262180ca4683ab6c369802cee7e8
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-06-23 11:12:27 -0700

    update forward

--- hf_integration/modeling_olmo.py
+++ hf_integration/modeling_olmo.py
@@ -1,9 +1,10 @@
 import os
-from typing import Optional, Union
+from typing import Optional, Union, Tuple, List
 
 import torch
 from transformers import PreTrainedModel
 from transformers.models.auto import AutoModelForCausalLM
+from transformers.modeling_outputs import CausalLMOutputWithPast
 
 from olmo.config import ModelConfig
 from olmo.model import Olmo
@@ -61,10 +62,60 @@ class OLMoForCausalLM(PreTrainedModel):
         else:
             self.model = model
 
-    def forward(self, *args, **kwargs):
-        # use_cache = self.config.use_cache or kwargs.pop("use_cache", False)
-        kwargs["use_cache"] = kwargs.pop("use_cache", self.config.use_cache)
-        return self.model.forward(*args, **kwargs)
+    # def forward(self, *args, **kwargs):
+    #     # use_cache = self.config.use_cache or kwargs.pop("use_cache", False)
+    #     kwargs["use_cache"] = kwargs.pop("use_cache", self.config.use_cache)
+    #     return self.model.forward(*args, **kwargs)
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+
+        if use_cache is None:
+            use_cache = self.config.use_cache
+
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+        outputs = self.model.forward(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            past_key_values=past_key_values,
+            use_cache=use_cache,
+        )
+
+        logits = outputs.logits
+
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens
+            loss_fct = torch.nn.CrossEntropyLoss()
+            shift_logits = shift_logits.view(-1, self.config.embedding_size)
+            shift_labels = shift_labels.view(-1)
+            # Enable model parallelism
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss = loss_fct(shift_logits, shift_labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.attn_key_values,
+        )
 
     def generate(self, input_ids, *args, **kwargs):
         with torch.no_grad():
