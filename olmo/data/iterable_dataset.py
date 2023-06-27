64abda36fd3e6f59d6a6962ed90c4bfa28b8dd51
epwalsh <petew@allenai.org>
2023-06-27 16:40:23 -0700

    logging

--- olmo/data/iterable_dataset.py
+++ olmo/data/iterable_dataset.py
@@ -1,3 +1,4 @@
+import logging
 import math
 from pathlib import Path
 from typing import Any, Dict, Iterator, List, Optional, Sequence, Union
@@ -10,6 +11,9 @@ from ..aliases import PathOrStr
 from ..util import barrier, get_global_rank, get_world_size
 
 __all__ = ["IterableDataset"]
+
+
+log = logging.getLogger(__name__)
 
 
 class IterableDataset(torch.utils.data.IterableDataset[Dict[str, Any]]):
@@ -56,6 +60,7 @@ class IterableDataset(torch.utils.data.IterableDataset[Dict[str, Any]]):
         if work_dir is not None:
             self.global_indices_file = Path(work_dir) / "global_indices.npy"
             if self.rank == 0:
+                log.info("Saving global data order indices...")
                 self.global_indices_file.parent.mkdir(parents=True, exist_ok=True)
                 global_indices = self._build_global_indices()
                 global_indices_mmap = np.memmap(
@@ -64,6 +69,7 @@ class IterableDataset(torch.utils.data.IterableDataset[Dict[str, Any]]):
                 global_indices_mmap[:] = global_indices
                 global_indices_mmap.flush()
                 del global_indices_mmap
+                log.info("Global data order indices saved to '%s'", self.global_indices_file)
             barrier()
 
     def _build_global_indices(self) -> List[int]:
