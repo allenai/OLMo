43adfd68f1658a7faba90e6f868b4fd13224a448
epwalsh <petew@allenai.org>
2023-06-30 11:43:32 -0700

    update repro script

--- scripts/error_repro.py
+++ scripts/error_repro.py
@@ -1,27 +1,56 @@
-import os
+import logging
+import sys
 
 import torch
 import torch.distributed as dist
 
+from olmo.config import TrainConfig
+from olmo.data import build_train_dataloader
+from olmo.exceptions import OlmoCliError
+from olmo.util import (
+    barrier,
+    clean_opt,
+    get_local_rank,
+    get_world_size,
+    move_to_device,
+    prepare_cli_environment,
+)
 
-def get_local_rank() -> int:
-    return int(os.environ.get("LOCAL_RANK") or 0)
+log = logging.getLogger(__name__)
 
 
-def print_rank0(*args):
-    if dist.get_rank() == 0:
-        print("RANK 0", *args)
-
-
-def main() -> None:
+def main(cfg: TrainConfig) -> None:
     dist.init_process_group(backend="nccl")
     dist.barrier()
-    print_rank0("Distributed process group initialized")
+    log.info("Distributed process group initialized")
 
     torch.cuda.set_device(f"cuda:{get_local_rank()}")
-    dist.barrier()
-    print_rank0("Done!")
+    barrier()
+
+    # Fill some configuration options.
+    cfg.model.precision = cfg.precision
+    cfg.device_train_batch_size = cfg.global_train_batch_size // get_world_size()
+    assert cfg.device_train_batch_size is not None  # for mypy
+    cfg.device_train_grad_accum = cfg.device_train_batch_size // cfg.device_train_microbatch_size
+
+    log.info("Building data loader...")
+    data_loader = build_train_dataloader(cfg)
+    for i, batch in enumerate(data_loader):
+        batch = move_to_device(batch, torch.device("cuda"))
+        log.info(f"Batch {i}, size: ({batch['input_ids'].shape[0]}, {batch['input_ids'].shape[1]})")
+        barrier()
+        if i > 9:
+            break
+
+    log.info("Done!")
 
 
 if __name__ == "__main__":
-    main()
+    prepare_cli_environment()
+    try:
+        yaml_path, args_list = sys.argv[1], sys.argv[2:]
+    except IndexError:
+        raise OlmoCliError(f"Usage: {sys.argv[0]} [CONFIG_PATH] [OPTIONS]")
+
+    cfg = TrainConfig.load(yaml_path, [clean_opt(s) for s in args_list])
+    main(cfg)
