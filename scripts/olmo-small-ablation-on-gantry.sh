4466b264477195e99ded9710ed4d7556fb56502b
Luca Soldaini <luca@soldaini.net>
2023-07-13 14:57:03 -0700

    updated script

--- scripts/olmo-small-ablation-on-gantry.sh
+++ scripts/olmo-small-ablation-on-gantry.sh
@@ -28,6 +28,15 @@ RUN_HASH=$(echo "${LOAD_PATH_ARG}-${CONFIG_PATH}" | md5sum | cut -c 1-8)
 # compose the two
 FULL_RUN_NAME="${RUN_NAME}-${RUN_HASH}"
 
+# check if there is an env var called 'WANDB_API_KEY' and if so, create a flag
+# to pass to gantry
+if [ -z ${WANDB_API_KEY+x} ]; then
+  WANDB_API_KEY_ARG="--env-secret WANDB_API_KEY=WANDB_API_KEY"
+else
+  WANDB_API_KEY_ARG="--env WANDB_API_KEY=${WANDB_API_KEY}"
+fi
+
+NUM_NODES=4
 
 gantry run \
   --workspace ai2/llm-testing \
@@ -37,14 +46,14 @@ gantry run \
   --beaker-image olmo-torch2-gantry \
   --cluster ai2/general-cirrascale-a100-80g-ib \
   --gpus 8 \
-  --replicas 5 \
+  --replicas ${NUM_NODES} \
   --leader-selection  \
   --host-networking \
   --nfs \
-  --env-secret WANDB_API_KEY=WANDB_API_KEY \
+  ${WANDB_API_KEY_ARG} \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --shared-memory 10GiB \
   --venv base \
   --yes \
-  -- /bin/bash -c "torchrun --nproc-per-node 8 scripts/train.py ${CONFIG_PATH} --run_name=${FULL_RUN_NAME} ${LOAD_PATH_ARG} --device_train_microbatch_size=16 ${@}"
+  -- /bin/bash -c "torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 8 --rdzv_id=101 --rdzv_backend=c10d --rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400 scripts/train.py ${CONFIG_PATH} --run_name=${FULL_RUN_NAME} ${LOAD_PATH_ARG} --device_train_microbatch_size=8 ${@}"
