092a8fabde3d22e221c68aa604dd562e2b9ba9f8
Pranjali Basmatkar <pranjalib@ip-172-16-21-221.us-west-2.compute.internal>
2023-08-10 15:33:02 -0700

    new files for inference

--- inference/efficiency/run_efficiency_benchmark.sh
+++ inference/efficiency/run_efficiency_benchmark.sh
@@ -1,9 +1,15 @@
 
-PRETRAINED_MODEL=$1
-QUANTIZED_MODEL=$2
+#PRETRAINED_MODEL=$1
+#QUANTIZED_MODEL=$2
+
+#efficiency-pentathlon run \
+#	--task wikitext-prompt \
+#      	--scenario single_stream \
+#	--limit 100 \
+#	-- python run_efficiency_benchmark.py --pretrained_model_dir $PRETRAINED_MODEL --quantized_model_dir $QUANTIZED_MODEL
 
 efficiency-pentathlon run \
 	--task wikitext-prompt \
-      	--scenario single_stream \
+	--scenario single_stream \
 	--limit 100 \
-	-- python run_efficiency_benchmark.py --pretrained_model_dir $PRETRAINED_MODEL --quantized_model_dir $QUANTIZED_MODEL
+	-- python efficiency_report.py --pretrained-model-dir 'facebook/opt-125m'
