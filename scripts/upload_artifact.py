216bbb7e7d13a2a0f81796085d623c846e65fe47
Dirk Groeneveld <dirkg@allenai.org>
2023-08-10 14:21:48 -0700

    Upload in parallel

--- scripts/upload_artifact.py
+++ scripts/upload_artifact.py
@@ -1,4 +1,6 @@
+import concurrent
 import logging
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from typing import Tuple
 
@@ -34,26 +36,38 @@ def main(
     bucket = storage_client.bucket("ai2-olmo", user_project="ai2-olmo")
     prefix = wandb_run_path.strip("/")
 
+    # Resolve directories to their contents; We only upload files.
     artifacts_to_upload = [
         (file_or_directory, prefix + "/" + file_or_directory.resolve().name)
         for file_or_directory in files_or_directories
     ]
+    files_to_upload = []
     while len(artifacts_to_upload) > 0:
         file_or_directory, key = artifacts_to_upload.pop()
         if file_or_directory.is_file():
-            blob = bucket.blob(key)
-            with file_or_directory.open("rb") as f:
-                with tqdm.wrapattr(
-                    f,
-                    "read",
-                    total=file_or_directory.stat().st_size,
-                    miniters=1,
-                    desc=f"Uploading {file_or_directory} to gs://{bucket.name}/{key}",
-                ) as f:
-                    blob.upload_from_file(f)
+            files_to_upload.append((file_or_directory, key))
         elif file_or_directory.is_dir():
             for directory_entry in file_or_directory.iterdir():
                 artifacts_to_upload.append((directory_entry, key + "/" + directory_entry.name))
+        del key
+
+    # Upload files in parallel
+    def upload(file, key):
+        blob = bucket.blob(key)
+        with file.open("rb") as f:
+            with tqdm.wrapattr(
+                f,
+                "read",
+                total=file.stat().st_size,
+                miniters=1,
+                desc=f"Uploading {file} to gs://{bucket.name}/{key}",
+            ) as f:
+                blob.upload_from_file(f)
+
+    with ThreadPoolExecutor(max_workers=32) as executor:
+        futures = [executor.submit(upload, *args) for args in files_to_upload]
+        for future in concurrent.futures.as_completed(futures):
+            future.result()     # We do this so we can see exceptions.
 
 
 if __name__ == "__main__":
