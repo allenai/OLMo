970326502e8c8bdbba536dde58d7438dc9cb5994
Pranjali Basmatkar <pranjalib@Pranjali-Basmatkars-MacBook-Pro.local>
2023-08-15 10:06:32 -0700

    minor edits

--- inference/efficiency/run_efficiency_benchmark.sh
+++ inference/efficiency/run_efficiency_benchmark.sh
@@ -1,15 +1,9 @@
 
-#PRETRAINED_MODEL='facebook/opt-125m'
+PRETRAINED_MODEL='facebook/opt-125m'
 #QUANTIZED_MODEL='/home/pranjalib/LLM/inference/quantized_opt125m'
-
-#efficiency-pentathlon run \
-#	--task wikitext-prompt \
-#      	--scenario single_stream \
-#	--limit 100 \
-#	-- python run_efficiency_benchmark.py --pretrained-model-dir $PRETRAINED_MODEL --quantized-model-dir $QUANTIZED_MODEL
 
 efficiency-pentathlon run \
 	--task wikitext-prompt \
 	--scenario single_stream \
 	--limit 100 \
-       -- python efficiency_report.py --pretrained-model-dir 'facebook/opt-125m'
+       -- python efficiency_report.py --pretrained-model-dir PRETRAINED_MODEL
