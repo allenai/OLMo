74d6c69d25b042f537450adf6c9af92946edd8e1
Pranjali Basmatkar <pranjalib@allennlp-cirrascale-20.reviz.ai2.in>
2023-08-16 18:42:47 -0700

    minor changes

--- inference/efficiency/run_efficiency_benchmark.sh
+++ inference/efficiency/run_efficiency_benchmark.sh
@@ -1,9 +1,9 @@
 
-PRETRAINED_MODEL='facebook/opt-125m'
+#PRETRAINED_MODEL= 'facebook/opt-125m'
 #QUANTIZED_MODEL='/home/pranjalib/LLM/inference/quantized_opt125m'
 
 efficiency-pentathlon run \
 	--task wikitext-prompt \
 	--scenario single_stream \
-	--limit 100 \
-       -- python efficiency_report.py --pretrained-model-dir PRETRAINED_MODEL
+	--limit 1 \
+       -- python efficiency_report.py --pretrained-model-dir 'facebook/opt-125m'
