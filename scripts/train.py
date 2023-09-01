eb1946ce67cdd9db0a45642ffc2594a20c35ee67
Dirk Groeneveld <dirkg@allenai.org>
2023-08-31 23:18:39 -0700

    Starting to remove the config

--- scripts/train.py
+++ scripts/train.py
@@ -1,26 +1,20 @@
 """Run this script with 'torchrun'."""
 
 import logging
-import math
-import os
 import sys
-from typing import Dict, Any, List
 
 import torch
 import torch.distributed as dist
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP, ShardingStrategy
 from torch.distributed.fsdp import MixedPrecision
 
 from olmo.config import TrainConfig
-from olmo.exceptions import OlmoCliError
 from olmo.model import Olmo
-from olmo.optim import build_optimizer, build_scheduler
+from olmo.optim import build_optimizer
 from olmo.util import (
     barrier,
     clean_opt,
     get_local_rank,
-    get_world_size,
-    log_extra_field,
     peak_gpu_memory,
     prepare_cli_environment,
     seed_all, move_to_device,
@@ -30,28 +24,13 @@ log = logging.getLogger("train")
 
 
 def main(cfg: TrainConfig) -> None:
-    # Ensure run name set.
-    if cfg.run_name is None:
-        cfg.run_name = os.environ.get("COMPOSER_RUN_NAME", "train-llm")
-    log_extra_field("run_name", cfg.run_name)
-
     # Initialize process group and set device.
     dist.init_process_group(backend="nccl")
     barrier()
     torch.cuda.set_device(f"cuda:{get_local_rank()}")
     device = torch.device("cuda")
-
-    # Fill some configuration options.
-    cfg.model.precision = cfg.precision
-    cfg.device_train_batch_size = cfg.global_train_batch_size // get_world_size()
-    assert cfg.device_train_batch_size is not None  # for mypy
-    cfg.device_train_grad_accum = cfg.device_train_batch_size // cfg.device_train_microbatch_size
-
     barrier()
-
-    # Set seed.
-    seed_all(cfg.seed)
-
+    seed_all(6198)
     barrier()
 
     # Initialize the model.
@@ -65,28 +44,23 @@ def main(cfg: TrainConfig) -> None:
     log.info("Wrapping model with FDSP...")
     fsdp_model = FSDP(
         olmo_model,
-        sharding_strategy=cfg.fsdp.sharding_strategy,
+        sharding_strategy=ShardingStrategy.FULL_SHARD,
         mixed_precision=MixedPrecision(  # equivalent to MosaicML's "PURE"
-            param_dtype=cfg.autocast_precision,
-            reduce_dtype=cfg.autocast_precision,
-            buffer_dtype=cfg.autocast_precision,
+            param_dtype=torch.bfloat16,
+            reduce_dtype=torch.bfloat16,
+            buffer_dtype=torch.bfloat16,
         ),
         auto_wrap_policy=olmo_model.fsdp_wrap_fn,
-        use_orig_params=cfg.fsdp.use_orig_params,  # needed for compile and some of our optimizer/parameter metrics
+        use_orig_params=True,  # needed for compile and some of our optimizer/parameter metrics
         limit_all_gathers=True,
         device_id=get_local_rank(),
     )
-
-    log.info(f"Peak GPU Memory (MB) after FSDP: {int(peak_gpu_memory() or 0)}")
-
-    assert not cfg.activation_checkpointing
 
     log.info("Model:")
     log.info(fsdp_model)
 
     # Construct optimizer and learning rate scheduler.
     optim = build_optimizer(cfg, fsdp_model)
-    scheduler = build_scheduler(cfg)
 
     fsdp_model.train()
 
@@ -181,7 +155,7 @@ def main(cfg: TrainConfig) -> None:
     }
 
     ce_batch_loss = torch.tensor(0.0, device=device)
-    with torch.autocast("cuda", enabled=True, dtype=cfg.autocast_precision):
+    with torch.autocast("cuda", enabled=True, dtype=torch.bfloat16):
         # Run forward pass.
         logits = fsdp_model(
             input_ids=micro_batch["input_ids"],
@@ -226,7 +200,7 @@ if __name__ == "__main__":
     try:
         yaml_path, args_list = sys.argv[1], sys.argv[2:]
     except IndexError:
-        raise OlmoCliError(f"Usage: {sys.argv[0]} [CONFIG_PATH] [OPTIONS]")
+        raise RuntimeError(f"Usage: {sys.argv[0]} [CONFIG_PATH] [OPTIONS]")
 
     cfg = TrainConfig.load(yaml_path, [clean_opt(s) for s in args_list])
     main(cfg)
