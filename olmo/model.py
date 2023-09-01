5930108e36cf30118b695f3872e91ccc29f646b5
Dirk Groeneveld <dirkg@allenai.org>
2023-08-31 23:47:06 -0700

    Remove config some more

--- olmo/model.py
+++ olmo/model.py
@@ -8,14 +8,12 @@ from __future__ import annotations
 
 import logging
 import math
-from abc import abstractmethod
 from typing import List, NamedTuple, Optional, Sequence, Tuple
 
 import torch
 import torch.backends.cuda
 import torch.nn as nn
 import torch.nn.functional as F
-from .config import ModelConfig
 from .initialization import init_weights
 
 __all__ = [
@@ -37,10 +35,7 @@ class LayerNorm(nn.Module):
         self.normalized_shape = (4096,)
         self.eps = 1e-05
         self.weight = nn.Parameter(torch.ones(self.normalized_shape, device="meta"))
-        if self.config.include_bias:
-            self.bias = nn.Parameter(torch.zeros(self.normalized_shape, device="meta"))
-        else:
-            self.register_parameter("bias", None)
+        self.register_parameter("bias", None)
         self.low_precision = low_precision
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -92,7 +87,7 @@ class Olmo(nn.Module):
     def __init__(self):
         super().__init__()
 
-        torch.backends.cuda.enable_flash_sdp(self.config.flash_attention)
+        torch.backends.cuda.enable_flash_sdp(False)
         torch.backends.cuda.enable_mem_efficient_sdp(False)  # this is super slow so make sure torch won't use it
 
         self.transformer = nn.ModuleDict(
@@ -112,12 +107,11 @@ class Olmo(nn.Module):
         log.info("Initializing model parameters...")
         # Top-level embeddings / linear layers.
         init_weights(
-            self.config,
             self.transformer.wte,  # type: ignore
-            std_factor=(0.5 * math.sqrt(self.config.d_model)) if self.config.scale_logits else 1.0,
+            std_factor=1.0,
         )
         if hasattr(self.transformer, "wpe"):
-            init_weights(self.config, self.transformer.wpe)  # type: ignore
+            init_weights(self.transformer.wpe)  # type: ignore
 
         # Top-level layer norm.
         self.transformer.ln_f.reset_parameters()  # type: ignore
@@ -126,10 +120,7 @@ class Olmo(nn.Module):
     def device(self) -> torch.device:
         device: torch.device = self.transformer.wte.weight.device  # type: ignore
         if device.type == "meta":
-            if self.config.init_device is not None and self.config.init_device != "meta":
-                return torch.device(self.config.init_device)
-            else:
-                return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            return torch.device("cuda" if torch.cuda.is_available() else "cpu")
         else:
             return device
 
@@ -142,42 +133,7 @@ class Olmo(nn.Module):
         use_cache: bool = False,
         last_logits_only: bool = False,
     ) -> OlmoOutput:
-        """
-        :param input_ids: A tensor of shape `(batch_size, seq_len)`.
-        :param attention_mask: A tensor of shape `(batch_size, seq_len)` that indicates
-            which input IDs are masked. A `1` value in the mask means that
-            the corresponding input ID should *not* be ignored. A `0` means
-            that the corresponding input ID is masked.
-
-            This has the same meaning as the `attention_mask` in HuggingFace's `transformers`
-            library.
-        :param attention_bias: A tensor of shape `(batch_size, 1, seq_len, seq_len)`,
-            `(1, 1, seq_len, seq_len)`, or `(seq_len, seq_len)`. This is used
-            to introduce causal or other biases.
-
-            If the tensor is a bool or byte tensor, a `True` or `1` at `attention_bias[:, :, i, j]`
-            indicates that the i-th element in the sequence is allowed to attend to the j-th
-            element in the sequence.
-
-            If the tensor is a float tensor, it will just be added to the attention
-            scores before the softmax.
-
-            The default is causal, which corresponds to a lower-diagonal byte matrix of ones.
-        :param past_key_values: Pre-computed keys and values for each attention block.
-            Can be used to speed up sequential decoding. The `input_ids` which have
-            their past given to this model should not be passed as `input_ids` as they have already been computed.
-        :param use_cache: If `True`, return key and value tensors for each block.
-        :param last_logits_only: If `True`, only compute the logits for the last token of each sequence.
-            This can speed up decoding when you only care about the next token.
-        """
-        if past_key_values:
-            assert len(past_key_values) == self.config.n_layers
-
         batch_size, seq_len = input_ids.size()
-        assert seq_len <= self.config.max_sequence_length, (
-            f"Cannot forward input with seq_len={seq_len}, "
-            f"this model only supports seq_len<={self.config.max_sequence_length}"
-        )
 
         # Get embeddings of input.
         # shape: (batch_size, seq_len, d_model)
@@ -209,8 +165,6 @@ class Olmo(nn.Module):
         # Get logits.
         # shape: (batch_size, seq_len or 1, vocab_size)
         logits = F.linear(x, self.transformer.wte.weight, None)  # type: ignore
-        if self.config.scale_logits:
-            logits.mul_(1 / math.sqrt(self.config.d_model))
 
         return OlmoOutput(logits=logits, attn_key_values=None)  # type: ignore[arg-type]
 
@@ -237,17 +191,4 @@ class Olmo(nn.Module):
 
     @property
     def num_fwd_flops(self):
-        if self.__num_fwd_flops:
-            return self.__num_fwd_flops
-        n_params = self.num_params()
-        # the number of parameters is approximately the number of multiply-accumulates (MAC) in the network
-        # each MAC has 2 FLOPs - we multiply by 2 ie 2 * n_param
-        # this gets us FLOPs / token
-        params_flops_per_token = 2 * n_params
-        params_flops_per_seq = params_flops_per_token * self.config.max_sequence_length
-        # there are 2 FLOPS per mac; there is A=Q*K^T and out=A*V ops (ie mult by 2)
-        attn_flops_per_seq = (
-            self.config.n_layers * 2 * 2 * (self.config.d_model * (self.config.max_sequence_length**2))
-        )
-        self.__num_fwd_flops = params_flops_per_seq + attn_flops_per_seq
-        return self.__num_fwd_flops
+        return 1
