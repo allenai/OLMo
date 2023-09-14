4c9a40d0cfbabb6d9c47f4e1bf903447a026def0
Dirk Groeneveld <dirkg@allenai.org>
2023-09-14 21:52:01 +0300

    Include the local rank in the log outputs

--- scripts/run_with_environment.sh
+++ scripts/run_with_environment.sh
@@ -4,11 +4,7 @@
 
 set -euo pipefail
 
-# Redirect stdout and stderr so that we get a prefix with the node name
 export NODENAME=$(hostname -s)
-exec > >(trap "" INT TERM; sed -u "s/^/$NODENAME out: /")
-exec 2> >(trap "" INT TERM; sed -u "s/^/$NODENAME err: /" >&2)
-
 export MASTER_ADDR=$(scontrol show hostnames | head -n 1)
 export MASTER_PORT=39591
 export WORLD_SIZE=$SLURM_NTASKS
@@ -16,6 +12,10 @@ export RANK=$SLURM_PROCID
 export LOCAL_WORLD_SIZE=$SLURM_NTASKS_PER_NODE
 export LOCAL_RANK=$SLURM_LOCALID
 export NODE_RANK=$((($RANK - $LOCAL_RANK) / $LOCAL_WORLD_SIZE))
+
+# Redirect stdout and stderr so that we get a prefix with the node name
+exec > >(trap "" INT TERM; sed -u "s/^/$NODENAME:$LOCAL_RANK out: /")
+exec 2> >(trap "" INT TERM; sed -u "s/^/$NODENAME:$LOCAL_RANK err: /" >&2)
 
 if [ $SLURM_LOCALID -eq 0 ] ; then
   rm -rf /dev/shm/* || true
