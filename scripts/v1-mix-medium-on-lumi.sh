31147fa9b278e94040d6e52b79d2086ee6c5016a
Dirk Groeneveld <dirkg@allenai.org>
2023-09-16 04:14:42 +0300

    128 nodes

--- scripts/v1-mix-medium-on-lumi.sh
+++ scripts/v1-mix-medium-on-lumi.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=v1-mix-medium
 #SBATCH --account=project_462000229
 #SBATCH --output=/pfs/lustref1/flash/project_462000229/logs/%j.log
-#SBATCH --nodes=32             # Total number of nodes 
+#SBATCH --nodes=128             # Total number of nodes 
 #SBATCH --ntasks-per-node=8
 #SBATCH --gpus-per-node=8       # Allocate one gpu per MPI rank
 #SBATCH --cpus-per-task=6
