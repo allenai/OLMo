b2a11fb3ceeaf09411e357bcede62e16ebc909d6
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-09-19 16:50:48 -0700

    more options

--- inference/paged_attention/run_vllm_test.py
+++ inference/paged_attention/run_vllm_test.py
@@ -4,30 +4,34 @@ from vllm import SamplingParams, LLM
 from vllm.model_executor.utils import set_random_seed
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-def compare_vllm_with_hf(path: str, prompt: str = "My name is John! I am "):
+def compare_vllm_with_hf(path: str, prompt: str = "My name is John! I am ", which: str = "both"):
+
+    assert which in ["both", "hf", "vllm"]
     
     # VLLM
-    """
-    s = SamplingParams(temperature=0.0)
-    llm = LLM(model=path, trust_remote_code=True, gpu_memory_utilization=0.90)
+    if which in ["vllm", "both"]:
+        s = SamplingParams(temperature=0.0)
+        llm = LLM(model=path, trust_remote_code=True, gpu_memory_utilization=0.90)
 
-    set_random_seed(0)
-    print(llm.generate([prompt], sampling_params=s))
-    """
+        set_random_seed(0)
+        print(llm.generate([prompt], sampling_params=s))
 
     # HF
-    hf_model = AutoModelForCausalLM.from_pretrained(path, trust_remote_code=True, device_map="auto").cuda()
-    tokenizer = AutoTokenizer.from_pretrained(path)
-    input = tokenizer.encode(prompt)
-    input_tensor = torch.tensor(input).unsqueeze(0)
+    if which in ["hf", "both"]:
+        hf_model = AutoModelForCausalLM.from_pretrained(path, trust_remote_code=True, device_map="auto").cuda()
+        tokenizer = AutoTokenizer.from_pretrained(path)
+        input = tokenizer.encode(prompt)
+        input_tensor = torch.tensor(input).unsqueeze(0)
 
-    set_random_seed(0)
-    hf_gen = hf_model.generate(input_tensor.long().cuda())
+        set_random_seed(0)
+        hf_gen = hf_model.generate(input_tensor.long().cuda())
 
-    print(tokenizer.decode(hf_gen[0].tolist()))
+        print(tokenizer.decode(hf_gen[0].tolist()))
 
-    #"""
 
 if __name__ == "__main__":
     import sys
-    compare_vllm_with_hf(sys.argv[1])
+    if len(sys.argv) == 2:
+        compare_vllm_with_hf(sys.argv[1])
+    else:
+        compare_vllm_with_hf(sys.argv[1], which=sys.argv[2])
