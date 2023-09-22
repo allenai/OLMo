570dd7778539ad87b2729fba55d420581f51bef6
epwalsh <petew@allenai.org>
2023-09-21 17:30:55 -0700

    try no affines anywhere

--- olmo/model.py
+++ olmo/model.py
@@ -362,9 +362,8 @@ class OlmoBlock(nn.Module):
             self.k_norm = LayerNormBase.build(
                 config,
                 size=config.d_model // config.n_heads if config.multi_query_attention else None,
-                elementwise_affine=True,
             )
-            self.q_norm = LayerNormBase.build(config, elementwise_affine=True)
+            self.q_norm = LayerNormBase.build(config)
 
         # Activation function.
         self.act = Activation.build(config)
