a9581b9748e7fb6a30e59475dae421d244e86ad0
Saurabh Shah <saurabhs@allennlp-cirrascale-20.reviz.ai2.in>
2023-09-27 13:54:29 -0700

    config

---
+++ saurabhs_stuff/run_tiny.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=8
+run_name=c4-tiny-run-001
+rm -rf "runs/${run_name}"
+torchrun --nproc-per-node 2 scripts/train.py saurabhs_stuff/c4-tiny.yaml \
+  --run_name="${run_name}" \
+  --save_folder="runs/${run_name}" \
+  --console_log_interval=1 
