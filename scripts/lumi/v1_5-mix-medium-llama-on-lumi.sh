db1040257de66609ca825025f560554babaea7b6
Dirk Groeneveld <dirkg@allenai.org>
2023-10-27 15:48:22 -0700

    Use new paths

--- scripts/lumi/v1_5-mix-medium-llama-on-lumi.sh
+++ scripts/lumi/v1_5-mix-medium-llama-on-lumi.sh
@@ -36,6 +36,10 @@ export SINGULARITYENV_LD_LIBRARY_PATH=/usr/local/lib:/opt/cray/libfabric/1.15.2.
 # Try playing with max_split_size_mb if you run into OOM errors.
 #export PYTORCH_HIP_ALLOC_CONF=max_split_size_mb:128
 
+export DATA_PATH=$FLASH_DIR/preprocessed/olmo-mix
+export CHECKPOINTS_PATH=$FLASH_DIR/checkpoints
+export EVAL_DATA_PATH=$SCRATCH_DIR/eval-data
+
 srun \
   --cpus-per-task=$SLURM_CPUS_PER_TASK \
   --distribution=block:block \
