6c0e41966872ead3c0c1fde71a86870e0725bb8d
Pete <epwalsh10@gmail.com>
2023-11-06 16:32:08 -0800

    Add gradient clipping warmup (#363)

--- olmo/config.py
+++ olmo/config.py
@@ -463,6 +463,18 @@ class SchedulerConfig(BaseConfig):
     t_max: Optional[int] = None
     alpha_f: float = 0.1
 
+    grad_clip_warmup_steps: Optional[int] = None
+    """
+    The warmup period for which the max grad norm (or norm ratio) will be set to its
+    warmup value of `max_grad_norm * grad_clip_warmup_factor`.
+    """
+
+    grad_clip_warmup_factor: Optional[float] = None
+    """
+    The ratio of the max allowed gradient norm (or norm ratio) for clipping during the warmup period
+    vs after the warmup period.
+    """
+
 
 class PaddingDirection(StrEnum):
     right = "right"
