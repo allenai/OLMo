36f2b171d40a7e2b1f576871e00a2f4881d162e4
Dirk Groeneveld <dirkg@allenai.org>
2023-11-21 15:24:55 -0800

    Revert "Mokey patch FSDP stream management"
    This reverts commit 12fa63c14d5cad5a432a8cf11fc0da78c54ed78f.

--- scripts/train.py
+++ scripts/train.py
@@ -11,8 +11,6 @@ import torch.distributed as dist
 import wandb
 from packaging import version
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-from torch.distributed.fsdp._common_utils import _FSDPState
-from torch.distributed.fsdp._init_utils import HYBRID_SHARDING_STRATEGIES
 
 from olmo.config import CheckpointType, TrainConfig
 from olmo.data import build_train_dataloader
@@ -35,46 +33,6 @@ from olmo.util import (
 )
 
 log = logging.getLogger("train")
-
-
-#
-# Monkey patching FSDP's stream management
-#
-
-def _init_streams(
-    state: _FSDPState,
-) -> None:
-    """
-    Initializes CUDA streams for overlapping communication, computation, and
-    data transfers. The streams should be shared across FSDP instances.
-    """
-    assert state._is_root
-    assert state._device_handle.is_available()
-    uses_hybrid_sharding = any(
-        fsdp_state.sharding_strategy in HYBRID_SHARDING_STRATEGIES
-        for fsdp_state in state._all_fsdp_states
-    )
-    # Prioritize all-gathers/reduce-scatters over async all-reduce for HSDP and
-    # preserve the default priority of 0 otherwise
-    high_priority = -1 if state.limit_all_gathers and uses_hybrid_sharding else 0
-    # Default stream for computation
-    state._default_stream = state._device_handle.current_stream()
-    # Stream for unshard logic, including allocating the all-gather destination
-    # tensors and the all-gathers themselves
-    state._unshard_stream = state._device_handle.Stream(priority=high_priority)
-    # Stream for overlapping gradient reduction with the backward pass gradient
-    # computation
-    state._post_backward_stream = state._device_handle.Stream(priority=high_priority)
-    # Stream for pre-unshard logic, namely allocations and writes for CPU
-    # offloading (H2D copy) and mixed precision (low precision cast)
-    state._pre_unshard_stream = state._post_backward_stream
-    # Stream to run HSDP's all-reduce as async (if using HSDP)
-    state._all_reduce_stream = (
-        state._device_handle.Stream() if uses_hybrid_sharding else state._default_stream
-    )
-
-torch.distributed.fsdp._runtime_utils._init_streams = _init_streams
-
 
 
 def main(cfg: TrainConfig) -> None:
