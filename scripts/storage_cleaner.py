6f9c261ef68f02f6bb1e617e56a7e4e77a44cf42
Shane A <shanea@allenai.org>
2023-11-22 09:30:46 -0500

    Fix instance where _get_file_size can get called on a directory

--- scripts/storage_cleaner.py
+++ scripts/storage_cleaner.py
@@ -143,7 +143,7 @@ class LocalFileSystemAdapter(StorageAdapter):
                 for entry in path.iterdir()
                 if (
                     (include_files or not entry.is_file())
-                    and (max_file_size is None or self._get_file_size(path) <= max_file_size)
+                    and (not entry.is_file() or max_file_size is None or self._get_file_size(entry) <= max_file_size)
                 )
             ]
 
