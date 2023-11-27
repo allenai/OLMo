60e384d0c205693ec26035a1fa702fbc5174b37b
Muennighoff <n.muennighoff@gmail.com>
2023-11-27 05:11:52 +0200

    Fix naming

--- scripts/lumi/olmo-small-ablation-on-lumi-test.sh
+++ scripts/lumi/olmo-small-ablation-on-lumi-test.sh
@@ -41,7 +41,7 @@ export PYTHONPATH=.:${PYTHONPATH}
 export WANDB_PROJECT=olmodeepspeed
 export ROCM_PATH=/opt/rocm
 export SINGULARITYENV_LD_LIBRARY_PATH=/usr/local/lib:/opt/cray/libfabric/1.15.2.0/lib64
-export CONFIG_PATH=configs/olmo-small-ablation.yaml
+export CONFIG_PATH=configs/olmo-small-ablation-lumi.yaml
 
 # Try playing with max_split_size_mb if you run into OOM errors.
 # export PYTORCH_HIP_ALLOC_CONF=max_split_size_mb:512
