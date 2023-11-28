385ee0ee27aef5865175b8f4566bcfefdb2bde82
Shane A <shanea@allenai.org>
2023-11-27 17:23:47 -0800

    FSDP temp experiments

--- scripts/fsdp-overlap-test.py
+++ scripts/fsdp-overlap-test.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+from typing import List, Optional
 from packaging import version
 
 import torch
@@ -17,28 +18,45 @@ from olmo.torch_util import (
     peak_gpu_memory,
     seed_all,
 )
+from olmo.util import prepare_cli_environment
 
 log = logging.getLogger(__name__)
 
 RANK_TO_BATCH_SIZE_MAP = {
-    0: 32,
-    1: 16384,
+    0: 2 ** 3,
+    1: 0,
 }
-INPUT_DIM: int = 1024
+PARAM_DIM: int = 2 ** 13
+GATHER_DIM: int = 2 ** 14
 
 
 class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.param = torch.nn.Linear(INPUT_DIM, 32768)
+        # self.param1 = torch.nn.Linear(PARAM_DIM, PARAM_DIM)
+        # self.param2 = torch.nn.Linear(PARAM_DIM, PARAM_DIM)
+        # self.param3 = torch.nn.Linear(PARAM_DIM, PARAM_DIM)
+        # self.param4 = torch.nn.Linear(PARAM_DIM, PARAM_DIM)
+        # self.param5 = torch.nn.Linear(PARAM_DIM, PARAM_DIM)
+
+        self.params = torch.nn.ModuleList([
+            torch.nn.Linear(PARAM_DIM, PARAM_DIM, bias=False),
+            torch.nn.Linear(PARAM_DIM, PARAM_DIM, bias=False),
+            torch.nn.Linear(PARAM_DIM, PARAM_DIM, bias=False),
+            torch.nn.Linear(PARAM_DIM, PARAM_DIM, bias=False),
+            torch.nn.Linear(PARAM_DIM, PARAM_DIM, bias=False),
+        ])
 
     def forward(self, x):
-        self.param(x)
+        for param in self.params:
+            x = param(x)
+
+        return x
 
 
 def get_profiler(save_folder: str) -> torch.profiler.profile:
-    profiling_schedule = schedule(wait=0, warmup=0, active=1)
+    profiling_schedule = schedule(wait=0, warmup=5, active=5)
 
     def on_trace_ready(p):
         profiler_output_dir = Path(save_folder) / "profiler"
@@ -49,7 +67,7 @@ def get_profiler(save_folder: str) -> torch.profiler.profile:
         output = p.key_averages().table(sort_by="self_cpu_time_total", row_limit=32)
         log.info("Profile by total CPU time at step %d:\n%s", p.step_num, output)
 
-        p.export_chrome_trace(str((profiler_output_dir / f"{p.step_num}.chrome_trace.json.gz")))
+        p.export_chrome_trace(str((profiler_output_dir / f"{get_global_rank()}.{p.step_num}.chrome_trace.json.gz")))
 
     return torch.profiler.profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
@@ -79,8 +97,8 @@ def get_fsdp_model() -> FSDP:
         param_init_fn = None
     return FSDP(
         model,
+        sync_module_states=True,  # Ensure same init across all ranks for this test
         sharding_strategy=ShardingStrategy.FULL_SHARD,
-        auto_wrap_policy=None,
         use_orig_params=True,  # needed for compile and some of our optimizer/parameter metrics
         limit_all_gathers=True,
         device_id=get_local_rank(),
@@ -95,27 +113,61 @@ def init_process_group():
     torch.cuda.set_device(f"cuda:{get_local_rank()}")
 
 
+def do_communication(data_to_gather: torch.Tensor, gather_list: Optional[List[torch.Tensor]]):
+    dist.gather(data_to_gather, gather_list, 1)
+
+
+def do_computation(fsdp_model: FSDP, batch: torch.Tensor):
+    fsdp_model(batch)
+
+
+def run_batch(fsdp_model: FSDP, batch: torch.Tensor, data_to_gather: torch.Tensor, gather_list: Optional[List[torch.Tensor]]):
+    do_computation(fsdp_model, batch)
+    do_communication(data_to_gather, gather_list)
+    barrier()
+
+
+def run_batches(fsdp_model: FSDP):
+    batch_size = RANK_TO_BATCH_SIZE_MAP[get_global_rank()]
+
+    save_folder = "."
+    torch_profiler = get_profiler(save_folder)
+
+    batch = torch.randn((batch_size, PARAM_DIM)).cuda()
+    data_to_gather = torch.randn((GATHER_DIM, GATHER_DIM)).cuda()
+    gather_list = None
+    if get_global_rank() == 1:
+        gather_list = [torch.zeros((GATHER_DIM, GATHER_DIM)).cuda(), torch.zeros((GATHER_DIM, GATHER_DIM)).cuda()]
+
+    with torch_profiler as p:
+        for _ in range(10):
+            run_batch(fsdp_model, batch, data_to_gather, gather_list)
+            p.step()
+
+
 def test():
-    init_process_group()
     fsdp_model = get_fsdp_model()
 
     log.info("Peak GPU Memory (MB) after FSDP: %d", int(peak_gpu_memory() or 0))
     log.info("Model:")
     log.info(fsdp_model)
 
-    batch_size = RANK_TO_BATCH_SIZE_MAP[get_global_rank()]
+    # log.warning("Param 1 weight shape %s", fsdp_model.param1.weight.shape)
+    # log.warning("Param 2 weight shape %s", fsdp_model.param2.weight.shape)
+    # log.warning("Param weight shape %s", fsdp_model.param.weight.shape)
+    for param in fsdp_model.parameters():
+        log.warning("Param weight shape %s", param.shape)
+    log.info("Global rank %d", get_global_rank())
+    log.info("Local rank %d", get_local_rank())
 
-    for _ in range(10):
-        x = torch.randn((batch_size, INPUT_DIM))
-        fsdp_model(x)
+    run_batches(fsdp_model)
 
 
 def main():
-    save_folder = "."
-    torch_profiler = get_profiler(save_folder)
-    with torch_profiler as p:
-        test()
+    test()
 
 
 if __name__ == "__main__":
+    init_process_group()
+    prepare_cli_environment()
     main()
