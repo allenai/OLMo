f613e004be68d3ae7fe249a715e5481808662fa2
Shane A <shanea@allenai.org>
2023-11-29 15:40:08 -0800

    Re-use computation and communication streams

--- scripts/amd_stream_overlap_issue_repro.py
+++ scripts/amd_stream_overlap_issue_repro.py
@@ -5,6 +5,7 @@ from typing import List, Optional
 
 import torch
 import torch.distributed as dist
+from torch.cuda import Stream
 from torch.profiler import ProfilerActivity, schedule
 
 log = logging.getLogger(__name__)
@@ -84,24 +85,27 @@ def init_process_group():
     torch.cuda.set_device(f"cuda:{get_local_rank()}")
 
 
-def do_communication(data_to_gather: torch.Tensor, gather_list: Optional[List[torch.Tensor]]):
-    s: torch.cuda.Stream = torch.cuda.Stream()
-    with torch.cuda.stream(s):
+def do_communication(data_to_gather: torch.Tensor, gather_list: Optional[List[torch.Tensor]], stream: Stream):
+    with torch.cuda.stream(stream):
         # dist.gather(data_to_gather, gather_list, 1)
         dist.all_gather(gather_list, data_to_gather)
 
 
-def do_computation(model: Model, batch: torch.Tensor):
-    s: torch.cuda.Stream = torch.cuda.Stream()
-    with torch.cuda.stream(s):
+def do_computation(model: Model, batch: torch.Tensor, stream: Stream):
+    with torch.cuda.stream(stream):
         model(batch)
 
 
 def run_batch(
-    model: Model, batch: torch.Tensor, data_to_gather: torch.Tensor, gather_list: Optional[List[torch.Tensor]]
+    model: Model,
+    batch: torch.Tensor,
+    data_to_gather: torch.Tensor,
+    gather_list: Optional[List[torch.Tensor]],
+    communication_stream: Stream,
+    computation_stream: Stream,
 ):
-    do_computation(model, batch)
-    do_communication(data_to_gather, gather_list)
+    do_computation(model, batch, computation_stream)
+    do_communication(data_to_gather, gather_list, communication_stream)
     barrier()
 
 
@@ -116,9 +120,12 @@ def run_batches(model: Model):
 
     gather_list = [torch.zeros((GATHER_DIM, GATHER_DIM)).cuda(), torch.zeros((GATHER_DIM, GATHER_DIM)).cuda()]
 
+    communication_stream: torch.cuda.Stream = Stream()
+    computation_stream: torch.cuda.Stream = Stream()
+
     with torch_profiler as p:
         for _ in range(6):
-            run_batch(model, batch, data_to_gather, gather_list)
+            run_batch(model, batch, data_to_gather, gather_list, communication_stream, computation_stream)
             p.step()
 
 
