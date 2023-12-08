81776a5d45c39d1a1c8422d70dbac155505bad2b
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-12-08 14:54:23 -0800

    add test for save_pretrained

--- tests/hf_olmo/modeling_olmo_test.py
+++ tests/hf_olmo/modeling_olmo_test.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import torch
 
 from olmo.model import Olmo
@@ -6,7 +8,7 @@ from olmo.model import Olmo
 def test_olmo_model(model_path: str):
     from transformers import AutoModelForCausalLM, AutoTokenizer
 
-    from hf_olmo import OLMoForCausalLM  # noqa: F401
+    from hf_olmo import OLMoForCausalLM, OLMoTokenizerFast  # noqa: F401
 
     model = Olmo.from_checkpoint(model_path)
     hf_model = AutoModelForCausalLM.from_pretrained(model_path)
@@ -19,3 +21,24 @@ def test_olmo_model(model_path: str):
     hf_output = hf_model(input_tensor)
 
     torch.testing.assert_allclose(output.logits, hf_output.logits)
+
+
+def test_save_pretrained(model_path: str):
+    from transformers import AutoModelForCausalLM, AutoTokenizer
+
+    from hf_olmo import OLMoForCausalLM, OLMoTokenizerFast  # noqa: F401
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    input = tokenizer.encode("My name is OLMo!")
+    input_tensor = torch.tensor(input).unsqueeze(0)
+
+    hf_model = AutoModelForCausalLM.from_pretrained(model_path)
+    hf_output = hf_model(input_tensor)
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        hf_model.save_pretrained(tmp_dir)
+
+        saved_hf_model = AutoModelForCausalLM.from_pretrained(tmp_dir)
+        saved_hf_output = saved_hf_model(input_tensor)
+
+        torch.testing.assert_allclose(saved_hf_output.logits, hf_output.logits)
