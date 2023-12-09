9aca8e108dd3c74f650c3bfe19ea29b1cccee660
Akshita Bhagia <akshita23bhagia@gmail.com>
2023-12-08 16:11:25 -0800

    simplify tokenizer

--- hf_olmo/add_hf_config_to_olmo_checkpoint.py
+++ hf_olmo/add_hf_config_to_olmo_checkpoint.py
@@ -5,7 +5,7 @@ import shutil
 
 from hf_olmo.configuration_olmo import OLMoConfig
 from hf_olmo.tokenization_olmo_fast import OLMoTokenizerFast
-from olmo import ModelConfig
+from olmo import ModelConfig, Tokenizer
 
 logger = logging.getLogger(__name__)
 
@@ -25,11 +25,11 @@ def write_config(checkpoint_dir: str):
     logger.info(f"Saving HF-compatible config to {os.path.join(checkpoint_dir, 'config.json')}")
     config.save_pretrained(checkpoint_dir)
 
-    tokenizer = OLMoTokenizerFast.from_pretrained(checkpoint_dir)
-    tokenizer.save_pretrained(checkpoint_dir)
+    # tokenizer = OLMoTokenizerFast.from_pretrained(checkpoint_dir)
+    # tokenizer.save_pretrained(checkpoint_dir)
 
 
-def update_checkpoint_filename(checkpoint_dir: str, soft_link: bool = True):
+def write_model(checkpoint_dir: str, soft_link: bool = True):
     if soft_link:
         try:
             os.symlink("model.pt", os.path.join(checkpoint_dir, "pytorch_model.bin"))
@@ -38,6 +38,21 @@ def update_checkpoint_filename(checkpoint_dir: str, soft_link: bool = True):
     else:
         if not os.path.exists(os.path.join(checkpoint_dir, "pytorch_model.bin")):
             os.rename(os.path.join(checkpoint_dir, "model.pt"), os.path.join(checkpoint_dir, "pytorch_model.bin"))
+
+
+def write_tokenizer(checkpoint_dir: str):
+    tokenizer_raw = Tokenizer.from_checkpoint(checkpoint_dir)
+    tokenizer = OLMoTokenizerFast(
+        tokenizer_object=tokenizer_raw.base_tokenizer,
+        truncation=tokenizer_raw.truncate_direction,
+        max_length=tokenizer_raw.truncate_to,
+        eos_token=tokenizer_raw.decode([tokenizer_raw.eos_token_id], skip_special_tokens=False),
+    )
+    tokenizer.model_input_names = ["input_ids", "attention_mask"]
+    tokenizer.pad_token_id = tokenizer_raw.pad_token_id
+    tokenizer.eos_token_id = tokenizer_raw.eos_token_id
+
+    tokenizer.save_pretrained(checkpoint_dir)
 
 
 def download_remote_checkpoint_and_add_hf_config(checkpoint_dir: str, local_dir: str):
@@ -60,7 +75,8 @@ def download_remote_checkpoint_and_add_hf_config(checkpoint_dir: str, local_dir:
             logger.info(f"File already present at {final_location}")
 
     write_config(local_model_path)
-    update_checkpoint_filename(local_model_path, soft_link=False)
+    write_model(local_model_path, soft_link=False)
+    write_tokenizer(local_model_path)
     return local_model_path
 
 
@@ -83,7 +99,8 @@ def main():
 
     args = parser.parse_args()
     write_config(checkpoint_dir=args.checkpoint_dir)
-    update_checkpoint_filename(checkpoint_dir=args.checkpoint_dir, soft_link=not args.ignore_olmo_compatibility)
+    write_model(checkpoint_dir=args.checkpoint_dir, soft_link=not args.ignore_olmo_compatibility)
+    write_tokenizer(checkpoint_dir=args.checkpoint_dir)
 
 
 if __name__ == "__main__":
