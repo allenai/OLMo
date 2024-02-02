94b4539eb2e372c2dc52ea730a7f896b7e7bf482
epwalsh <petew@allenai.org>
2024-02-02 12:52:48 -0800

    fixes

--- scripts/combine_wandb_runs.py
+++ scripts/combine_wandb_runs.py
@@ -1,15 +1,11 @@
-from pathlib import Path
-
 import wandb
 from wandb.wandb_run import Run
 
-runs_path = Path("~/Downloads/OLMo Runs - mitchish-mcli.csv").expanduser()
 dst_entity = "ai2-llm"
 dst_project = "OLMo-7B"
-dst_group = "OLMo-7B"
-dst_run_name = "OLMo-7B"
 
 # Read CSV dump from "OLMo Runs" spreadsheet.
+#  runs_path = Path("~/Downloads/OLMo Runs - mitchish-mcli.csv").expanduser()
 #  run_paths_to_copy = []
 #  with runs_path.open("r") as f:
 #      reader = csv.DictReader(f, delimiter=",")
@@ -19,47 +15,47 @@ dst_run_name = "OLMo-7B"
 #      # Order oldest -> newest.
 #      run_paths_to_copy = list(reversed(run_paths_to_copy))
 
-run_paths_to_copy = [
-    "ai2-llm/olmo-medium/runs/wvc30anm",
-    "ai2-llm/olmo-medium/runs/uhy9bs35",
-    "ai2-llm/olmo-medium/runs/l6v218f4",
-    "ai2-llm/olmo-medium/runs/8fioq3qx",
-    "ai2-llm/olmo-medium/runs/mk9kaqh0",
-    "ai2-llm/olmo-medium/runs/49i87wpn",
-    "ai2-llm/olmo-medium/runs/0j2eqydw",
-    "ai2-llm/olmo-medium/runs/5wkmhkqh",
-    "ai2-llm/olmo-medium/runs/hrshlkzq",
-    "ai2-llm/olmo-medium/runs/eysi0t0y",
-    "ai2-llm/olmo-medium/runs/7gomworq",
-    "ai2-llm/olmo-medium/runs/lyij2l8m",
-    "ai2-llm/olmo-medium/runs/99euueq4",
-    "ai2-llm/olmo-medium/runs/fcn5q3zw",
-    "ai2-llm/olmo-medium/runs/j18wauyq",
-    "ai2-llm/olmo-medium/runs/jtfwv96r",
-    "ai2-llm/olmo-medium/runs/yuc5kl7s",
-    "ai2-llm/olmo-medium/runs/25urleov",
-    "ai2-llm/olmo-medium/runs/obde4w9j",
-    "ai2-llm/olmo-medium/runs/eaqax5ns",
-    "ai2-llm/olmo-medium/runs/cojbrc1o",
-    "ai2-llm/olmo-medium/runs/4xel5n7e",
-    "ai2-llm/olmo-medium/runs/jcs4c32w",
-    "ai2-llm/olmo-medium/runs/x55jyv7k",
-    "ai2-llm/olmo-medium/runs/yv7lgx0i",
-    "ai2-llm/olmo-medium/runs/11uf7gsv",
-    "ai2-llm/olmo-medium/runs/lds6zcog",
-    "ai2-llm/olmo-medium/runs/ho7jy4ey",
-    "ai2-llm/olmo-medium/runs/87shig0a",
-    "ai2-llm/olmo-medium/runs/x6zdcp5j",
-    "ai2-llm/olmo-medium/runs/olocmvn0",
-    "ai2-llm/olmo-medium/runs/xtruaap8",
-    "ai2-llm/olmo-medium/runs/2l070ogq",
-    "ai2-llm/olmo-medium/runs/uy2ydw12",
-    "ai2-llm/olmo-medium/runs/x23ciyv9",
-    "ai2-llm/olmo-medium/runs/67i5mdg0",
-    "ai2-llm/olmo-medium/runs/wrv46m83",
-    "ai2-llm/olmo-medium/runs/wd2gxrza",
-    "ai2-llm/olmo-medium/runs/z4z0x4m9",
-    "ai2-llm/olmo-medium/runs/p067ktg9",
+runs_to_copy = [
+    ("ai2-llm/olmo-medium/runs/wvc30anm", "OLMo-7B-run-001", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/uhy9bs35", "OLMo-7B-run-002", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/l6v218f4", "OLMo-7B-run-003", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/8fioq3qx", "OLMo-7B-run-004", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/mk9kaqh0", "OLMo-7B-run-005", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/49i87wpn", "OLMo-7B-run-006", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/0j2eqydw", "OLMo-7B-run-007", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/5wkmhkqh", "OLMo-7B-run-008", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/hrshlkzq", "OLMo-7B-run-009", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/eysi0t0y", "OLMo-7B-run-010", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/7gomworq", "OLMo-7B-run-011", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/lyij2l8m", "OLMo-7B-run-012", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/99euueq4", "OLMo-7B-run-013", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/fcn5q3zw", "OLMo-7B-run-014", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/j18wauyq", "OLMo-7B-run-015", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/jtfwv96r", "OLMo-7B-run-016", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/yuc5kl7s", "OLMo-7B-run-017", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/25urleov", "OLMo-7B-run-018", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/obde4w9j", "OLMo-7B-run-019", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/eaqax5ns", "OLMo-7B-run-020", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/cojbrc1o", "OLMo-7B-run-021", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/4xel5n7e", "OLMo-7B-run-022", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/jcs4c32w", "OLMo-7B-run-023", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/x55jyv7k", "OLMo-7B-run-024", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/yv7lgx0i", "OLMo-7B-run-025", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/11uf7gsv", "OLMo-7B-run-026", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/lds6zcog", "OLMo-7B-run-027", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/ho7jy4ey", "OLMo-7B-run-028", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/87shig0a", "OLMo-7B-run-029", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/x6zdcp5j", "OLMo-7B-run-030", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/olocmvn0", "OLMo-7B-run-031", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/xtruaap8", "OLMo-7B-run-032", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/2l070ogq", "OLMo-7B-run-033", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/uy2ydw12", "OLMo-7B-run-034", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/x23ciyv9", "OLMo-7B-run-035", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/67i5mdg0", "OLMo-7B-run-036", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/wrv46m83", "OLMo-7B-run-037", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/wd2gxrza", "OLMo-7B-run-038", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/z4z0x4m9", "OLMo-7B-run-039", "OLMo-7B"),
+    ("ai2-llm/olmo-medium/runs/p067ktg9", "OLMo-7B-Tulu", "OLMo-7B-Tulu"),
 ]
 
 # Set your API key
@@ -69,28 +65,30 @@ wandb.login()
 api = wandb.Api()
 
 # Iterate through the runs and copy them to the destination project
-for i, run_path in enumerate(run_paths_to_copy):
+for run_path, new_run_name, new_run_group in runs_to_copy:
     run = api.run(run_path)
 
-    print(f"Copying run {i+1} of {len(run_paths_to_copy)}")
+    print(f"Copying run '{run_path}' to '{new_run_name}'...")
 
     # Get the run history and files
-    history = run.history()
+    history = run.scan_history()
 
     # Create a new run in the destination project
     new_run = wandb.init(
         project=dst_project,
         entity=dst_entity,
         config=run.config,
-        name=f"{dst_run_name}-run-{i+1:03d}",
+        name=new_run_name,
         resume="allow",
-        group=dst_group,
+        group=new_run_group,
+        settings=wandb.Settings(_disable_stats=True),
     )
     assert isinstance(new_run, Run)
 
     # Log the history to the new run
-    for index, row in history.iterrows():
-        new_run.log(row.to_dict())
+    for data in history:
+        step = data.pop("_step")
+        new_run.log(data, step=step)
 
     # Finish the new run
     new_run.finish()
