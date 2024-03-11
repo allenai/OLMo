aebfaeb6de604b3b199f7eb8f24e107b89683c4d
Luca Soldaini <lucas@allenai.org>
2024-03-10 17:46:52 -0700

    script

--- scripts/beaker/olmo1-ablation.sh
+++ scripts/beaker/olmo1-ablation.sh
@@ -28,38 +28,44 @@ RUN_HASH=$(echo "${LOAD_PATH_ARG}-${CONFIG_PATH}" | md5sum | cut -c 1-8)
 # compose the two
 FULL_RUN_NAME="${RUN_NAME}-${RUN_HASH}"
 
-# check if there is an env var called 'WANDB_API_KEY' and if so, create a flag
-# to pass to gantry
-if [ -z ${WANDB_API_KEY+x} ]; then
-  WANDB_API_KEY_ARG="--env-secret WANDB_API_KEY=WANDB_API_KEY"
-else
-  WANDB_API_KEY_ARG="--env WANDB_API_KEY=${WANDB_API_KEY}"
-fi
+# # check if there is an env var called 'WANDB_API_KEY' and if so, create a flag
+# # to pass to gantry
+# if [ -z ${WANDB_API_KEY+x} ]; then
+#   WANDB_API_KEY_ARG="--env-secret WANDB_API_KEY=WANDB_API_KEY"
+# else
+#   WANDB_API_KEY_ARG="--env WANDB_API_KEY=${WANDB_API_KEY}"
+# fi
 
 NUM_NODES=3
 MORE_ARGS='--model.flash_attention=true --fsdp.wrapping_strategy=by_block_and_size --fsdp.sharding_strategy=SHARD_GRAD_OP --save_folder=runs/ --device_train_microbatch_size=12'
 
+# --workspace ai2/llm-testing \
 
 gantry run \
-  --workspace ai2/llm-testing \
+  --allow-dirty \
   --task-name "${FULL_RUN_NAME}" \
   --description "${FULL_RUN_NAME}" \
-  --allow-dirty \
   --priority high \
-  --beaker-image olmo-torch2-gantry \
+  --beaker-image petew/olmo-torch2-gantry \
   --cluster ai2/pluto-cirrascale \
   --gpus 8 \
-  --replicas ${NUM_NODES} \
-  --leader-selection  \
+  --replicas "${NUM_NODES}" \
+  --leader-selection \
   --host-networking \
   --nfs \
-  ${WANDB_API_KEY_ARG} \
+  --mount /net/nfs.cirrascale/allennlp/petew/cache:/root/.cache \
+  --budget ai2/oe-data \
+
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
+  --env OLMO_TASK=model \
+  --env-secret WANDB_API_KEY=WANDB_API_KEY \
+  --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
+  --env-secret AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY \
   --shared-memory 10GiB \
   --venv base \
   --yes \
-  -- /bin/bash -c "source scripts/beaker/warm_hf_cache.sh && torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 8 --rdzv_id=101 --rdzv_backend=c10d --rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400 scripts/train.py ${CONFIG_PATH} --run_name=${FULL_RUN_NAME} ${LOAD_PATH_ARG} --device_train_microbatch_size=8 ${@}"
+  -- /bin/bash -c "source scripts/beaker/warm_hf_cache.sh && torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 8 --rdzv_id=101 --rdzv_backend=c10d --rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400 scripts/train.py ${CONFIG_PATH} --run_name=${FULL_RUN_NAME} ${LOAD_PATH_ARG} ${MORE_ARGS} ${@}"
 #!/usr/bin/env bash
 
 set -ex
