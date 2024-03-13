01a9213da1846d0ac13d039c3197a6490f11e2a8
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-03-13 17:42:17 +0000

    updated beaker script

--- scripts/beaker/qkv-mitch-ish-7b.sh
+++ scripts/beaker/qkv-mitch-ish-7b.sh
@@ -3,14 +3,14 @@
 set -ex
 
 CONFIG_PATH=configs/qkv-v1_5-mix-medium-mitch-ish-s3.yaml
-NUM_NODES=4
-ARGS='--run_name=olmo7-ablation-qkv-clip-8 --wandb.name=qkv-clip --model.flash_attention=true --fsdp.wrapping_strategy=by_block_and_size --fsdp.sharding_strategy=SHARD_GRAD_OP --save_folder=runs/ --device_train_microbatch_size=3 --global_train_batch_size=6144 --wandb.group=qkv-clip --remote_save_folder=s3://ai2-llm/checkpoints/olmo7-ablation/qkv-clip --load-path=r2://olmo-checkpoints/ai2-llm/olmo-medium/vpxr4bil/step81000-unsharded/'
+NUM_NODES=8
+ARGS='--run_name=olmo7-ablation-qkv-clip-beaker --wandb.name=qkv-clip-beaker --model.flash_attention=true --fsdp.wrapping_strategy=by_block_and_size --fsdp.sharding_strategy=SHARD_GRAD_OP --save_folder=runs/ --device_train_microbatch_size=3 --global_train_batch_size=6144 --wandb.group=qkv-clip-beaker --remote_save_folder=s3://ai2-llm/checkpoints/olmo7-ablation/qkv-clip-beaker --load-path=r2://olmo-checkpoints/ai2-llm/olmo-medium/vpxr4bil/step81000-unsharded/'
 
 gantry run \
   --allow-dirty \
   --workspace ai2/llm-testing \
-  --task-name qkv-mitchish-7b \
-  --description qkv-mitchish-7b \
+  --task-name olmo7-ablation-qkv-clip-beaker \
+  --description olmo7-ablation-qkv-clip-beaker \
   --priority high \
   --beaker-image olmo-torch2-gantry \
   --cluster ai2/pluto-cirrascale \
@@ -33,4 +33,4 @@ gantry run \
   --shared-memory 10GiB \
   --venv base \
   --yes \
-  -- /bin/bash -c "torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 8 --rdzv_id=101 --rdzv_backend=c10d --rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400 scripts/train.py ${CONFIG_PATH} ${ARGS}"
+  -- /bin/bash -c "source scripts/beaker/warm_hf_cache.sh && torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 8 --rdzv_id=101 --rdzv_backend=c10d --rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400 scripts/train.py ${CONFIG_PATH} ${ARGS}"
