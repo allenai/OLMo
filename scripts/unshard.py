6a97372845b67788c2c27411793cd8e679d3f4ea
epwalsh <petew@allenai.org>
2024-03-18 15:29:18 -0700

    Add `--safe-tensors` option to `unshard.py`

--- scripts/unshard.py
+++ scripts/unshard.py
@@ -11,6 +11,7 @@ from olmo.checkpoint import (
     TorchLegacyShardedCheckpointer,
 )
 from olmo.config import ShardedCheckpointerType, TrainConfig
+from olmo.safetensors_util import state_dict_to_safetensors_file
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +21,7 @@ def main(
     output_dir: Union[str, Path],
     sharded_checkpoint_type: ShardedCheckpointerType = ShardedCheckpointerType.torch_legacy,
     model_only: bool = False,
+    safe_tensors: bool = False,
 ) -> None:
     if isinstance(input_dir, str):
         input_dir = Path(input_dir)
@@ -45,14 +47,22 @@ def main(
     # model
     model_output = str(output_dir / "model.pt")
     logger.info("Saving model state to %s", model_output)
-    torch.save(model_state_dict, model_output)
+    if safe_tensors:
+        state_dict_to_safetensors_file(model_state_dict, model_output)
+    else:
+        torch.save(model_state_dict, model_output)
     del model_state_dict
 
     if not model_only:
+        assert optim_state_dict is not None
+
         # optimizer
         optim_output = str(output_dir / "optim.pt")
         logger.info("Saving optimizer state to %s", optim_output)
-        torch.save(optim_state_dict, optim_output)
+        if safe_tensors:
+            state_dict_to_safetensors_file(optim_state_dict, optim_output)
+        else:
+            torch.save(optim_state_dict, optim_output)
         del optim_state_dict
 
         # trainer
@@ -81,6 +91,10 @@ if __name__ == "__main__":
         "--model-only",
         action="store_true",
     )
+    parser.add_argument(
+        "--safe-tensors",
+        action="store_true",
+    )
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
@@ -89,4 +103,5 @@ if __name__ == "__main__":
         args.output_dir,
         sharded_checkpoint_type=args.type,
         model_only=args.model_only,
+        safe_tensors=args.safe_tensors,
     )
