d7974a3fc3ff044c2b9fdf04515fc683ba917f27
epwalsh <petew@allenai.org>
2024-03-19 13:37:05 -0700

    put things on CPU!

--- olmo/checkpoint.py
+++ olmo/checkpoint.py
@@ -196,6 +196,12 @@ def load_fsdp_model_and_optim_state(
             ),
         )
         del model_state
+
+        # Make sure tensors are on CPU.
+        for state in optim_state["state"].values():
+            for k in state.keys():
+                state[k] = state[k].cpu()
+
         torch.cuda.empty_cache()
         load_fsdp_optim_state(fsdp_model, optim, optim_state["optim"])
 
