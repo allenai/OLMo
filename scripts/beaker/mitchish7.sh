6bbf4569e0f0daf7303cbd42fef77bc7b324d748
Dirk Groeneveld <dirkg@allenai.org>
2024-03-19 17:42:40 -0700

    Microbatch of 2

--- scripts/beaker/mitchish7.sh
+++ scripts/beaker/mitchish7.sh
@@ -4,7 +4,7 @@ set -ex
 
 CONFIG_PATH=configs/mitchish7-s3.yaml
 NUM_NODES=8
-ARGS='--run_name=mitchish7 --wandb.name=mitchish7 --model.flash_attention=true --fsdp.wrapping_strategy=by_block_and_size --fsdp.sharding_strategy=SHARD_GRAD_OP --save_folder=runs/ --device_train_microbatch_size=3 --global_train_batch_size=1536'
+ARGS='--run_name=mitchish7 --wandb.name=mitchish7 --model.flash_attention=true --fsdp.wrapping_strategy=by_block_and_size --fsdp.sharding_strategy=SHARD_GRAD_OP --save_folder=runs/ --device_train_microbatch_size=2 --global_train_batch_size=1024'
 
 gantry run \
   --workspace ai2/dirkg \
