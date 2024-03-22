bb37a7bdd7d9377dc2d1642e54adfdd2c3368f46
Dirk Groeneveld <dirkg@allenai.org>
2024-03-22 16:10:19 -0700

    Yes I know how tar works. Why are you asking?

--- scripts/beaker/warm_hf_cache.sh
+++ scripts/beaker/warm_hf_cache.sh
@@ -4,6 +4,6 @@ set -ex
 
 mkdir -p /root/.cache
 pushd /root/.cache
-curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v2.tar.gz" | tar -xzf --keep-newer-files -
+curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v2.tar.gz" | tar --keep-newer-files -xzf -
 popd
 export HF_DATASETS_OFFLINE=1
