746f11cfabc808cd8d192bb44ef9f8713deb2cb3
Dirk Groeneveld <dirkg@allenai.org>
2024-03-22 15:32:50 -0700

    Load from checkpoint

--- scripts/beaker/mitchish7.sh
+++ scripts/beaker/mitchish7.sh
@@ -33,4 +33,5 @@ torchrun \
     --fused_loss=true \
     --device_train_microbatch_size=2 \
     --global_train_batch_size=1024 \
-    --save_overwrite
+    --save_overwrite \
+    --load_path=s3://ai2-llm/checkpoints/OLMo-medium/mitchish7/step14200/
