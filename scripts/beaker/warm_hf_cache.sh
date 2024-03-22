6190cbca3ca952d5b480e935e6758d3d31c3519d
Dirk Groeneveld <dirkg@allenai.org>
2024-03-22 16:08:12 -0700

    Make tar more robust

--- scripts/beaker/warm_hf_cache.sh
+++ scripts/beaker/warm_hf_cache.sh
@@ -4,6 +4,6 @@ set -ex
 
 mkdir -p /root/.cache
 pushd /root/.cache
-curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v2.tar.gz" | tar -xzf -
+curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v2.tar.gz" | tar -xzf --keep-newer-files -
 popd
 export HF_DATASETS_OFFLINE=1
