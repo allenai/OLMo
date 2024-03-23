485c71df186963d5e84712ec39c7f04b2608e2cf
Dirk Groeneveld <dirkg@allenai.org>
2024-03-22 22:02:02 -0700

    I that did wrong.

--- scripts/beaker/mitchish7.sh
+++ scripts/beaker/mitchish7.sh
@@ -34,4 +34,4 @@ torchrun \
     --device_train_microbatch_size=2 \
     --global_train_batch_size=1024 \
     --save_overwrite \
-    '--load_path=${path.last_checkpoint:${s3://ai2-llm/checkpoints/OLMo-medium/mitchish7/}}'
+    '--load_path=${path.last_checkpoint:s3://ai2-llm/checkpoints/OLMo-medium/mitchish7/}'
