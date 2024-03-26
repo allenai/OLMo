0472b2978f4c56f7dca18a929392d41bfc05e410
David Wadden <davidw@ip-192-168-0-13.us-west-2.compute.internal>
2024-03-25 21:00:51 -0700

    Kick off annealing run that goes for 50B steps.

---
+++ scripts/beaker/annealing/v0-step_1.5T-warmup_true-steps_50B.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CONFIG_PATH=configs/annealing/v0-step_1.5T-warmup_true-steps_50B.yaml
+NUM_NODES=3
+
+gantry run \
+  --allow-dirty \
+  --workspace ai2/davidw-oe-annealing \
+  --task-name v0-step_1.5T-warmup_true-steps_50B \
+  --description v0-step_1.5T-warmup_true-steps_50B \
+  --priority normal \
+  --beaker-image petew/olmo-torch2-gantry \
+  --cluster ai2/pluto-cirrascale \
+  --gpus 8 \
+  --replicas "${NUM_NODES}" \
+  --leader-selection \
+  --host-networking \
+  --nfs \
+  --mount /net/nfs.cirrascale/allennlp/petew/cache:/root/.cache \
+  --budget ai2/oe-training \
+  --env LOG_FILTER_TYPE=local_rank0_only \
+  --env OMP_NUM_THREADS=8 \
+  --env OLMO_TASK=model \
+  --env-secret WANDB_API_KEY=WANDB_API_KEY \
+  --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
+  --env-secret AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY \
+  --env-secret R2_ACCESS_KEY_ID=R2_ACCESS_KEY_ID \
+  --env-secret R2_SECRET_ACCESS_KEY=R2_SECRET_ACCESS_KEY \
+  --env-secret R2_ENDPOINT_URL=R2_ENDPOINT_URL \
+  --shared-memory 10GiB \
+  --venv base \
+  --yes \
+  -- /bin/bash -c "source scripts/beaker/warm_hf_cache.sh && torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 8 --rdzv_id=101 --rdzv_backend=c10d --rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400 scripts/train.py ${CONFIG_PATH}"
