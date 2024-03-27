fd7c434a902bffe7b4ee5d754c99993780e3c776
Dirk Groeneveld <dirkg@allenai.org>
2024-03-27 16:45:32 -0700

    New HF cache

--- scripts/beaker/mitchish70.sh
+++ scripts/beaker/mitchish70.sh
@@ -11,7 +11,7 @@ shift
 # Warm HF cache
 mkdir -p /root/.cache
 pushd /root/.cache
-curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v2.tar.gz" | tar --keep-newer-files -xzf -
+curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -
 popd
 export HF_DATASETS_OFFLINE=1
 
