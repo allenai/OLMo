1c129802ce0a304d0e5db516c98cce7b3537f4ec
Pete <epwalsh10@gmail.com>
2024-04-01 20:21:13 -0700

    make garbage collection interval configurable (#533)
    Co-authored-by: Shane A <shanea@allenai.org>

--- olmo/config.py
+++ olmo/config.py
@@ -987,6 +987,12 @@ class TrainConfig(BaseConfig):
     How often to log to the console.
     """
 
+    gen1_gc_interval: Optional[int] = 1
+    """
+    How often (in steps) to run generation 1 garbage collection.
+    Set to ``None`` to use automatic garbage collection (i.e. we don't mess with it).
+    """
+
     compile: Optional[CompilerConfig] = None
     """
     Settings for compiling the model with ``torch.compile()``.
