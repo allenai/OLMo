19d2d73604ee930388ed42490ff13aa77fbeb22e
Shane A <shanea@allenai.org>
2024-04-03 18:49:05 -0700

    Add beaker config for 1b const lr run

---
+++ scripts/beaker/lr-scheduling.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+IFS=$'\n\t'
+
+BEAKER_LEADER_REPLICA_HOSTNAME=$1
+shift
+
+NUM_NODES=$1
+shift
+
+# Warm HF cache
+mkdir -p /root/.cache
+pushd /root/.cache
+curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -
+popd
+export HF_DATASETS_OFFLINE=1
+
+torchrun \
+  --nnodes ${NUM_NODES}:${NUM_NODES} \
+  --nproc-per-node 8 \
+  --rdzv_id=101 \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  scripts/train.py \
+    configs/lr-scheduling-s3.yaml \
+      --run_name=lr-schedule-const-lr \
+      --fsdp.sharding_strategy=SHARD_GRAD_OP \
+      '--load_path=${path.last_checkpoint:${remote_save_folder}}' \
+      --save_overwrite
