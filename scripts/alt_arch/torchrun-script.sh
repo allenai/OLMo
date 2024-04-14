a0aee46171eea4fc2bdfabab2c8495bcc6cc0583
Ananya Harsh Jha <ananyahjha93@gmail.com>
2024-04-13 20:41:22 -0700

    baseline gantry

---
+++ scripts/alt_arch/torchrun-script.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+IFS=$'\n\t'
+
+BEAKER_LEADER_REPLICA_HOSTNAME=$1
+shift
+
+NUM_NODES=$1
+shift
+
+# Warm HF cache
+mkdir -p /root/.cache
+pushd /root/.cache
+curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -
+popd
+export HF_DATASETS_OFFLINE=1
+
+torchrun \
+  --nnodes ${NUM_NODES}:${NUM_NODES} \
+  --nproc-per-node 8 \
+  --rdzv_id=101 \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  scripts/train.py \
+    configs/alt_arch/tranformer_base-300M.yaml \
+      --run_name=mqa-transformer-300M-baseline \
+      --device_train_microbatch_size=128 \
+      --save_folder=/net/nfs/allennlp/ananyaj/alt_arch/checkpoints \
+      --save_overwrite
