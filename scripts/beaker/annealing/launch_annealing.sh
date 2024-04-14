d0bf954b1e18a0ee378d365d7ef15094f449bc28
dwadden <davidw@allenai.org>
2024-04-14 05:37:07 +0000

    Launch annealing run with downweighted flan.

--- scripts/beaker/annealing/launch_annealing.sh
+++ scripts/beaker/annealing/launch_annealing.sh
@@ -47,4 +47,5 @@ function launch {
 
 # Launch runs.
 
-launch v1.7-step_2T-resume_optimizer-steps_50B 8 ai2/pluto-cirrascale
+# launch v1.7-step_2T-resume_optimizer-steps_50B 8 ai2/pluto-cirrascale
+launch v1.7-step_2T-resume_optimizer-steps_50B-flan_downweight 8 ai2/pluto-cirrascale
