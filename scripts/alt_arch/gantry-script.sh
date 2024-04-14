3b74eb2a93f8d357264a0f2cc7019ec6859eb032
Ananya Harsh Jha <ananyahjha93@gmail.com>
2024-04-13 20:52:08 -0700

    .

--- scripts/alt_arch/gantry-script.sh
+++ scripts/alt_arch/gantry-script.sh
@@ -17,7 +17,7 @@ gantry run \
   --host-networking \
   --budget ai2/oe-training \
   --nfs \
-  --mount /net/nfs.cirrascale/allennlp/ananyaj/cache:/root/.cache \
+  --mount /net/nfs.cirrascale/allennlp/shanea/cache:/root/.cache \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
