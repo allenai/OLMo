710f38c5cd255972611f1449585bc76d11ae50e8
Shane A <shanea@allenai.org>
2024-04-19 16:33:12 -0700

    Use sharded checkpoint

--- scripts/beaker/lr-scheduling-7b.sh
+++ scripts/beaker/lr-scheduling-7b.sh
@@ -44,4 +44,4 @@ torchrun \
     --device_train_microbatch_size=2 \
     --global_train_batch_size=1024 \
     --save_overwrite \
-    '--load_path=s3://ai2-llm/checkpoints/OLMo-medium/mitchish7/step358000-unsharded'
+    '--load_path=s3://ai2-llm/checkpoints/OLMo-medium/mitchish7/step358000'
