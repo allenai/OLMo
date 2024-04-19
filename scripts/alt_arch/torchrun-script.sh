ca4e740ed1f32241268425eb22370c29af52b5e3
Ananya Harsh Jha <ananyahjha93@gmail.com>
2024-04-19 14:40:10 -0700

    7B mqa transformer

--- scripts/alt_arch/torchrun-script.sh
+++ scripts/alt_arch/torchrun-script.sh
@@ -15,15 +15,29 @@ curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" 
 popd
 export HF_DATASETS_OFFLINE=1
 
+# 300M mamba or olmo
+torchrun \
+  # --nnodes ${NUM_NODES}:${NUM_NODES} \
+  # --nproc-per-node 8 \
+  # --rdzv_id=101 \
+  # --rdzv_backend=c10d \
+  # --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  # scripts/train_alt-arch.py \
+  #   configs/alt_arch/mamba-300M.yaml \
+  #     --run_name=mamba-300M-baseline \
+  #     --device_train_microbatch_size=16 \
+  #     --fsdp.sharding_strategy=SHARD_GRAD_OP \
+  #     --save_overwrite
+
+# 7B mamba or olmo
 torchrun \
   --nnodes ${NUM_NODES}:${NUM_NODES} \
   --nproc-per-node 8 \
   --rdzv_id=101 \
   --rdzv_backend=c10d \
   --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
-  scripts/train_alt-arch.py \
-    configs/alt_arch/mamba-300M.yaml \
-      --run_name=mamba-300M-baseline \
-      --device_train_microbatch_size=16 \
-      --fsdp.sharding_strategy=SHARD_GRAD_OP \
+  scripts/train.py \
+    configs/alt_arch/transformer_base-7B.yaml \
+      --run_name=olmo-7B-baseline \
+      --device_train_microbatch_size=1 \
       --save_overwrite
