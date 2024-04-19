92f0f41f6286cc375083a35df066603083d6100a
Shane A <shanea@allenai.org>
2024-04-18 17:07:52 -0700

    Fix load path for first 7B run

--- scripts/beaker/lr-scheduling-7b.sh
+++ scripts/beaker/lr-scheduling-7b.sh
@@ -34,4 +34,4 @@ torchrun \
     --device_train_microbatch_size=2 \
     --global_train_batch_size=1024 \
     --save_overwrite \
-    '--load_path=${path.last_checkpoint:s3://ai2-llm/checkpoints/7b/lr-schedule-const-lr-7B/}'
+    '--load_path=s3://ai2-llm/checkpoints/OLMo-medium/mitchish7/step358000-unsharded'
