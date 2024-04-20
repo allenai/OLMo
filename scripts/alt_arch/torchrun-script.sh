54e6d115329152c05af62a2926246e436b6adb3f
Ananya Harsh Jha <ananyahjha93@gmail.com>
2024-04-19 17:07:20 -0700

    mamba 7B benchmark

--- scripts/alt_arch/torchrun-script.sh
+++ scripts/alt_arch/torchrun-script.sh
@@ -16,6 +16,20 @@ popd
 export HF_DATASETS_OFFLINE=1
 
 # 300M mamba or olmo
+# torchrun \
+#   --nnodes ${NUM_NODES}:${NUM_NODES} \
+#   --nproc-per-node 8 \
+#   --rdzv_id=101 \
+#   --rdzv_backend=c10d \
+#   --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+#   scripts/train_alt-arch.py \
+#     configs/alt_arch/mamba-300M.yaml \
+#       --run_name=mamba-300M-baseline \
+#       --device_train_microbatch_size=8 \
+#       --fsdp.sharding_strategy=SHARD_GRAD_OP \
+#       --save_overwrite
+
+# 7B mamba or olmo
 torchrun \
   --nnodes ${NUM_NODES}:${NUM_NODES} \
   --nproc-per-node 8 \
@@ -23,21 +37,7 @@ torchrun \
   --rdzv_backend=c10d \
   --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
   scripts/train_alt-arch.py \
-    configs/alt_arch/mamba-300M.yaml \
-      --run_name=mamba-300M-baseline \
-      --device_train_microbatch_size=8 \
-      --fsdp.sharding_strategy=SHARD_GRAD_OP \
+    configs/alt_arch/mamba-7B.yaml \
+      --run_name=mamba-7B \
+      --device_train_microbatch_size=2 \
       --save_overwrite
-
-# 7B mamba or olmo
-# torchrun \
-#   --nnodes ${NUM_NODES}:${NUM_NODES} \
-#   --nproc-per-node 8 \
-#   --rdzv_id=101 \
-#   --rdzv_backend=c10d \
-#   --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
-#   scripts/train.py \
-#     configs/alt_arch/transformer_base-7B.yaml \
-#       --run_name=olmo-7B-baseline \
-#       --device_train_microbatch_size=2 \
-#       --save_overwrite
