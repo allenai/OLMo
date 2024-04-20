521304b1e1ffdf6f01aa86f2a771eb0bc24526d8
Shane A <shanea@allenai.org>
2024-04-20 07:59:08 -0700

    Revert "Revert "Try new docker imaage""
    This reverts commit 9e69a90b6d407f87264e39b9c2e48840f0094231.

--- scripts/beaker/lr-scheduling-7b.sh
+++ scripts/beaker/lr-scheduling-7b.sh
@@ -18,16 +18,16 @@ export HF_DATASETS_OFFLINE=1
 # Disabling infiniband for jupiter as temp workaround
 # export NCCL_IB_DISABLE=1
 
-# Install OLMo-core
-mkdir -p ~/.ssh
-printenv SSH_KEY > ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519 | wc
-ssh -T git@github.com
-git clone git@github.com:allenai/OLMo-core.git
-cd OLMo-core
-pip install .[all]
-cd ..
-rm -rf OLMo-core
+# # Install OLMo-core
+# mkdir -p ~/.ssh
+# printenv SSH_KEY > ~/.ssh/id_ed25519
+# cat ~/.ssh/id_ed25519 | wc
+# ssh -T git@github.com
+# git clone git@github.com:allenai/OLMo-core.git
+# cd OLMo-core
+# pip install .[all]
+# cd ..
+# rm -rf OLMo-core
 
 torchrun \
   --nnodes ${NUM_NODES}:${NUM_NODES} \
