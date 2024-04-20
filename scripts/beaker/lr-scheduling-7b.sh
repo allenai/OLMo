0d8f10f5f065aad811014934f51c1e2956294ccd
Shane A <shanea@allenai.org>
2024-04-19 22:31:58 -0700

    Print statements

--- scripts/beaker/lr-scheduling-7b.sh
+++ scripts/beaker/lr-scheduling-7b.sh
@@ -21,7 +21,7 @@ export HF_DATASETS_OFFLINE=1
 # Install OLMo-core
 mkdir -p ~/.ssh
 printenv SSH_KEY > ~/.ssh/id_ed25519
-ls ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519 | wc
 ssh -T git@github.com
 git clone git@github.com:allenai/OLMo-core.git
 cd OLMo-core
