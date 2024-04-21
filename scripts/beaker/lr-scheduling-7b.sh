c9150faf3beb1611d3d610863437a8324d401d6f
Shane A <shanea@allenai.org>
2024-04-21 12:46:28 -0700

    Update load path

--- scripts/beaker/lr-scheduling-7b.sh
+++ scripts/beaker/lr-scheduling-7b.sh
@@ -48,4 +48,4 @@ torchrun \
     --device_train_microbatch_size=2 \
     --global_train_batch_size=1024 \
     --save_overwrite \
-    '--load_path=s3://ai2-llm/checkpoints/OLMo-medium/mitchish7/step358000'
+    '--load_path=${path.last_checkpoint:s3://ai2-llm/checkpoints/7b/lr-schedule-const-lr-7B/}'
