e14538067a436619553999b2ba2afa0831299e1a
Ananya Harsh Jha <ananyahjha93@gmail.com>
2024-04-22 14:27:05 -0700

    .

--- scripts/alt_arch/torchrun-script.sh
+++ scripts/alt_arch/torchrun-script.sh
@@ -38,7 +38,7 @@ torchrun \
   --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
   scripts/train_alt-arch.py \
     configs/alt_arch/mamba-300M.yaml \
-      --run_name=mamba-300M-bf16-param-layernorm-baseline \
+      --run_name=mamba-300M-bf16-residual-param-layernorm \
       --device_train_microbatch_size=8 \
       --fsdp.sharding_strategy=SHARD_GRAD_OP \
       --save_overwrite
