9a64fb209f82ed46114d5b82b1b77e43539bcefb
Dirk Groeneveld <dirkg@allenai.org>
2024-04-24 17:12:44 -0700

    Get rid of progress bars that don't progress

--- scripts/wandb_to_csv.py
+++ scripts/wandb_to_csv.py
@@ -12,6 +12,7 @@ def dump_run(run, output_file: Path):
     # open output file and check what's already there
     data: Dict[int, Dict[str, Any]] = {}
     if output_file.is_file():
+        log.info("Reading existing info for %s from %s", run.path[-1], output_file)
         f = output_file.open("r+", encoding="UTF-8")
         lines = iter(f)
         try:
@@ -27,10 +28,13 @@ def dump_run(run, output_file: Path):
                     column_name: line[i]
                     for i, column_name in enumerate(columns)
                 }
+        log.info("Found %d entries in %s", len(data), output_file)
     else:
         f = output_file.open("x", encoding="UTF-8")
 
     def write_out_results():
+        log.info("Writing %d entries to %s", len(data), output_file)
+
         f.seek(0)
 
         # find columns
@@ -48,13 +52,12 @@ def dump_run(run, output_file: Path):
         f.write("\t".join(columns))
         f.write("\n")
 
-        with click.progressbar(ordered_data) as bar:
-            for step, values in bar:
-                line_values = (values.get(column) for column in columns)
-                line_values = (str(v) if v is not None else "" for v in line_values)
-                line = "\t".join(line_values)
-                line += "\n"
-                f.write(line)
+        for step, values in ordered_data:
+            line_values = (values.get(column) for column in columns)
+            line_values = (str(v) if v is not None else "" for v in line_values)
+            line = "\t".join(line_values)
+            line += "\n"
+            f.write(line)
 
     # scan ranges of steps
     current_step = 0
@@ -62,20 +65,20 @@ def dump_run(run, output_file: Path):
     while True:
         while current_step in data.keys():
             current_step += 1
-        with click.progressbar(run.scan_history(min_step=current_step, page_size=10000)) as bar:
-            for s in bar:
-                step = s["_step"]
-                if step in data:
-                    break
-                data[step] = s
-                current_step = max(current_step, step)
-            else:
+        log.info("Downloading %s starting with step %d", run.path[-1], current_step)
+        for s in run.scan_history(min_step=current_step, page_size=100000):
+            step = s["_step"]
+            if step in data:
                 break
+            data[step] = s
+            current_step = max(current_step, step)
+        else:
+            break
 
-            # If we've been reading for 10 minutes, write out results
-            if time.time() - last_write > 60 * 10:
-                write_out_results()
-                last_write = time.time()
+        # If we've been reading for 10 minutes, write out results
+        if time.time() - last_write > 60 * 10:
+            write_out_results()
+            last_write = time.time()
 
     write_out_results()
 
