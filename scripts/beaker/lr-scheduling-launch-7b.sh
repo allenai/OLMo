71e43f37884c8964bf2c732b1fb0d2e71ba2ab82
Shane A <shanea@allenai.org>
2024-04-25 14:33:59 -0700

    Remove error propagation

--- scripts/beaker/lr-scheduling-launch-7b.sh
+++ scripts/beaker/lr-scheduling-launch-7b.sh
@@ -15,7 +15,6 @@ gantry run \
   --replicas "${NUM_NODES}" \
   --leader-selection \
   --host-networking \
-  --propagate-failure \
   --budget ai2/oe-training \
   --no-nfs \
   --env LOG_FILTER_TYPE=local_rank0_only \
