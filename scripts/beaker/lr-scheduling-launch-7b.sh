a2ecf29093f02ad048e0953b76a44ebb49b65259
Shane A <shanea@allenai.org>
2024-04-29 11:18:48 -0700

    Pass node rank to inner 7B job

--- scripts/beaker/lr-scheduling-launch-7b.sh
+++ scripts/beaker/lr-scheduling-launch-7b.sh
@@ -34,4 +34,4 @@ gantry run \
   --venv base \
   --yes \
   --timeout=-1 \
-  -- /bin/bash -c "scripts/beaker/lr-scheduling-7b.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES}"
+  -- /bin/bash -c "scripts/beaker/lr-scheduling-7b.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
