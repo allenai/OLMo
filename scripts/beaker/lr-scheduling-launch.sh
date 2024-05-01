1cabbbff2ea431724096a1cb72d9a702eb012314
Shane A <shanea@allenai.org>
2024-05-01 15:20:30 -0700

    Use synchronized start for 1B

--- scripts/beaker/lr-scheduling-launch.sh
+++ scripts/beaker/lr-scheduling-launch.sh
@@ -16,7 +16,7 @@ gantry run \
   --leader-selection \
   --host-networking \
   --budget ai2/oe-training \
-  --propagate-failure \
+  --synchronized-start-timeout "10m" \
   --no-nfs \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
