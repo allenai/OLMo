ed5a18ca520dffceffad72ea9422943c80d56a27
Dirk Groeneveld <dirkg@allenai.org>
2024-05-01 10:53:39 -0700

    Try the old rdzv

--- scripts/beaker/mitchish7-llamainit.sh
+++ scripts/beaker/mitchish7-llamainit.sh
@@ -22,9 +22,8 @@ torchrun \
   --nnodes ${NUM_NODES}:${NUM_NODES} \
   --nproc-per-node 8 \
   --rdzv_id=12346 \
-  --rdzv_backend=static \
+  --rdzv_backend=c10d \
   --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
-  --node_rank=$BEAKER_REPLICA_RANK \
   --rdzv_conf="read_timeout=420" \
   scripts/train.py \
   configs/mitchish7-llamainit-s3.yaml \
