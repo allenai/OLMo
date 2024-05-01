fd9ae868d332688604687cc5e666a92f825d270d
Shane A <shanea@allenai.org>
2024-05-01 15:17:34 -0700

    Use static rendezous and low const LR for 1B

--- scripts/beaker/lr-scheduling.sh
+++ scripts/beaker/lr-scheduling.sh
@@ -8,6 +8,9 @@ shift
 NUM_NODES=$1
 shift
 
+BEAKER_REPLICA_RANK=$1
+shift
+
 # Warm HF cache
 mkdir -p /root/.cache
 pushd /root/.cache
@@ -18,19 +21,20 @@ export HF_DATASETS_OFFLINE=1
 torchrun \
   --nnodes ${NUM_NODES}:${NUM_NODES} \
   --nproc-per-node 8 \
-  --rdzv_id=101 \
-  --rdzv_backend=c10d \
+  --rdzv_id=12347 \
+  --rdzv_backend=static \
   --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  --node_rank=$BEAKER_REPLICA_RANK \
+  --rdzv_conf="read_timeout=420" \
   scripts/train.py \
     configs/lr-scheduling-s3.yaml \
-      --run_name=olmo-small-linear-decay-step730000-match40000steps \
+      --run_name=lr-scheduling-low-const-lr \
       --fsdp.sharding_strategy=SHARD_GRAD_OP \
-      --load_path=s3://ai2-llm/checkpoints/olmo-small-3T-lower-lr-tie/5683801/step730000-unsharded/ \
-      --wandb.name=olmo-small-linear-decay-step730000-match40000steps \
-      --wandb.group=olmo-small-linear-decay-step730000-match40000steps \
+      --load_path=r2://olmo-checkpoints/unsorted/6746551/step440000-unsharded/ \
+      --wandb.name=lr-scheduling-low-const-lr \
+      --wandb.group=lr-scheduling-low-const-lr \
       --scheduler.name=linear_with_warmup \
-      --scheduler.t_warmup=730000 \
+      --scheduler.t_warmup=440000 \
       --scheduler.alpha_f=0.0 \
-      --optimizer.learning_rate=4.023e-5 \
-      --max_duration=736500 \
+      --optimizer.learning_rate=5.07e-5 \
       --save_overwrite
