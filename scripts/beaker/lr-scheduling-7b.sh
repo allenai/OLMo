9c91e1681ef4fd1b9d9c66b45de8f56dd9f49b9b
Shane A <shanea@allenai.org>
2024-05-02 09:35:53 -0700

    Attempt to get around NCCL timeout

--- scripts/beaker/lr-scheduling-7b.sh
+++ scripts/beaker/lr-scheduling-7b.sh
@@ -21,6 +21,9 @@ export HF_DATASETS_OFFLINE=1
 # Disabling infiniband for jupiter as temp workaround
 # export NCCL_IB_DISABLE=1
 
+# Try to fix NCCL timeouts?
+export NCCL_CUMEM_ENABLE=0
+
 # # Install OLMo-core
 # mkdir -p ~/.ssh
 # printenv SSH_KEY > ~/.ssh/id_ed25519
