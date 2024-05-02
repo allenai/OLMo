9d7a395ecee0a483de70fc2b11bad8266a8e1a3c
Dirk Groeneveld <dirkg@allenai.org>
2024-05-02 13:23:22 -0700

    Maybe I can specify this in seconds?

--- scripts/beaker/mitchish7-llamainit-launch.sh
+++ scripts/beaker/mitchish7-llamainit-launch.sh
@@ -18,7 +18,7 @@ gantry run \
   --budget ai2/oe-training \
   --no-nfs \
   --propagate-failure \
-  --synchronized-start-timeout 10m \
+  --synchronized-start-timeout 600 \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
