1fc07cd990397cf5d85a26277bcd2d87ab9c6ed4
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-05-03 21:04:29 +0530

    right config key

--- olmo/train.py
+++ olmo/train.py
@@ -712,7 +712,7 @@ class Trainer:
         )
 
         emb_norm = optim_metrics["param/transformer.wte.weight.norm"]
-        emb_std = math.sqrt(emb_norm^2 / (self.cfg.embedding_size * self.cfg.vocab_size))
+        emb_std = math.sqrt(emb_norm^2 / (self.cfg.model.embedding_size * self.cfg.model.vocab_size))
         emb_decay_factor = 1.0 - emb_std
 
         # Adjust the learning rate.
