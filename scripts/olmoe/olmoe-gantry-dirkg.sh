dc3c3cd27a33671491fc845fe8ee5632050a79a5
Muennighoff <n.muennighoff@gmail.com>
2024-05-05 14:47:25 -0700

    Add chmod

--- scripts/olmoe/olmoe-gantry-dirkg.sh
+++ scripts/olmoe/olmoe-gantry-dirkg.sh
@@ -20,6 +20,7 @@ pushd /root/.cache
 curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -
 popd
 export HF_DATASETS_OFFLINE=1
+chmod a+rwx scripts/olmoe/olmoe-gantry-dirkg.sh
 
 torchrun \
   --nnodes ${NUM_NODES}:${NUM_NODES} \
