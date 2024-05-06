4ed3d0dc430ae64eb1fe5e87b9121178ff72a7d4
Muennighoff <n.muennighoff@gmail.com>
2024-05-05 20:04:37 -0700

    Fix beaker scripts

--- scripts/olmoe/olmoe-gantry-fix.sh
+++ scripts/olmoe/olmoe-gantry-fix.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-
 set -ex
 
-CONFIG_PATH=configs/olmoe/OLMo-1B-NOSHARD.yml
-NUM_NODES=1
+CONFIG_PATH=configs/olmoe/OLMo-1B-NOSHARD-S3.yml
+NUM_NODES=2
 BEAKER_REPLICA_RANK=0
 
+# Warm HF cache
+#mkdir -p /root/.cache
+#pushd /root/.cache
+#curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -
+#popd
+#export HF_DATASETS_OFFLINE=1
 
 gantry run \
   --allow-dirty \
@@ -19,12 +24,28 @@ gantry run \
   --gpus 8 \
   --replicas "${NUM_NODES}" \
   --nfs \
-  --mount /net/nfs.cirrascale/allennlp/petew/cache:/root/.cache \
+  --env-secret WANDB_API_KEY=WANDB_API_KEY \
+  --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
+  --env-secret AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY \
+  --env-secret R2_ENDPOINT_URL=R2_ENDPOINT_URL \
+  --leader-selection \
+  --host-networking \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
   --shared-memory 10GiB \
   --venv base \
   --yes \
-  -- /bin/bash -c "torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 8 --node_rank=$BEAKER_REPLICA_RANK --rdzv_id=12347 --rdzv_backend=static --rdzv_conf='read_timeout=420' --rdzv_endpoint=\$BEAKER_NODE_HOSTNAME:29400 scripts/train.py ${CONFIG_PATH}"
+  -- /bin/bash -c "mkdir -p /root/.cache; pushd /root/.cache; curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -; popd; export HF_DATASETS_OFFLINE=1; torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 8 --rdzv_id=12347 --rdzv_backend=c10d --rdzv_conf='read_timeout=420' --rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400 scripts/train.py ${CONFIG_PATH}"
+
+#export NCCL_DEBUG=INFO;
 # ${ARGS}"
+
+# Single node:
+#--rdzv_endpoint=\$BEAKER_NODE_HOSTNAME:29400
+# Multinode:
+#--rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400
+#  --mount /net/nfs.cirrascale/allennlp/petew/cache:/root/.cache \
+#pip install megablocks; 
+#--node_rank=$BEAKER_REPLICA_RANK
+#BEAKER_LEADER_REPLICA_HOSTNAME
