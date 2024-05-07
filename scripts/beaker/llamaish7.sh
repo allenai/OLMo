4d3afa63c451ea89e034bbae092adcec7ab6f7c9
Dirk Groeneveld <dirkg@allenai.org>
2024-05-07 16:02:39 -0700

    Create way more detailed artifacts

--- scripts/beaker/llamaish7.sh
+++ scripts/beaker/llamaish7.sh
@@ -28,8 +28,9 @@ torchrun \
   --rdzv_conf="read_timeout=420" \
   scripts/train.py \
   configs/llamaish7-s3.yaml \
-    --run_name=llamaish7 \
-    --wandb.name=llamaish7 \
+    --run_name=llamaish7-detailed \
+    --wandb.name=llamaish7-detailed \
+    --wandb.group=llamaish7-detailed \
     --model.flash_attention=true \
     --fsdp.wrapping_strategy=by_block_and_size \
     --fsdp.sharding_strategy=SHARD_GRAD_OP \
@@ -37,7 +38,10 @@ torchrun \
     --activation_checkpointing=fine_grained \
     --fused_loss=true \
     --device_train_microbatch_size=2 \
-    --global_train_batch_size=1024
-    
+    --global_train_batch_size=1024 \
+    --save_interval=50 \
+    --eval_interval=50 \
+    --optimizer.metrics_log_interval=1
+
     #--save_overwrite \
     #'--load_path=${path.last_checkpoint:s3://ai2-llm/checkpoints/OLMo-medium/llamaish7/}'
