0d7d7b5cc8864ce5db9387e1fe56b7a18c84eda2
Ananya Harsh Jha <ananyahjha93@gmail.com>
2024-05-08 21:36:29 +0000

    rebase

--- scripts/beaker/warm_hf_cache.sh
+++ scripts/beaker/warm_hf_cache.sh
@@ -4,6 +4,12 @@ set -ex
 
 mkdir -p /root/.cache
 pushd /root/.cache
+<<<<<<< HEAD
 curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache.tar.gz" | tar -xzf -
 popd
 export HF_DATASETS_OFFLINE=1
+=======
+curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v2.tar.gz" | tar -xzf -
+popd
+export HF_DATASETS_OFFLINE=1
+>>>>>>> 1236894e (Adds a config to train the 7B in the same way)
