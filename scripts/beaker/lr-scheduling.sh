6c5de926d025fe10c0b83595823c42506f2e0c19
Shane A <shanea@allenai.org>
2024-05-09 14:07:54 -0700

    Try get secret mounting working

--- scripts/beaker/lr-scheduling.sh
+++ scripts/beaker/lr-scheduling.sh
@@ -18,6 +18,10 @@ curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" 
 popd
 export HF_DATASETS_OFFLINE=1
 
+mkdir -p ~/.aws
+echo $AWS_CONFIG > ~/.aws/config
+echo $AWS_CREDENTIALS > ~/.aws/credentials
+
 # Force processes to synchronize at init_process_group
 export TORCH_DIST_INIT_BARRIER=1
 
