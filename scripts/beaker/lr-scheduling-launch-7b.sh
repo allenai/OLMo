faead57afcbaa011f985afca458708afb94d32d4
Shane A <shanea@allenai.org>
2024-05-12 20:21:53 -0700

    Add Weka credentials to 7B

--- scripts/beaker/lr-scheduling-launch-7b.sh
+++ scripts/beaker/lr-scheduling-launch-7b.sh
@@ -22,17 +22,14 @@ gantry run \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
-  --env NCCL_IB_HCA=^mlx5_bond_0 \
-  --env-secret SSH_KEY=SSH_KEY \
-  --env-secret WANDB_API_KEY=WANDB_API_KEY \
-  --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
-  --env-secret AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY \
-  --env-secret R2_ACCESS_KEY_ID=R2_ACCESS_KEY_ID \
-  --env-secret R2_SECRET_ACCESS_KEY=R2_SECRET_ACCESS_KEY \
+  --env R2_PROFILE=R2 \
+  --env S3_PROFILE=S3 \
+  --env WEKA_PROFILE=WEKA \
+  --env-secret AWS_CONFIG=AWS_CONFIG \
+  --env-secret AWS_CREDENTIALS=AWS_CREDENTIALS \
   --env-secret R2_ENDPOINT_URL=R2_ENDPOINT_URL \
-  --env-secret WEKA_ACCESS_KEY_ID=WEKA_ACCESS_KEY_ID \
-  --env-secret WEKA_SECRET_ACCESS_KEY=WEKA_SECRET_ACCESS_KEY \
   --env-secret WEKA_ENDPOINT_URL=WEKA_ENDPOINT_URL \
+  --env-secret WANDB_API_KEY=WANDB_API_KEY \
   --shared-memory 10GiB \
   --venv base \
   --yes \
