66a84ab3f4a44e558f1b4083a625ed8da56e060a
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-05-14 16:15:49 -0700

    turn off flash attn

--- scripts/beaker/llamaish7-fp32.sh
+++ scripts/beaker/llamaish7-fp32.sh
@@ -31,7 +31,7 @@ torchrun \
     --run_name=llamaish7-fp32 \
     --wandb.name=llamaish7-fp32 \
     --wandb.group=llamaish7-fp32 \
-    --model.flash_attention=true \
+    --model.flash_attention=false \
     --fsdp.wrapping_strategy=by_block_and_size \
     --fsdp.sharding_strategy=SHARD_GRAD_OP \
     --save_folder=runs/ \
