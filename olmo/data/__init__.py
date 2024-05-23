0baa67ab5e936aad099f66130f5e75047e5f8467
Shane A <shanea@allenai.org>
2024-05-23 15:33:19 -0700

    Add world size option to build_train_dataloader

--- olmo/data/__init__.py
+++ olmo/data/__init__.py
@@ -80,7 +80,7 @@ def build_eval_dataloader(
     )
 
 
-def build_train_dataloader(train_config: TrainConfig) -> DataLoader:
+def build_train_dataloader(train_config: TrainConfig, world_size: Optional[int] = None) -> DataLoader:
     assert train_config.device_train_batch_size is not None
     collator = DataCollator(
         pad_direction=train_config.data.pad_direction, pad_token_id=train_config.model.pad_token_id
@@ -103,6 +103,7 @@ def build_train_dataloader(train_config: TrainConfig) -> DataLoader:
             seed=seed + (train_config.epoch or 0),
             shuffle=True,
             drop_last=train_config.data.drop_last,
+            world_size=world_size,
             work_dir=work_dir,
         ),
         batch_size=train_config.device_train_batch_size,
