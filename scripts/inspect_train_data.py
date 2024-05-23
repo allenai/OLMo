1dcac1752dfee08a23b86c405b2783bd5ceb9e24
Shane A <shanea@allenai.org>
2024-05-23 15:30:42 -0700

    Use argparse and add new arguments

--- scripts/inspect_train_data.py
+++ scripts/inspect_train_data.py
@@ -2,19 +2,27 @@
 Use this script to inspect the data in given batches from a training run.
 """
 
+import argparse
 import gzip
-import sys
 from pathlib import Path
 from typing import Optional
 
 from olmo.config import TrainConfig
 from olmo.data import build_memmap_dataset
-from olmo.exceptions import OLMoCliError
 from olmo.tokenizer import Tokenizer
 from olmo.util import clean_opt, prepare_cli_environment
 
 
-def main(save_folder: Path, *steps: int, rank: Optional[int] = None):
+def main(
+    run_path: str,
+    *steps: int,
+    world_size: Optional[int] = None,
+    rank: Optional[int] = None,
+    reference_step: Optional[int] = None,
+    use_data_indices: bool = True,
+):
+    save_folder = Path(run_path)
+
     cfg = TrainConfig.load(save_folder / "config.yaml", overrides=[clean_opt("--evaluators=[]")])
     dataset = build_memmap_dataset(cfg, cfg.data)
     tokenizer = Tokenizer.from_train_config(cfg)
@@ -48,9 +56,39 @@ def main(save_folder: Path, *steps: int, rank: Optional[int] = None):
 if __name__ == "__main__":
     prepare_cli_environment()
 
-    try:
-        save_folder, rank, steps = sys.argv[1], int(sys.argv[2]), [int(i) for i in sys.argv[3:]]
-    except (IndexError, ValueError):
-        raise OLMoCliError(f"Usage: {sys.argv[0]} [SAVE_FOLDER] [RANK] [STEP_NUMBER...]")
+    parser = argparse.ArgumentParser()
 
-    main(Path(save_folder), *steps, rank=rank if rank >= 0 else None)
+    parser.add_argument("run_path", help="Path to run of which you want to inspect training data")
+    parser.add_argument(
+        "rank",
+        type=int,
+        help="Device rank for which you want to see training data. Set to `-1` to get all ranks.",
+    )
+    parser.add_argument(
+        "steps", nargs="+", type=int, help="Steps of run for which you want to see training data"
+    )
+    parser.add_argument(
+        "--no_data_indices",
+        action="store_false",
+        dest="use_data_indices",
+        help="If set, this script acts as if data indices are not present.",
+    )
+    parser.add_argument(
+        "--checkpoint_num",
+        type=int,
+        help="Step number of checkpoint from which training state is to be obtained. Required when data indices are not present.",
+    )
+    parser.add_argument(
+        "--world_size", type=int, help="World size. Required when data indices are not present."
+    )
+
+    args = parser.parse_args()
+
+    main(
+        args.run_path,
+        *args.steps,
+        world_size=args.world_size,
+        rank=args.rank if args.rank >= 0 else None,
+        reference_step=args.checkpoint_num if args.checkpoint_num >= 0 else None,
+        use_data_indices=args.use_data_indices,
+    )
