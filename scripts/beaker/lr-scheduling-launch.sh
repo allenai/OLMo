63780da20f32becc3cb6614e15664700e7c3b3cc
Shane A <shanea@allenai.org>
2024-05-28 11:05:22 -0700

    Remove --no-nfs

--- scripts/beaker/lr-scheduling-launch.sh
+++ scripts/beaker/lr-scheduling-launch.sh
@@ -19,7 +19,6 @@ gantry run \
   --budget ai2/oe-training \
   --propagate-failure \
   --synchronized-start-timeout "30m" \
-  --no-nfs \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
