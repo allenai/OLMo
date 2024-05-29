a1604340756f2e93474c32f37f7b3b32a502f77b
Dustin <drschwenk@gmail.com>
2024-05-29 15:55:46 -0700

    change run name

--- scripts/beaker/llm-360-amber-launch.sh
+++ scripts/beaker/llm-360-amber-launch.sh
@@ -6,8 +6,8 @@ NUM_NODES=8
 
 gantry run \
   --workspace ai2/OLMo-training  \
-  --task-name llm-360-amber-data-repro \
-  --description "OLMO 7B LLM 360 Amber data" \
+  --task-name llm-306-amber-data-hyper-init-repro\
+  --description "OLMO 7B LLM 360 Amber data, init, and hyperparams" \
   --priority high \
   --preemptible \
   --beaker-image shanea/olmo-torch2.2-gantry \
