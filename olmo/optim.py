1b1382c68c3f8fdff5e38e08386679da6316563f
Dustin <drschwenk@gmail.com>
2024-05-29 15:39:51 -0700

    change adamw eps to match llm-360

--- olmo/optim.py
+++ olmo/optim.py
@@ -730,7 +730,7 @@ def build_optimizer(cfg: TrainConfig, model: nn.Module) -> Optimizer:
             lr=cfg.optimizer.learning_rate,
             betas=cfg.optimizer.betas,
             weight_decay=cfg.optimizer.weight_decay,
-            eps=1e-5,
+            eps=1e-8,
         )
     else:
         raise NotImplementedError
