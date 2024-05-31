9ac254c107cd34978bfd72b3415dd50eefe3b124
ananyahjha93 <ananyahjha93@gmail.com>
2024-05-31 04:02:25 -0700

    added scripts

---
+++ scripts/tiny/torchrun-script.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+IFS=$'\n\t'
+
+BEAKER_LEADER_REPLICA_HOSTNAME=$1
+shift
+
+NUM_NODES=$1
+shift
+
+# Warm HF cache
+mkdir -p /root/.cache
+pushd /root/.cache
+curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -
+popd
+export HF_DATASETS_OFFLINE=1
+
+mqa-transformer-300M
+torchrun \
+  --nnodes ${NUM_NODES}:${NUM_NODES} \
+  --nproc-per-node 8 \
+  --rdzv_id=101 \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  scripts/train.py \
+    configs/alt_arch/transformer_base-300M.yaml \
+      --run_name=mqa-transformer-300M-baseline \
+      --device_train_microbatch_size=16 \
+      --fsdp.sharding_strategy=SHARD_GRAD_OP \
+      --save_overwrite
