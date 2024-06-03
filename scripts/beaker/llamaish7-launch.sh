35ac73b51f72baf998055b4eba639a04a79ab982
ananyahjha93 <ananyahjha93@gmail.com>
2024-06-03 14:59:41 -0700

    cluster

--- scripts/beaker/llamaish7-launch.sh
+++ scripts/beaker/llamaish7-launch.sh
@@ -9,10 +9,10 @@ gantry run \
   --workspace ai2/OLMo-training \
   --task-name ${TASK_NAME} \
   --description "OLMo medium - 7B - Llamaish - EmbInitFix - No grad clipping warmup - start from 0 lr - alibi" \
-  --priority high \
+  --priority urgent \
   --preemptible \
   --beaker-image shanea/olmo-torch2.2-gantry \
-  --cluster ai2/jupiter-cirrascale \
+  --cluster ai2/pluto-cirrascale \
   --gpus 8 \
   --replicas "${NUM_NODES}" \
   --leader-selection \
