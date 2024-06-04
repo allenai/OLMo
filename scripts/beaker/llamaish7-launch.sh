49fa2ce03a8e90108cdf5133e665b3e78d76ca30
ananyahjha93 <ananyahjha93@gmail.com>
2024-06-04 03:46:30 -0700

    alibi rerun

--- scripts/beaker/llamaish7-launch.sh
+++ scripts/beaker/llamaish7-launch.sh
@@ -20,7 +20,7 @@ gantry run \
   --budget ai2/oe-training \
   --no-nfs \
   --propagate-failure \
-  --synchronized-start-timeout 10m \
+  --synchronized-start-timeout 30m \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
