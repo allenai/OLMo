f3dffa47edbc3333a8ba35681bc78ccbcfa28744
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-06-04 14:29:37 -0700

    organize

--- scripts/beaker/chameleon/llamaish7-qk-norm-reorder-zloss-launch.sh
+++ scripts/beaker/chameleon/llamaish7-qk-norm-reorder-zloss-launch.sh
@@ -30,4 +30,4 @@ gantry run \
   --venv base \
   --yes \
   --timeout=-1 \
-  -- /bin/bash -c "scripts/beaker/llamaish7-qk-norm-reorder-zloss.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
+  -- /bin/bash -c "scripts/beaker/chameleon/llamaish7-qk-norm-reorder-zloss.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
