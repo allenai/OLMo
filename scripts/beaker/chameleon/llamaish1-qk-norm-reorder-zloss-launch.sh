e2e653aede5417b8b85bada336b25e647d530732
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-06-05 12:59:12 -0700

    run on other cluster

--- scripts/beaker/chameleon/llamaish1-qk-norm-reorder-zloss-launch.sh
+++ scripts/beaker/chameleon/llamaish1-qk-norm-reorder-zloss-launch.sh
@@ -11,7 +11,7 @@ gantry run \
   --priority high \
   --preemptible \
   --beaker-image shanea/olmo-torch2.2-gantry \
-  --cluster ai2/jupiter-cirrascale \
+  --cluster ai2/jupiter-cirrascale-2 \
   --gpus 8 \
   --replicas "${NUM_NODES}" \
   --leader-selection \
