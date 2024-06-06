59ec8625eb10d6fe56f24763a368eae53df26ae2
Ian <magnusson.i@northeastern.edu>
2024-06-06 11:21:42 -0700

    test run

---
+++ scripts/beaker/cheap_decisions.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+IFS=$'\n\t'
+
+BEAKER_LEADER_REPLICA_HOSTNAME=$1
+shift
+
+NUM_NODES=$1
+shift
+
+BEAKER_REPLICA_RANK=$1
+shift
+
+torchrun \
+  --nnodes ${NUM_NODES}:${NUM_NODES} \
+  --nproc-per-node 8 \
+  --rdzv_id=12347 \
+  --rdzv_backend=static \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  --node_rank=$BEAKER_REPLICA_RANK \
+  --rdzv_conf="read_timeout=420" \
+  scripts/train.py \
+  configs/mitchish1-s3-cheap-decisions.yaml \
+    --model.flash_attention=true \
+    # --save_folder=runs/ \
+    # --save_overwrite \
+    --gen1_gc_interval=10
