1e0d34964220f1a12931d1db87ccb1e9bb4400a7
Ian <magnusson.i@northeastern.edu>
2024-06-11 00:07:13 +0100

    it was hf cache!

--- scripts/beaker/cheap_decisions_launch.sh
+++ scripts/beaker/cheap_decisions_launch.sh
@@ -19,7 +19,7 @@ gantry run \
   --budget ai2/oe-eval \
   --no-nfs \
   --propagate-failure \
-  --synchronized-start-timeout 20m \
+  --synchronized-start-timeout 600m \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
