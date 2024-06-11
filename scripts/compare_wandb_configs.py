049969b22d0b6d0789ae2fc48f2b40111f9ff533
Dirk Groeneveld <dirkg@allenai.org>
2024-06-11 13:46:50 -0700

    Simpler

--- scripts/compare_wandb_configs.py
+++ scripts/compare_wandb_configs.py
@@ -9,14 +9,14 @@ log = logging.getLogger(__name__)
 
 
 def flatten(dictionary, parent_key="", separator="."):
-    items = []
+    d = {}
     for key, value in dictionary.items():
         new_key = parent_key + separator + key if parent_key else key
         if isinstance(value, MutableMapping):
-            items.extend(flatten(value, new_key, separator=separator).items())
+            d.update(**flatten(value, new_key, separator=separator))
         else:
-            items.append((new_key, value))
-    return dict(items)
+            d[new_key] = value
+    return d
 
 
 @click.command()
