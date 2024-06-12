397af9552ebb64326b1e2f70879e76ad33ac8085
Dustin <drschwenk@gmail.com>
2024-06-12 15:12:23 -0700

    additional config changes

--- scripts/beaker/amberish1-normal.sh
+++ scripts/beaker/amberish1-normal.sh
@@ -28,27 +28,15 @@ torchrun \
   --node_rank=$BEAKER_REPLICA_RANK \
   --rdzv_conf="read_timeout=420" \
   scripts/train.py \
-  configs/llamaish1-s3.yaml \
+  configs/amberish1-s3.yaml \
     --model.flash_attention=true \
-    --fsdp.wrapping_strategy=by_block_and_size \
-    --fsdp.sharding_strategy=SHARD_GRAD_OP \
     --gen1_gc_interval=null \
     --save_folder=runs/ \
     --activation_checkpointing=fine_grained \
     --fused_loss=false \
-    --device_train_microbatch_size=4 \
-    --global_train_batch_size=512 \
     --save_interval=250 \
     --eval_interval=250 \
     --optimizer.metrics_log_interval=1 \
     --save_overwrite \
-    --model.init_fn=normal \
-    --model.init_std=0.02 \
-    --model.init_cutoff_factor=3 \
-    --model.clip_qkv=null \
     --save_num_checkpoints_to_keep=3 \
-    --scheduler.warmup_min_lr=0 \
-    --scheduler.grad_clip_warmup_steps=null \
-    --scheduler.units=steps \
-    --scheduler.t_warmup=2000
-    '--load_path=${path.last_checkpoint:s3://ai2-llm/checkpoints/OLMo-small/llm-306-amber-data-repro-db-normal-init-2}'
+    # '--load_path=${path.last_checkpoint:s3://ai2-llm/checkpoints/OLMo-small/llm-306-amber-data-repro-db-normal-init-2}'
