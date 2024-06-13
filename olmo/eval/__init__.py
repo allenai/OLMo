97911fdc472314ba243c957408f16d945ebd120a
Shane A <shanea@allenai.org>
2024-06-13 15:43:24 -0700

    Remove type ignore for downstream evaluator construction

--- olmo/eval/__init__.py
+++ olmo/eval/__init__.py
@@ -34,7 +34,7 @@ def build_downstream_evaluator(
         task_class, task_kwargs = task_class
     ds_eval_dataset = task_class(
         tokenizer=tokenizer, datasets_cache_dir=train_config.hf_datasets_cache_dir, **task_kwargs
-    )  # type: ignore
+    )
     data_config = eval_cfg.data
     if is_unit_test:
         ds_eval_sampler = None
