2c3e1e629a5803b97067a9990f90fef27b445112
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-06-13 14:03:16 -0700

    use newer configs

--- scripts/beaker/llamaish1-normal.sh
+++ scripts/beaker/llamaish1-normal.sh
@@ -14,7 +14,7 @@ shift
 # Warm HF cache
 mkdir -p /root/.cache
 pushd /root/.cache
-curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -
+curl "https://storage.googleapis.com/hf-cache/huggingface_cache_v4.tar.gz" | tar --keep-newer-files -xzf -
 popd
 export HF_DATASETS_OFFLINE=1
 
@@ -35,30 +35,18 @@ torchrun \
   --node_rank=$BEAKER_REPLICA_RANK \
   --rdzv_conf="read_timeout=420" \
   scripts/train.py \
-  configs/llamaish1-weka.yaml \
+  configs/llamaish1-normal-weka.yaml \
     --run_name=$EXPERIMENT \
     --wandb.name=$EXPERIMENT \
     --wandb.group=$EXPERIMENT \
-    --model.flash_attention=true \
     --fsdp.wrapping_strategy=by_block_and_size \
     --fsdp.sharding_strategy=SHARD_GRAD_OP \
-    --gen1_gc_interval=1 \
     --save_folder=runs/ \
-    --activation_checkpointing=fine_grained \
-    --fused_loss=false \
     --device_train_microbatch_size=4 \
     --global_train_batch_size=512 \
     --save_interval=250 \
     --eval_interval=250 \
     --optimizer.metrics_log_interval=1 \
     --save_overwrite \
-    --model.init_fn=normal \
-    --model.init_std=0.02 \
-    --model.init_cutoff_factor=3 \
-    --model.clip_qkv=null \
     --save_num_checkpoints_to_keep=3 \
-    --scheduler.warmup_min_lr=0 \
-    --scheduler.grad_clip_warmup_steps=null \
-    --scheduler.units=tokens \
-    --scheduler.t_warmup=4194304000 \
     --load_path=s3://ai2-llm/checkpoints/OLMo-small/llamaish1-normal-shard/step2000
