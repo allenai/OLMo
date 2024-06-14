d3887f89596e54a7741ebf16193a3eb22ae08e0e
epwalsh <petew@allenai.org>
2024-06-14 14:04:45 -0700

    start over

--- scripts/beaker/epwalsh-stability/amberish7.sh
+++ scripts/beaker/epwalsh-stability/amberish7.sh
@@ -60,8 +60,7 @@ torchrun \
       --run_name="${GANTRY_TASK_NAME}" \
       --optimizer.metrics_log_interval=1 \
       --global_train_batch_size=2304 \
-      --device_train_microbatch_size=4 \
-      '--load_path=/weka/oe-training-default/ai2-llm/checkpoints/OLMo-medium/epwalsh-stability/${run_name}/step2250'
+      --device_train_microbatch_size=4
 
 #      '--load_path=/weka/oe-training-default/ai2-llm/checkpoints/OLMo-medium/epwalsh-stability/${run_name}/step2250'
 #      '--load_path=${path.last_checkpoint:${save_folder}}'
