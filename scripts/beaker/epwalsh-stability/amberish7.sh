d83ba2fea9b855c5baaea12e03248171f2f135f3
epwalsh <petew@allenai.org>
2024-06-14 10:53:19 -0700

    try increasing micro batch size again

--- scripts/beaker/epwalsh-stability/amberish7.sh
+++ scripts/beaker/epwalsh-stability/amberish7.sh
@@ -58,9 +58,10 @@ torchrun \
   scripts/train.py \
     configs/amberish7-weka.yaml \
       --run_name="${GANTRY_TASK_NAME}" \
+      --wandb=null \
       --optimizer.metrics_log_interval=1 \
       --global_train_batch_size=2304 \
-      --device_train_microbatch_size=4 \
+      --device_train_microbatch_size=8 \
       '--load_path=/weka/oe-training-default/ai2-llm/checkpoints/OLMo-medium/epwalsh-stability/${run_name}/step2250'
 
 #      '--load_path=/weka/oe-training-default/ai2-llm/checkpoints/OLMo-medium/epwalsh-stability/${run_name}/step2250'
