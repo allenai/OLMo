8a860621bec6dda2ed903b404181ed7b422aa377
Ian <magnusson.i@northeastern.edu>
2024-06-20 12:42:18 +0100

    try out new mix

---
+++ scripts/beaker/cheap_decisions_launch-baseline-and-sources.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -ex
+
+NUM_NODES=8
+
+gantry run \
+  --workspace ai2/cheap_decisions  \
+  --task-name cheap-decisions-test-mitchish1-001\
+  --description "OLMO 1B LLM 1T tokens on dolma 1.7" \
+  --priority normal \
+  --preemptible \
+  --beaker-image shanea/olmo-torch2.2-gantry \
+  --cluster ai2/jupiter-cirrascale-2 \
+  --gpus 8 \
+  --replicas "${NUM_NODES}" \
+  --leader-selection \
+  --host-networking \
+  --budget ai2/oe-eval \
+  --no-nfs \
+  --propagate-failure \
+  --synchronized-start-timeout 600m \
+  --env LOG_FILTER_TYPE=local_rank0_only \
+  --env OMP_NUM_THREADS=8 \
+  --env OLMO_TASK=model \
+  --env-secret WANDB_API_KEY=WANDB_API_KEY \
+  --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
+  --env-secret AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY \
+  --shared-memory 10GiB \
+  --venv base \
+  --yes \
+  --timeout=-1 \
+  -- /bin/bash -c "scripts/beaker/cheap_decisions_launch-baseline-and-sources.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
