3605e548f147fbcf59d3a2ba05b17f3da0332a06
epwalsh <petew@allenai.org>
2024-06-25 11:10:15 -0700

    revert back to old workspace

--- scripts/beaker/amberish7-launch.sh
+++ scripts/beaker/amberish7-launch.sh
@@ -5,7 +5,7 @@ set -ex
 NUM_NODES=64
 
 gantry run \
-  --workspace ai2/OLMo-pretraining-stability \
+  --workspace ai2/OLMo-training \
   --task-name amberish7 \
   --description "Amberish 7B train" \
   --priority urgent \
