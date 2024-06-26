allenai/OLMo

commit 714647320afa0e226106c25be75c576769cd232c

Works even if the whole microbatch is masked

olmo/train.py
@@ -641,6 +641,7 @@ class Trainer:
     def train_batch(self, batch: Dict[str, Any]) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
         # Split into micro-batches.
         micro_batches = self.split_batch(batch)
+        batch_size_in_tokens = batch['input_ids'].numel()
 
         # In case this helps with memory utilization.
         del batch
@@ -651,9 +652,11 @@ class Trainer:
             with torch.autocast("cuda", enabled=True, dtype=self.cfg.autocast_precision):
                 # Run forward pass.
                 ce_loss, z_loss, logits = self.model_forward(
-                    micro_batch, compute_z_loss=self.cfg.softmax_auxiliary_loss
+                    micro_batch,
+                    compute_z_loss=self.cfg.softmax_auxiliary_loss,
+                    loss_reduction="sum"
                 )
-                ce_loss = ce_loss / len(micro_batches)
+                ce_loss = ce_loss / batch_size_in_tokens
 
                 # In case this helps with memory utilization.
                 del micro_batch

commit d15262a341a3408cbaeb7ef70b8100191701688a

Fix Z-loss calculation
It should be average over tokens, not instances.
714647320afa0e226106c25be75c576769cd232c introduced this bug.
See https://github.com/mlfoundations/open_lm/blob/main/open_lm/losses.py
for a reference implementation.

olmo/train.py
@@ -653,8 +653,8 @@ class Trainer:
         return ce_loss, z_loss, logits
 
     def train_micro_batch(
-        self, micro_batch: Dict[str, Any], batch_size_in_tokens: int, num_micro_batches: int
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        self, micro_batch: Dict[str, Any], batch_size_in_tokens: int
+    ) -> Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor]]:
         ce_loss, z_loss, logits = self.model_forward(
             micro_batch, compute_z_loss=self.cfg.softmax_auxiliary_loss, loss_reduction="sum"
         )
@@ -666,7 +666,7 @@ class Trainer:
         # Get loss to optimize for.
         if self.cfg.softmax_auxiliary_loss:
             assert z_loss is not None
-            z_loss = z_loss / num_micro_batches
+            z_loss = z_loss / batch_size_in_tokens
             loss = ce_loss + z_loss
         else:
             loss = ce_loss
@@ -701,9 +701,7 @@ class Trainer:
             with grad_sync_context():
                 with torch.autocast("cuda", enabled=True, dtype=self.cfg.autocast_precision):
                     # Run forward pass.
-                    loss, ce_loss, z_loss = self.train_micro_batch(
-                        micro_batch, batch_size_in_tokens, num_micro_batches
-                    )
+                    loss, ce_loss, z_loss = self.train_micro_batch(micro_batch, batch_size_in_tokens)
 
                     # Update overall CE batch loss.
                     ce_batch_loss += ce_loss.detach()