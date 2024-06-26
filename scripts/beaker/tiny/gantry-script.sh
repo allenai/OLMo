71621a1e3c4dc2b74e42568ed6c85e910bd70245
ananyahjha93 <ananyahjha93@gmail.com>
2024-06-26 14:51:59 -0700

    150M run on jupiter

--- scripts/beaker/tiny/gantry-script.sh
+++ scripts/beaker/tiny/gantry-script.sh
@@ -2,17 +2,17 @@
 
 set -ex
 
-NUM_NODES=4
-TASK_NAME=tiny-olmo-60M-rms-norm-adam-eps-1e-8
+NUM_NODES=8
+TASK_NAME=tiny-olmo-150M-rms-norm-adam-eps-1e-8
 
 gantry run \
   --workspace ai2/OLMo-training \
   --task-name ${TASK_NAME} \
   --description "Tiny OLMo runs" \
-  --priority urgent \
+  --priority high \
   --preemptible \
   --beaker-image shanea/olmo-torch2.2-gantry \
-  --cluster ai2/pluto-cirrascale \
+  --cluster ai2/jupiter-cirrascale-2 \
   --gpus 8 \
   --replicas "${NUM_NODES}" \
   --leader-selection \
