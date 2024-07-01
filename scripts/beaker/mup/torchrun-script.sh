16c935ec38a5d5c07626e689bcf4c41584cd1901
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-06-30 23:15:24 -0700

    scripts for running  check

---
+++ scripts/beaker/mup/torchrun-script.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+IFS=$'\n\t'
+
+BEAKER_LEADER_REPLICA_HOSTNAME=$1
+shift
+
+NUM_NODES=$1
+shift
+
+TASK_NAME=$1
+shift
+
+# Warm HF cache
+mkdir -p /root/.cache
+pushd /root/.cache
+curl "https://storage.googleapis.com/hf-cache/huggingface_cache_v4.tar.gz" | tar --keep-newer-files -xzf -
+popd
+export HF_DATASETS_OFFLINE=1
+
+# base-olmo
+torchrun \
+  --nnodes ${NUM_NODES}:${NUM_NODES} \
+  --nproc-per-node 8 \
+  --rdzv_id=101 \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  scripts/train.py \
+    configs/mup/base-olmo.yaml \
+      --run_name="sp_olmo_128" \
+      --wandb.name=$TASK_NAME \
+      --wandb.group=$TASK_NAME \
+      --wandb.project=olmo-mup \
+      --model.width=128
+
+
+torchrun \
+  --nnodes ${NUM_NODES}:${NUM_NODES} \
+  --nproc-per-node 8 \
+  --rdzv_id=101 \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  scripts/train.py \
+    configs/mup/base-olmo.yaml \
+      --run_name="mup_olmo_128" \
+      --wandb.name=$TASK_NAME \
+      --wandb.group=$TASK_NAME \
+      --wandb.project=olmo-mup \
+      --model.use_mup \
+      --model.width=128
