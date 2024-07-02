d4b395b07d016906fa2ced0f93a7b10d9b7a7e27
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-07-01 19:10:36 -0700

    updated paths

--- scripts/beaker/mup/torchrun-script.sh
+++ scripts/beaker/mup/torchrun-script.sh
@@ -26,27 +26,6 @@ printenv AWS_CREDENTIALS > ~/.aws/credentials
 # Temporary, since it is not part of the image yet.
 pip install mup@git+https://github.com/microsoft/mup#egg=19814971934ef91dd546f88e913fc963e096d11c
 
-#for WIDTH in 128 256 512 1024 2048 4096;
-#do
-#  torchrun \
-#    --nnodes ${NUM_NODES}:${NUM_NODES} \
-#    --nproc-per-node 8 \
-#    --rdzv_id=101 \
-#    --rdzv_backend=c10d \
-#    --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
-#    --node_rank=$BEAKER_REPLICA_RANK \
-#    scripts/train.py \
-#      configs/mup/base-olmo.yaml \
-#        --run_name="mup_olmo_$WIDTH" \
-#        --wandb.name="mup_olmo_$WIDTH" \
-#        --wandb.group="mup_olmo" \
-#        --wandb.project=olmo-mup \
-#        --stop_at=1000 \
-#        --model.use_mup \
-#	      --model.mup_base_shapes=scripts/beaker/mup/base_olmo_shapes.bsh \
-#        --model.d_model=$WIDTH
-#done
-
 for WIDTH in 128 256 512 1024 2048 4096;
 do
   torchrun \
@@ -58,10 +37,32 @@ do
     --node_rank=$BEAKER_REPLICA_RANK \
     scripts/train.py \
       configs/mup/base-olmo.yaml \
-        --run_name="sp_olmo_$WIDTH" \
-        --wandb.name="sp_olmo_$WIDTH" \
-        --wandb.group="sp_olmo" \
+        --run_name="new_mup_olmo_$WIDTH" \
+        --wandb.name="new_mup_olmo_$WIDTH" \
+        --wandb.group="new_mup_olmo" \
         --wandb.project=olmo-mup \
         --stop_at=1000 \
+        --model.use_mup \
+	      --model.mup_base_shapes=scripts/beaker/mup/base_olmo_shapes.bsh \
         --model.d_model=$WIDTH
 done
+
+#for WIDTH in 128 256 512 1024 2048 4096;
+#do
+#  torchrun \
+#    --nnodes ${NUM_NODES}:${NUM_NODES} \
+#    --nproc-per-node 8 \
+#    --rdzv_id=101 \
+#    --rdzv_backend=c10d \
+#    --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+#    --node_rank=$BEAKER_REPLICA_RANK \
+#    scripts/train.py \
+#      configs/mup/base-olmo.yaml \
+#        --run_name="sp_olmo_$WIDTH" \
+#        --wandb.name="sp_olmo_$WIDTH" \
+#        --wandb.group="sp_olmo" \
+#        --wandb.project=olmo-mup \
+#        --save_overwrite \
+#        --stop_at=1000 \
+#        --model.d_model=$WIDTH
+#done
