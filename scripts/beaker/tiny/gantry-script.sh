b11b73054141af0af040cdf06ea23890b06db489
ananyahjha93 <ananyahjha93@gmail.com>
2024-07-01 17:41:41 -0700

    restart 150M run

--- scripts/beaker/tiny/gantry-script.sh
+++ scripts/beaker/tiny/gantry-script.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-NUM_NODES=16
-TASK_NAME=tiny-olmo-300M-rms-norm-adam-eps-1e-8-emb-wd
+NUM_NODES=8
+TASK_NAME=tiny-olmo-150M-rms-norm-adam-eps-1e-8
 
 gantry run \
   --workspace ai2/OLMo-tiny \
