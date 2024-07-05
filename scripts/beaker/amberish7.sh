94469adb7fc614d1436619c8e2801722278eff9e
epwalsh <petew@allenai.org>
2024-07-05 13:59:49 -0700

    start new epoch

--- scripts/beaker/amberish7.sh
+++ scripts/beaker/amberish7.sh
@@ -69,4 +69,5 @@ torchrun \
       --save_interval_ephemeral=500 \
       --fsdp.sharding_strategy=HYBRID_SHARD \
       --fsdp.hybrid_sharding_num_model_replicas=8 \
+      --epoch=1 \
       '--load_path=${path.last_checkpoint:${save_folder}}'
