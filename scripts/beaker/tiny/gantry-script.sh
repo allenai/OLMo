4974133209609b925d810bb1a89f7e2e6d3d56b2
ananyahjha93 <ananyahjha93@gmail.com>
2024-07-05 17:15:26 -0700

    700M OLMo config

--- scripts/beaker/tiny/gantry-script.sh
+++ scripts/beaker/tiny/gantry-script.sh
@@ -3,7 +3,7 @@
 set -ex
 
 NUM_NODES=32
-TASK_NAME=tiny-olmo-750M-rms-norm-adam-eps-1e-8-emb-wd
+TASK_NAME=tiny-olmo-700M-rms-norm-adam-eps-1e-8-emb-wd
 
 gantry run \
   --workspace ai2/OLMo-tiny \
