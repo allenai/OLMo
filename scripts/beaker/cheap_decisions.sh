b0b980d79d4aae5deb58c6abcb3e01e7adbd5533
Ian <magnusson.i@northeastern.edu>
2024-07-08 16:04:41 -0700

    frogot one

--- scripts/beaker/cheap_decisions.sh
+++ scripts/beaker/cheap_decisions.sh
@@ -39,6 +39,6 @@ torchrun \
   configs/mitchish1-s3-cheap-decisions-dolma-v1-7.yaml \
     --load_path='${path.last_checkpoint:s3://ai2-llm/checkpoints/cheap_decisions/dolma-v1-7-1B-N-1T-D-mitchish1-006}' \
     --model.flash_attention=true \
-    # --save_folder=runs/ \
     --save_overwrite=true \
     --gen1_gc_interval=10
+    # --save_folder=runs/ \
