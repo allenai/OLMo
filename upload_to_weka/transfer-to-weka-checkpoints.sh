11ec60cfe5af9f4556b6718e9ae8b3ce2746815f
Ian <magnusson.i@northeastern.edu>
2024-07-08 12:51:18 -0700

    moving checkpoints to weka

---
+++ upload_to_weka/transfer-to-weka-checkpoints.sh
@@ -0,0 +1,7 @@
+export BUDGET=ai2/oe-eval
+export S3_BUCKET=ai2-llm
+export S3_PREFIX=checkpoints/cheap_decisions/
+export WEKA_BUCKET=oe-eval-default
+export WEKA_PREFIX=ianm/ai2-llm/checkpoints/cheap_decisions/
+export SYNC_INCLUDE="*unsharded/model.pt"
+export SYNC_INCLUDE_2="*unsharded/config.yaml"
