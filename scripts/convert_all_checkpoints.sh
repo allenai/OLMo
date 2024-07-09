14c3d8699cac31341c98852c5596ff6fe0dc86ff
Ian <magnusson.i@northeastern.edu>
2024-07-09 14:50:36 -0700

    typo

--- scripts/convert_all_checkpoints.sh
+++ scripts/convert_all_checkpoints.sh
@@ -8,7 +8,11 @@ path=$1
 checkpoints_needing_conversion=$(python scripts/find_all_checkpoints_needing_conversion.py --path $path)
 echo "Converting the following checkpoints: $checkpoints_needing_conversion"
 
+len=${#checkpoints_needing_conversion[@]}
+progress=0
 for checkpoint in $checkpoints_needing_conversion; do
-    echo "Converting $checkpoint"
+    echo "Converting $progress / $len $checkpoint"
     scripts/convert_olmo_to_hf_new.py --input_dir ${checkpoint} --output_dir ${checkpoint}-hf --tokenizer_json_path tokenizers/allenai_gpt-neox-olmo-dolma-v1_5.json
+    progress=$((progress+1))
     break
+done
