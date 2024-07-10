2776aab7fb04de196b4a65cbeba6f3ca9de608fc
epwalsh <petew@allenai.org>
2024-07-10 11:21:14 -0700

    Add unsharding scripts

---
+++ scripts/beaker/unshard.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+IFS=$'\n\t'
+
+# Setup Python environment.
+conda shell.bash activate base
+
+# Install flash-attn
+# pip install awscli
+pip install '.[train]'
+pip freeze
+
+# Move AWS credentials from env to relevant files
+mkdir -p ~/.aws
+printenv AWS_CONFIG > ~/.aws/config
+printenv AWS_CREDENTIALS > ~/.aws/credentials
+
+checkpoint=/weka/oe-training-default/ai2-llm/checkpoints/OLMo-medium/amberish7/step477850
+
+python scripts/unshard.py "${checkpoint}" "${checkpoint}-unsharded" --type=olmo_core
