22b81f15f578e1385722c85aeaf3a63d663b2b7c
epwalsh <petew@allenai.org>
2024-07-17 14:13:37 -0700

    update

--- scripts/beaker/amberish7.sh
+++ scripts/beaker/amberish7.sh
@@ -65,7 +65,7 @@ torchrun \
       --run_name="${GANTRY_TASK_NAME}" \
       --optimizer.metrics_log_interval=1 \
       --save_overwrite \
-      --save_interval_ephemeral=500 \
+      --save_interval_ephemeral=null \
       --fsdp.sharding_strategy=HYBRID_SHARD \
       --fsdp.hybrid_sharding_num_model_replicas=2 \
       --epoch=1 \
