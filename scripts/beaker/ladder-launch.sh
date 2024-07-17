49167bb89b596123800660f2ae42fb9dcbcfd90e
Dustin <drschwenk@gmail.com>
2024-07-16 18:33:26 -0700

    Revert "move to pluto"
    This reverts commit cab790343ad41a8df9bd833ad11b8cbb5dc5bc1a.

--- scripts/beaker/ladder-launch.sh
+++ scripts/beaker/ladder-launch.sh
@@ -20,7 +20,8 @@ gantry run \
   --priority normal \
   --preemptible \
   --beaker-image shanea/olmo-torch2.2-gantry \
-  --cluster ai2/pluto-cirrascale \
+  --cluster ai2/jupiter-cirrascale-2 \
+  --weka=oe-training-default:/weka/oe-training-default \
   --gpus 8 \
   $MULTI_NODE_ARGS \
   --budget ai2/oe-training \
