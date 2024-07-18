5ba9d27d71128dc8e575f34bd821f3f563b34b6d
epwalsh <petew@allenai.org>
2024-07-18 15:33:54 -0700

    clean up

--- scripts/beaker/amberish/amberish1-chameleon.sh
+++ scripts/beaker/amberish/amberish1-chameleon.sh
@@ -16,31 +16,23 @@ shift
 conda shell.bash activate base
 
 # Install flash-attn
-#conda install -y -c nvidia cuda-python
 pip install packaging ninja
 export FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE
 pip install flash-attn==2.5.9.post1 --no-build-isolation
-# pip install awscli
 pip install '.[train]'
 pip freeze
 
 # Warm HF cache
-mkdir -p /root/.cache
-pushd /root/.cache
-# curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -
-curl "https://storage.googleapis.com/hf-cache/huggingface_cache_v4.tar.gz" | tar --keep-newer-files -xzf -
-popd
-export HF_DATASETS_OFFLINE=1
+# mkdir -p /root/.cache
+# pushd /root/.cache
+# curl "https://storage.googleapis.com/hf-cache/huggingface_cache_v4.tar.gz" | tar --keep-newer-files -xzf -
+# popd
+# export HF_DATASETS_OFFLINE=1
 
 # Move AWS credentials from env to relevant files
 mkdir -p ~/.aws
 printenv AWS_CONFIG > ~/.aws/config
 printenv AWS_CREDENTIALS > ~/.aws/credentials
-
-# mkdir /root/checkpoint-unsharded
-# aws s3 cp --no-progress --recursive --profile=S3 \
-#     s3://ai2-llm/checkpoints/OLMo-medium/llamaish7-EmbInitFix/step0-unsharded \
-#     /root/checkpoint-unsharded
 
 # Force processes to synchronize at init_process_group
 export TORCH_DIST_INIT_BARRIER=1
@@ -51,7 +43,6 @@ export OLMO_SHARED_FS=1
 export NCCL_DEBUG=INFO
 export NCCL_IB_HCA="^=mlx5_bond_0"
 export NCCL_SOCKET_IFNAME=ib
-# export NCCL_IB_GID_INDEX=0
 
 torchrun \
   --nnodes "${NUM_NODES}:${NUM_NODES}" \
@@ -65,6 +56,7 @@ torchrun \
     configs/amberish1-weka.yaml \
       --run_name="${GANTRY_TASK_NAME}" \
       --device_train_microbatch_size=4 \
+      --save_interval_ephemeral=null \
       --softmax_auxiliary_loss=true \
       --auxiliary_loss_multiplier=1e-5 \
       --model.attention_layer_norm=true \
