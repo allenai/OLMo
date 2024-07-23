8312a058c3a6cf202de972870db753463da38f23
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-07-22 22:45:11 -0700

    simplify script

--- scripts/run_eval.py
+++ scripts/run_eval.py
@@ -1,228 +1,136 @@
+import argparse
 import logging
-import sys
+from itertools import islice
+from typing import Any, Dict, List
+
 import torch
-import torch.distributed as dist
-import torch.multiprocessing as mp
-
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-from torch.distributed.fsdp import ShardingStrategy
-from torch.nn.parallel import DistributedDataParallel as DDP
-
-from packaging import version
-
 from cached_path import cached_path
 
+from olmo import Tokenizer
+from olmo.config import EvaluatorConfig, EvaluatorType, TrainConfig
+from olmo.eval import Evaluator, build_downstream_evaluator
+from olmo.model import OLMo
 from olmo.torch_util import (
     barrier,
-    get_default_device,
-    get_global_rank,
     get_local_rank,
-    get_local_world_size,
-    get_world_size,
+    move_to_device,
     peak_gpu_memory,
     seed_all,
 )
-
-from olmo.config import (
-    CheckpointType,
-    DDPGradSyncMode,
-    DistributedStrategy,
-    TrainConfig,
-)
-from olmo.data import build_train_dataloader
-from olmo.optim import BoltOnWarmupScheduler, build_optimizer, build_scheduler
 from olmo.train import Trainer
-from olmo.util import (
-    add_cached_path_clients,
-    clean_opt,
-    log_extra_field,
-    prepare_cli_environment,
-)
-
-from olmo.eval import build_evaluators
-from olmo.exceptions import OLMoCliError, OLMoConfigurationError
-from olmo.model import OLMo
+from olmo.util import prepare_cli_environment
 
 log = logging.getLogger("eval")
 
 
-def main(cfg: TrainConfig):
+def eval_step(
+    cfg: TrainConfig, model: OLMo, batch: Dict[str, Any], evaluator: Evaluator, device: torch.device
+) -> None:
+    # Move tensors to the right device.
+    batch = move_to_device(batch, device)
+
+    # Run forward pass.
+    with torch.no_grad():  # NOTE: 'torch.inference_mode()' doesn't work with 'torch.compile()'.
+        with torch.autocast("cuda", enabled=True, dtype=cfg.autocast_precision):
+            logits = model(
+                input_ids=batch["input_ids"],
+                attention_mask=batch.get("attention_mask"),
+                attention_bias=batch.get("attention_bias"),
+                doc_lens=batch.get("doc_lens"),
+                max_doc_lens=batch.get("max_doc_lens"),
+            ).logits
+
+    # Update metrics. Only downstream, we don't care about lm type ce loss here; setting to 0.0
+    evaluator.update_metrics(
+        batch, torch.Tensor([0.0]), logits
+    )  # batch includes all keys that the downstream evaluation needs
 
     barrier()
 
-    # Set CUDA device.
-    torch.cuda.set_device(f"cuda:{get_local_rank()}")
-    device = torch.device("cuda")
 
-    # Fill some configuration options.
-    cfg.model.precision = cfg.precision
-    cfg.device_train_batch_size = cfg.global_train_batch_size // get_world_size()
-    assert cfg.device_train_batch_size is not None  # for mypy
-
-    # Display configuration.
-    if get_global_rank() == 0:
-        if cfg.data.paths is not None and len(cfg.data.paths) < 50:
-            log.info("Configuration:")
-            log.info(cfg)
-
-    barrier()
-
-    # Set seed.
-    seed_all(cfg.seed)
-
-    # Construct data loader.
-    # train_loader = build_train_dataloader(cfg)
-    train_loader = None
-    cfg = cfg.update_with(reset_optimizer_state=True, reset_trainer_state=True)
-
-    # Construct evaluators.
-    # TODO: new evaluators here.
-    eval_cfgs = [eval_cfg for eval_cfg in cfg.evaluators if eval_cfg.type=='downstream']
-    cfg = cfg.update_with(evaluators=eval_cfgs[:1])
-    evaluators = build_evaluators(cfg, device)
-    print(evaluators)
-    barrier()
-
-    # Initialize the model.
-    log.info("Building model...")
-    olmo_model = OLMo(cfg.model)
-    log.info(f"Total number of parameters: {olmo_model.num_params():,d}")
-    log.info(f"Number of non-embedding parameters: {olmo_model.num_params(include_embedding=False):,d}")
-    log.info(f"Peak GPU Memory (MB) before {cfg.distributed_strategy}: {int(peak_gpu_memory() or 0)}")
-
-    if cfg.distributed_strategy == DistributedStrategy.ddp:
-        log.info("Wrapping model with DDP...")
-        assert cfg.ddp is not None, "DistributedStrategy ddp needs cfg.ddp to be set!"
-
-        if cfg.model.init_device != "cuda":
-            raise OLMoConfigurationError("DDP does not work with init_device set to anything other than `cuda`.")
-
-        if cfg.ddp.find_unused_params is True and cfg.ddp.grad_sync_mode != DDPGradSyncMode.micro_batch:
-            raise OLMoConfigurationError(
-                "`find_unused_params` is set to True. DDP needs to synchronize gradients for every micro-batch to avoid errors. Set `grad_sync_mode` to `micro_batch`."
-            )
-
-        param_init_fn = None
-
-        # move to cuda before calling ddp
-        dist_model = DDP(olmo_model.to(device), find_unused_parameters=cfg.ddp.find_unused_params)
-    elif cfg.distributed_strategy == DistributedStrategy.fsdp:
-        # Wrap the model in FSDP.
-        log.info("Wrapping model with FSDP...")
-        assert cfg.fsdp is not None, "DistributedStrategy fsdp needs cfg.fsdp to be set!"
-        wrap_policy = olmo_model.get_fsdp_wrap_policy(cfg.fsdp.wrapping_strategy)
-
-        if version.parse(torch.__version__) >= version.parse("2.1.0"):
-            # This prevents any parameters from being initialized twice
-            def dummy_init_fn(module: torch.nn.Module) -> None:
-                module.to_empty(device=get_default_device())
-
-            param_init_fn = dummy_init_fn
-        else:
-            param_init_fn = None
-
-        # Set up device mesh for hybrid sharding in order to specify which nodes are assoicated to a given model replica
-        device_mesh = None
-        hybrid_sharding_fsdp_kwargs = {}
-        if cfg.fsdp.sharding_strategy in (ShardingStrategy.HYBRID_SHARD, ShardingStrategy._HYBRID_SHARD_ZERO2):
-            if version.parse(torch.__version__) < version.parse("2.2.0"):
-                # Device mesh was not added to PyTorch until v2.2.0
-                raise OLMoConfigurationError(
-                    "OLMo training does not correctly support hybrid sharding before torch 2.2.0"
-                )
-
-            from torch.distributed.device_mesh import init_device_mesh
-
-            num_model_replicas = cfg.fsdp.hybrid_sharding_num_model_replicas or (
-                get_world_size() // get_local_world_size()
-            )
-
-            if num_model_replicas <= 0:
-                raise OLMoConfigurationError("fsdp.hybrid_sharding_num_model_replicas must be a positive integer")
-
-            if get_world_size() % num_model_replicas != 0:
-                raise OLMoConfigurationError("fsdp.hybrid_sharding_num_model_replicas must divide world size")
-
-            device_mesh = init_device_mesh("cuda", (num_model_replicas, get_world_size() // num_model_replicas))
-            hybrid_sharding_fsdp_kwargs["device_mesh"] = device_mesh
-
-        dist_model = FSDP(
-            olmo_model,
-            sharding_strategy=cfg.fsdp.sharding_strategy,
-            mixed_precision=cfg.fsdp_precision,
-            auto_wrap_policy=wrap_policy,
-            use_orig_params=cfg.fsdp.use_orig_params,  # needed for compile and some of our optimizer/parameter metrics
-            limit_all_gathers=True,
-            device_id=get_local_rank(),
-            param_init_fn=param_init_fn,
-            **hybrid_sharding_fsdp_kwargs,
-        )
-    elif cfg.distributed_strategy is None:
-        raise NotImplementedError("Single accelerator training not implemented yet!")
-
-    # when param_init_fn is None, FSDP will call reset_parameters() automatically
-    if param_init_fn is not None or cfg.distributed_strategy == DistributedStrategy.ddp:
-        olmo_model.reset_parameters()
-
-    log.info(f"Peak GPU Memory (MB) after {cfg.distributed_strategy}: {int(peak_gpu_memory() or 0)}")
-    log.info("Model:")
-    log.info(dist_model)
-
-
-    # Construct optimizer and learning rate scheduler.
-    optim = build_optimizer(cfg, dist_model)
-    scheduler = None #build_scheduler(cfg)
-
-    # Consolidate components into `Trainer` object.
-    with Trainer(
-        cfg=cfg,
-        epoch=cfg.epoch,
-        model=olmo_model,
-        dist_model=dist_model,
-        optim=optim,
-        scheduler=scheduler,
-        train_loader=train_loader,
-        device=device,
-        evaluators=evaluators,
-        indices_file=None,
-    ) as trainer:
-
-        assert cfg.load_path is not None
-
-        log.info(f"Loading checkpoint from {cfg.load_path}...")
-        trainer.restore_checkpoint(
-            cfg.load_path,
-            load_optimizer_state=not cfg.reset_optimizer_state,
-            load_trainer_state=not cfg.reset_trainer_state,
-            sharded_checkpointer=cfg.load_path_sharded_checkpointer,
-        )
-        log.info("Checkpoint successfully loaded")
-
-        log.info("Starting evaluation...")
-        trainer.eval()
-        log.info("Evaluation complete")
-
-
-if __name__ == "__main__":
-    try:
-        mp.set_start_method("spawn", force=True)
-    except RuntimeError as e:
-        print(f"failed to set multiprocessing start method: {e}")
-    log.info(f"Multiprocessing start method set to '{mp.get_start_method()}'")
-
-    # Initialize process group.
-    dist.init_process_group(backend="nccl")
-    log.info("Process group initialized")
+def main(checkpoint_path: str, eval_configs: List[EvaluatorConfig]):
+    yaml_path = checkpoint_path + "/" + "config.yaml"
+    cfg = TrainConfig.load(cached_path(yaml_path))
 
     prepare_cli_environment()
     log.info("CLI environment prepared")
 
-    add_cached_path_clients()
+    # Set seed.
+    seed_all(cfg.seed)
 
-    try:
-        yaml_path, args_list = sys.argv[1], sys.argv[2:]
-    except IndexError:
-        raise OLMoCliError(f"Usage: {sys.argv[0]} [CONFIG_PATH] [OPTIONS]")
+    # Set CUDA device.
 
-    cfg = TrainConfig.load(cached_path(yaml_path), [clean_opt(s) for s in args_list])
-    main(cfg)
+    if torch.cuda.is_available():
+        torch.cuda.set_device(f"cuda:{get_local_rank()}")
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    log.info("Building model...")
+    olmo_model = OLMo.from_checkpoint(checkpoint_path).to(device).eval()
+    log.info(f"Total number of parameters: {olmo_model.num_params():,d}")
+    log.info(f"Number of non-embedding parameters: {olmo_model.num_params(include_embedding=False):,d}")
+    log.info(f"Peak GPU Memory (MB) before {cfg.distributed_strategy}: {int(peak_gpu_memory() or 0)}")
+
+    tokenizer = Tokenizer.from_train_config(cfg)
+
+    evaluators = [build_downstream_evaluator(cfg, eval_config, tokenizer, device) for eval_config in eval_configs]
+
+    # TODO: temporary, remove
+    cfg = cfg.update_with(eval_subset_num_batches=10)
+
+    eval_metrics = {}
+    for evaluator in evaluators:
+        log.info(f"Running evaluation for '{evaluator.label}'...")
+
+        # Reset metrics.
+        evaluator.reset_metrics()
+
+        # Initialize data loader iterator.
+        eval_batches = iter(evaluator.eval_loader)
+
+        # Adjust how many batches to evaluate on.
+        num_eval_batches = (
+            evaluator.subset_num_batches
+            if evaluator.subset_num_batches is not None
+            else cfg.eval_subset_num_batches
+        )
+        if num_eval_batches > 0:
+            num_eval_batches = min(num_eval_batches, len(evaluator.eval_loader))
+            eval_batches = islice(eval_batches, num_eval_batches)
+
+        # Run model over batches.
+        for eval_step_n, eval_batch in enumerate(eval_batches):
+            eval_step(cfg, olmo_model, eval_batch, evaluator, device)
+
+            # Log to console.
+            if eval_step_n + 1 == num_eval_batches or (eval_step_n + 1) % cfg.console_log_interval == 0:
+                log.info(f"[eval_step={eval_step_n + 1}/{num_eval_batches}]")
+
+        # Get final metrics.
+        metrics = evaluator.compute_metrics()
+        eval_metrics.update(metrics)
+        Trainer.log_metrics_to_console(f"{evaluator.label}", metrics)
+
+        del eval_batches
+
+    return eval_metrics
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run specified downstream evals for a checkpoint",
+    )
+
+    parser.add_argument("checkpoint_path")
+
+    parser.add_argument("-e", "--downstream-evals", nargs="+", default=[])
+
+    args = parser.parse_args()
+    print(args)
+
+    eval_configs = [
+        EvaluatorConfig(label=eval_name, type=EvaluatorType.downstream) for eval_name in args.downstream_evals
+    ]
+
+    main(args.checkpoint_path, eval_configs)
