2c11e2e1836b86af782694c2695432614183f2b9
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-07-26 15:51:52 -0700

    priority

--- scripts/beaker/ladder-launch.sh
+++ scripts/beaker/ladder-launch.sh
@@ -17,7 +17,7 @@ gantry run \
   --workspace ai2/OLMo-tiny \
   --task-name ladder \
   --description "OLMo ladder with $*" \
-  --priority high \
+  --priority urgent \
   --preemptible \
   --beaker-image shanea/olmo-torch2.2-gantry \
   --cluster ai2/jupiter-cirrascale-2 \
