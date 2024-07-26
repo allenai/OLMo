b0574fcc65d69ce46fe77f74255942b96f8ed0ba
Akshita Bhagia <akshita23bhagia@gmail.com>
2024-07-26 15:34:20 -0700

    more cluster options

--- scripts/beaker/ladder-launch.sh
+++ scripts/beaker/ladder-launch.sh
@@ -21,7 +21,8 @@ gantry run \
   --preemptible \
   --beaker-image shanea/olmo-torch2.2-gantry \
   --cluster ai2/jupiter-cirrascale-2 \
-  --weka=oe-training-default:/weka/oe-training-default \
+  --cluster ai2/pluto-cirrascale \
+  --cluster ai2/allennlp-cirrascale \
   --gpus 8 \
   $MULTI_NODE_ARGS \
   --budget ai2/oe-training \
@@ -37,3 +38,5 @@ gantry run \
   --yes \
   --timeout=-1 \
   -- /bin/bash -c "${COMMAND}"
+
+  # --weka=oe-training-default:/weka/oe-training-default \
