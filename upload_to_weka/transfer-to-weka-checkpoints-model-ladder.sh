5f4e32c9ff734a5247f6e8aba71c55e523cff0d9
Ian <magnusson.i@northeastern.edu>
2024-07-29 16:11:15 -0700

    revert

--- upload_to_weka/transfer-to-weka-checkpoints-model-ladder.sh
+++ upload_to_weka/transfer-to-weka-checkpoints-model-ladder.sh
@@ -1,7 +1,7 @@
 export BUDGET=ai2/oe-eval
 export S3_BUCKET=ai2-llm
-export S3_PREFIX=checkpoints/OLMo-ladder/baseline-1B-2xC/
+export S3_PREFIX=checkpoints/OLMo-ladder/
 export WEKA_BUCKET=oe-eval-default
-export WEKA_PREFIX=ianm/ai2-llm/checkpoints/OLMo-ladder/baseline-1B-2xC/
+export WEKA_PREFIX=ianm/ai2-llm/checkpoints/OLMo-ladder/
 export SYNC_INCLUDE="*unsharded/model.pt"
 export SYNC_INCLUDE_2="*unsharded/config.yaml"
