6a6ebf5c55bb97f70092ba153f34e299c51dc92e
Ian <magnusson.i@northeastern.edu>
2024-07-29 16:40:11 -0700

    just get dclm

--- upload_to_weka/transfer-to-weka-checkpoints-model-ladder.sh
+++ upload_to_weka/transfer-to-weka-checkpoints-model-ladder.sh
@@ -3,5 +3,5 @@ export S3_BUCKET=ai2-llm
 export S3_PREFIX=checkpoints/OLMo-ladder/
 export WEKA_BUCKET=oe-eval-default
 export WEKA_PREFIX=ianm/ai2-llm/checkpoints/OLMo-ladder/
-export SYNC_INCLUDE="*unsharded/model.pt"
-export SYNC_INCLUDE_2="*unsharded/config.yaml"
+export SYNC_INCLUDE="*dclm*unsharded/model.pt"
+export SYNC_INCLUDE_2="*dclm*unsharded/config.yaml"
