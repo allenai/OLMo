4f51f3611ad00fc1fbb4097a0545eb7f15a33bc4
Dustin <drschwenk@gmail.com>
2024-07-31 15:47:28 -0700

    reduce total token run

--- scripts/launch_lc_7b_ct.sh
+++ scripts/launch_lc_7b_ct.sh
@@ -8,8 +8,8 @@ NUM_NODES=4
 
 gantry run \
   --workspace ai2/long-contexts \
-  --task-name long_contexts_7B_cont_train_16K \
-  --description "OLMo medium - 7B - long context continued pretraining 16K CL" \
+  --task-name long_contexts_7B_cont_train_8K \
+  --description "OLMo medium - 7B - long context continued pretraining 8K CL" \
   --priority high \
   --beaker-image petew/olmo-torch23-gantry \
   --cluster ai2/jupiter-cirrascale-2\
