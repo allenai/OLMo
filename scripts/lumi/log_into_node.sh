37e3a9755664258eeafc501ffdb78b75abc728ff
Dirk Groeneveld <dirkg@allenai.org>
2024-08-02 14:02:20 +0300

    Update the log_into_node script to use Shane's container

--- scripts/lumi/log_into_node.sh
+++ scripts/lumi/log_into_node.sh
@@ -7,8 +7,6 @@ srun --interactive --pty --jobid=$1 \
     -B"$PROJECT_DIR:$PROJECT_DIR" \
     -B"$SCRATCH_DIR:$SCRATCH_DIR" \
     -B"$FLASH_DIR:$FLASH_DIR" \
-    -B /opt/cray:/opt/cray \
-    -B /usr/lib64/libcxi.so.1:/usr/lib64/libcxi.so.1 \
     -B /usr/lib64/libjson-c.so.3:/usr/lib64/libjson-c.so.3 \
-    $PROJECT_DIR/containers/llm-lumi-torch22_latest.sif \
-      fish
+    $SIF \
+      bash
