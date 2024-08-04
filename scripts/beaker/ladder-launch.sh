f21aa2afdc488c73f7a56f9094b9a012fa2251a0
Ben Bogin <benb@allenai.org>
2024-08-03 23:26:26 -0700

    add data ablations

--- scripts/beaker/ladder-launch.sh
+++ scripts/beaker/ladder-launch.sh
@@ -14,10 +14,10 @@ else
 fi
 
 gantry run \
-  --workspace ai2/OLMo-tiny \
+  --workspace ai2/cheap_decisions \
   --task-name ladder \
   --description "OLMo ladder with $*" \
-  --priority urgent \
+  --priority high \
   --preemptible \
   --beaker-image shanea/olmo-torch23-gantry \
   --cluster ai2/jupiter-cirrascale-2 \
@@ -28,13 +28,13 @@ gantry run \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
-  --env-secret WANDB_API_KEY=AKSHITAB_WANDB_API_KEY \
-  --env-secret AWS_ACCESS_KEY_ID=AKSHITAB_AWS_ACCESS_KEY_ID \
-  --env-secret AWS_SECRET_ACCESS_KEY=AKSHITAB_AWS_SECRET_ACCESS_KEY \
+  --env-secret WANDB_API_KEY=BENB_WANDB_API_KEY \
+  --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
+  --env-secret AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY \
   --shared-memory 10GiB \
   --venv base \
   --yes \
-  --timeout=-1 \
+  --timeout=0 \
   -- /bin/bash -c "${COMMAND}"
 
   # --cluster ai2/pluto-cirrascale \
