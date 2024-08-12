ff552e73cb8293b8e3cdbed7b8edb31bd6b948fa
Shane A <shanea@allenai.org>
2024-08-12 10:23:16 -0700

    Clean up lr schedule LUMI script

--- scripts/lumi/lr-schedule-amberish1.sh
+++ scripts/lumi/lr-schedule-amberish1.sh
@@ -11,13 +11,11 @@
 #SBATCH --partition=standard-g
 
 module load LUMI/23.09 partition/G
-# module load PyTorch/2.2.2-rocm-5.6.1-python-3.10-singularity-20240617
 
 export OLMO_CONTAINER=llm-lumi-torch22_latest.sif
 
 export CACHED_PATH_CACHE_ROOT=${SCRATCH_DIR}/tmp/${USER}
 export SIF_CONTAINER=$PROJECT_DIR/containers/$OLMO_CONTAINER
-# export SIF_CONTAINER=$SIF
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export MPICH_GPU_SUPPORT_ENABLED=1
@@ -37,17 +35,9 @@ export FI_CXI_DEFAULT_CQ_SIZE=131072
 export PYTHONPATH=.:${PYTHONPATH}
 export ROCM_PATH=/opt/rocm
 export SINGULARITYENV_LD_LIBRARY_PATH=/usr/local/lib:/opt/cray/libfabric/1.15.2.0/lib64
-# export SINGULARITYENV_LD_LIBRARY_PATH=/usr/local/lib:/opt/cray/libfabric/1.15.2.0/lib64:/opt/rocm/lib
 export SINGULARITYENV_TORCH_DIST_INIT_BARRIER=1
 
-# Try playing with max_split_size_mb if you run into OOM errors.
-#export PYTORCH_HIP_ALLOC_CONF=max_split_size_mb:128
-
 export HF_DATASETS_OFFLINE=1
-
-# export DATA_PATH=$FLASH_DIR/preprocessed/olmo-mix
-# export CHECKPOINTS_PATH=$FLASH_DIR/checkpoints
-# export EVAL_DATA_PATH=$SCRATCH_DIR/eval-data
 
 srun \
   --cpus-per-task=$SLURM_CPUS_PER_TASK \
@@ -70,4 +60,5 @@ srun \
       --optimizer.learning_rate=2.2e-4 \
       --scheduler.alpha_f=1.0 \
       --data.num_workers=$SLURM_CPUS_PER_TASK \
-      --save_overwrite
+      --save_overwrite \
+      "${@}"
