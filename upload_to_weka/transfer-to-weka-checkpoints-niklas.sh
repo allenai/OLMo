7ba15c4ff3bbccb28d2eca3424cb1c295d1dda8c
Ian <magnusson.i@northeastern.edu>
2024-08-19 12:07:46 -0700

    niklas to weka

---
+++ upload_to_weka/transfer-to-weka-checkpoints-niklas.sh
@@ -0,0 +1,7 @@
+export BUDGET=ai2/oe-eval
+export S3_BUCKET=ai2-llm
+export S3_PREFIX=checkpoints/OLMoE/
+export WEKA_BUCKET=oe-eval-default
+export WEKA_PREFIX=ianm/ai2-llm/checkpoints/OLMoE/
+export SYNC_INCLUDE="*olmo-1b*"
+export SYNC_INCLUDE_2="*olmo-1b*"
