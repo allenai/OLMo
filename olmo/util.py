1f51ff2a8386ccf12cdba732767b26b68d4b3c75
shmsw25 <sewon@cs.washington.edu>
2024-08-24 14:52:26 -0700

    small change for converting checkpoint

--- olmo/util.py
+++ olmo/util.py
@@ -652,7 +652,7 @@ def _http_get_bytes_range(scheme: str, host_name: str, path: str, bytes_start: i
 
 
 def save_hf_dataset_to_disk(
-    dataset: datasets.DatasetDict | datasets.Dataset,
+    dataset, #: datasets.DatasetDict | datasets.Dataset,
     hf_path: str,
     name: Optional[str],
     split: str,
--- sewon-scripts/convert_checkpoint.sh
+++ sewon-scripts/convert_checkpoint.sh
@@ -1,24 +1,10 @@
 #!/usr/bin/env bash
 
-$CHECKPOINT="s3://lucas-archival/checkpoints/peteish1-baseline/step19000"
 
-command="pip install awscli && pip install -e '.[all]' && python hf_olmo/convert_olmo_to_hf.py \
---checkpoint-dir '$CHECKPOINT' \
---destination-dir '${CHECKPOINT}-hf' \
---keep-olmo-artifact"
+CHECKPOINT="s3://ai2-lucas-archival/checkpoints/peteish1-baseline/step19000"
 
-gantry run \
-    --description "Converting ${CHECKPOINT}" \
-    --no-python \
-    --workspace ai2/oe-training \
-    --priority normal \
-    --gpus 0 \
-    --preemptible \
-    --beaker-image petew/olmo-torch23-gantry \
-    --cluster 'ai2/*' \
-    --budget ai2/oe-training \
-    --env-secret AWS_CONFIG=SEWONM_AWS_CONFIG \
-    --env-secret AWS_CREDENTIALS=SEWONM_AWS_CREDENTIALS \
-    --shared-memory 10GiB \
-    --yes \
-    -- /bin/bash -c "${command}"
+echo $CHECKPOINT
+python hf_olmo/convert_olmo_to_hf.py \
+--checkpoint-dir $CHECKPOINT \
+--destination-dir ${CHECKPOINT}-hf \
+--keep-olmo-artifact
