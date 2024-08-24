269f747d0f03ef74cc68de277293934052dc5c0b
shmsw25 <sewon@cs.washington.edu>
2024-08-23 18:16:30 -0700

    fix path issues

--- sewon-scripts/write_paths.py
+++ sewon-scripts/write_paths.py
@@ -2,8 +2,11 @@
 
 
 with open("output.txt", "w") as f:
-    for shard_idx in range(6):
-        for file_idx in range(50):
-            f.write("    - /weka/oe-training-default/ai2-llm/preprocessed/Spicy-OLMo/SD-B34v0/shard{}/part-00-{}.npy\n".format(shard_idx, str(file_idx).zfill(5)))
-
+    for repeat_idx in range(10):
+        f.write("    # {}\n".format(repeat_idx))
+        for shard_idx in range(6):
+            n_files = 35 if shard_idx==5 else 50
+            for file_idx in range(n_files):
+                f.write("    - /weka/oe-training-default/ai2-llm/preprocessed/Spicy-OLMo/SD-B34v0/shard{}/part-{}-00000.npy\n".format(shard_idx, str(file_idx).zfill(2)))
+        f.write("\n")
 
