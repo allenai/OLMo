f9fb3871fd6db7c77964d23ecc8c8c26e84ddaa1
shmsw25 <sewon@cs.washington.edu>
2024-08-24 13:17:04 -0700

    add SD-B34 baseline and config

--- sewon-scripts/write_paths.py
+++ sewon-scripts/write_paths.py
@@ -4,9 +4,8 @@
 with open("output.txt", "w") as f:
     for repeat_idx in range(10):
         f.write("    # {}\n".format(repeat_idx))
-        for shard_idx in range(6):
-            n_files = 35 if shard_idx==5 else 50
-            for file_idx in range(n_files):
+        for shard_idx in range(5):
+            for file_idx in range(10):
                 f.write("    - /weka/oe-training-default/ai2-llm/preprocessed/Spicy-OLMo/SD-B34v0/shard{}/part-{}-00000.npy\n".format(shard_idx, str(file_idx).zfill(2)))
         f.write("\n")
 
