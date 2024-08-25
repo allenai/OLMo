3b06b0bb7854c71b2c358485e6edbb3ddc24d1d4
shmsw25 <sewon@cs.washington.edu>
2024-08-25 13:58:17 -0700

    remove prints for debugging

--- sewon-scripts/peteish1-debug.sh
+++ sewon-scripts/peteish1-debug.sh
@@ -40,9 +40,6 @@ export NCCL_IB_HCA="^=mlx5_bond_0"
 export NCCL_SOCKET_IFNAME=ib
 # export NCCL_IB_GID_INDEX=0
 
-echo "BEAKER_LEADER_REPLICA_HOSTNAME"
-echo $BEAKER_LEADER_REPLICA_HOSTNAME
-
 torchrun \
   --nnodes "${NUM_NODES}:${NUM_NODES}" \
   --nproc-per-node 1 \
