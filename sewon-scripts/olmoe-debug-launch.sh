8faffa59266bb3f34a87827abfbd816e54768cc5
shmsw25 <sewon@cs.washington.edu>
2024-08-26 07:46:47 -0700

    small fix

--- sewon-scripts/olmoe-debug-launch.sh
+++ sewon-scripts/olmoe-debug-launch.sh
@@ -3,7 +3,7 @@ set -ex
 
 CONFIG_NAME=olmoe
 CONFIG_PATH=sewon-configs/${CONFIG_NAME}.yaml
-ARGS="--run_name=olmoe-8x1b-newhp-newds-final --save-overwrite --fsdp.sharding_strategy=FULL_SHARD --device_train_microbatch_size=4 --canceled_check_interval=9999999 '--load_path=\${path.last_checkpoint:\${save_folder}}'"
+ARGS="--run_name=${CONFIG_NAME} --save-overwrite --fsdp.sharding_strategy=FULL_SHARD --device_train_microbatch_size=4 --canceled_check_interval=9999999 '--load_path=\${path.last_checkpoint:\${save_folder}}'"
 
 NUM_NODES=1
 NUM_PROCS=1
