b8aee6d12ee343ee97ece94ce76c5bf51c9cf29a
shmsw25 <sewon@cs.washington.edu>
2024-08-29 14:44:37 -0700

    .

--- sewon-scripts/convert_checkpoint.sh
+++ sewon-scripts/convert_checkpoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CHECKPOINT="s3://ai2-lucas-archival/checkpoints/peteish1-B34v0/step19000"
+CHECKPOINT="s3://ai2-lucas-archival/checkpoints/peteish1-B34v0/latest"
 
 python hf_olmo/convert_olmo_to_hf.py \
     --checkpoint-dir $CHECKPOINT \
