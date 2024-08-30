6826343ec6a094e98d4fb86bcf4dd6f097223646
shmsw25 <sewon@cs.washington.edu>
2024-08-30 12:06:28 -0700

    add a script for a higher LR

--- sewon-scripts/convert_checkpoint.sh
+++ sewon-scripts/convert_checkpoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CHECKPOINT="s3://ai2-lucas-archival/checkpoints/peteish1-B34v0/latest"
+CHECKPOINT="s3://ai2-lucas-archival/checkpoints/peteish7-anneal-B34v0x10/latest"
 
 python hf_olmo/convert_olmo_to_hf.py \
     --checkpoint-dir $CHECKPOINT \
