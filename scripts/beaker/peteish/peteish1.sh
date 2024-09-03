73103a044de7023716453c7a7e1e0a7815e8083c
Shane A <shanea@allenai.org>
2024-09-03 08:42:55 -0700

    Set an ephemeral save interval in case of preemption

--- scripts/beaker/peteish/peteish1.sh
+++ scripts/beaker/peteish/peteish1.sh
@@ -52,7 +52,7 @@ torchrun \
     configs/peteish1-weka.yaml \
       --run_name="${GANTRY_TASK_NAME}" \
       --try_load_latest_save \
-      --save_interval_ephemeral=null \
+      --save_interval_ephemeral=100 \
       --fsdp.sharding_strategy=HYBRID_SHARD \
       --fsdp.precision=fp32_params \
       --load_path=/weka/oe-training-default/ai2-llm/checkpoints/OLMo-small/peteish1/step0 \
