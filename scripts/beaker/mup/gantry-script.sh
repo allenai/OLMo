d20ee3232e3d98b2fb6bf47ed52879c2bb9d8001
Shane A <shanea@allenai.org>
2024-09-05 12:11:41 -0700

    Setup mup run scripts

--- scripts/beaker/mup/gantry-script.sh
+++ scripts/beaker/mup/gantry-script.sh
@@ -2,40 +2,44 @@
 
 set -ex
 
-NUM_NODES=1
+NUM_NODES=$1
+shift
+
+WIDTH=$1
+shift
+
+LR=$1
+shift
 
 gantry run \
-  --workspace ai2/OLMo-training \
-  --task-name mup-olmo \
-  --description "Check if wider is better with mup" \
+  --workspace ai2/shanea \
+  --task-name mup-olmo-lr-search \
+  --description "Search for a good LR for OLMo mup" \
   --priority normal \
   --preemptible \
   --beaker-image petew/olmo-torch23-gantry \
   --cluster ai2/jupiter-cirrascale-2 \
-  --cluster ai2/pluto-cirrascale \
-  --cluster ai2/allennlp-cirrascale \
   --gpus 8 \
   --replicas "${NUM_NODES}" \
   --leader-selection \
   --host-networking \
   --budget ai2/oe-training \
   --no-nfs \
+  --weka oe-training-default:/weka/oe-training-default \
   --propagate-failure \
+  --propagate-preemption \
+  --no-python \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
-  --env-secret WANDB_API_KEY=AKSHITAB_WANDB_API_KEY \
-  --env-secret AWS_ACCESS_KEY_ID=AKSHITAB_AWS_ACCESS_KEY_ID \
-  --env-secret AWS_SECRET_ACCESS_KEY=AKSHITAB_AWS_SECRET_ACCESS_KEY \
   --env R2_PROFILE=R2 \
   --env S3_PROFILE=S3 \
   --env WEKA_PROFILE=WEKA \
-  --env-secret AWS_CONFIG=PETEW_AWS_CONFIG \
-  --env-secret AWS_CREDENTIALS=PETEW_AWS_CREDENTIALS \
+  --env-secret AWS_CONFIG=AWS_CONFIG \
+  --env-secret AWS_CREDENTIALS=AWS_CREDENTIALS \
   --env-secret R2_ENDPOINT_URL=R2_ENDPOINT_URL \
-  --env-secret WEKA_ENDPOINT_URL=WEKA_ENDPOINT_URL \
+  --env-secret WANDB_API_KEY=WANDB_API_KEY \
   --shared-memory 10GiB \
-  --venv base \
   --yes \
   --timeout=-1 \
-  -- /bin/bash -c "scripts/beaker/mup/torchrun-script.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
+  -- /bin/bash -c "scripts/beaker/mup/torchrun-script.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK ${WIDTH} ${LR}"
--- scripts/beaker/mup/torchrun-script.sh
+++ scripts/beaker/mup/torchrun-script.sh
@@ -11,6 +11,12 @@ shift
 BEAKER_REPLICA_RANK=$1
 shift
 
+WIDTH=$1
+shift
+
+LR=$1
+shift
+
 # Warm HF cache
 mkdir -p /root/.cache
 pushd /root/.cache
@@ -26,27 +32,25 @@ printenv AWS_CREDENTIALS > ~/.aws/credentials
 # Temporary, since it is not part of the image yet.
 pip install mup@git+https://github.com/microsoft/mup#egg=19814971934ef91dd546f88e913fc963e096d11c
 
-for WIDTH in 128 256 512 1024 2048 4096;
-do
-  torchrun \
-    --nnodes ${NUM_NODES}:${NUM_NODES} \
-    --nproc-per-node 8 \
-    --rdzv_id=101 \
-    --rdzv_backend=c10d \
-    --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
-    --node_rank=$BEAKER_REPLICA_RANK \
-    scripts/train.py \
-      configs/mup/base-olmo-cosine.yaml \
-        --run_name="cosine_mup_olmo_$WIDTH" \
-        --wandb.name="cosine_mup_olmo_$WIDTH" \
-        --wandb.group="cosine_mup_olmo" \
-        --wandb.project=olmo-mup \
-        --stop_at=1000 \
-        --model.use_mup \
-	      --model.mup_base_shapes=scripts/beaker/mup/base_olmo_shapes.bsh \
-        --model.d_model=$WIDTH \
-        --scheduler.t_warmup=$((3 * $WIDTH / 128))
-done
+torchrun \
+  --nnodes ${NUM_NODES}:${NUM_NODES} \
+  --nproc-per-node 8 \
+  --rdzv_id=101 \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  --node_rank=$BEAKER_REPLICA_RANK \
+  scripts/train.py \
+    configs/mup/base-olmo-cosine.yaml \
+      --run_name="cosine_mup_olmo_$WIDTH" \
+      --wandb.name="cosine_mup_olmo_$WIDTH" \
+      --wandb.group="cosine_mup_olmo" \
+      --wandb.project=olmo-mup \
+      --stop_at=1000 \
+      --model.use_mup \
+      --model.mup_base_shapes=scripts/beaker/mup/base_olmo_shapes.bsh \
+      --model.d_model=$WIDTH \
+      --optimizer.learning_rate=$LR \
+      --scheduler.t_warmup=$((3 * $WIDTH / 128))
 
 #for WIDTH in 128 256 512 1024 2048 4096;
 #do
