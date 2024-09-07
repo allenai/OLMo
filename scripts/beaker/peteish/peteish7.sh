6d395d74119d1b42e9448769f7eccf660a9f9f6d
Dirk Groeneveld <dirkg@allenai.org>
2024-09-07 00:29:53 -0700

    Shard better

--- scripts/beaker/peteish/peteish7.sh
+++ scripts/beaker/peteish/peteish7.sh
@@ -55,7 +55,7 @@ torchrun \
     ${CONFIG_PATH} \
       --save_interval_ephemeral=null \
       --fsdp.sharding_strategy=HYBRID_SHARD \
-      --fsdp.hybrid_sharding_num_model_replicas=2 \
+      --fsdp.hybrid_sharding_num_model_replicas=${NUM_NODES} \
       --save_overwrite
 
      # '--load_path=${path.last_checkpoint:${save_folder}}' \
