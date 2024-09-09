2ed6a50e60e686a448e3320749b4e97e71428c2d
Jena Hwang <jenah@allenai.org>
2024-09-09 15:23:34 -0700

    convert checkpoint batch

--- scripts/convert_checkpoints_batch.py
+++ scripts/convert_checkpoints_batch.py
@@ -10,28 +10,36 @@ Requires AWS CLI to be installed and configured.
 import argparse
 import subprocess
 import os
+import time
+import boto3
 import json
 
-SANITY_CHECK = False
+SANITY_CHECK = True
 
 def convert_checkpoint(cps, load_dir="/data/input"):
-    cps = expand_paths(cps)
+    s3_client = boto3.client('s3')
+    s3_resource = boto3.resource('s3')
+
+    cps = expand_paths(cps, s3_client)
     processed = []
 
     for checkpoint_path in cps:
         # Convert to old-style checkpoint.
-
         retain_path_name = checkpoint_path.replace('s3://', '').strip('/')
         weka_loc = f"{load_dir}/{retain_path_name}-hf/"
 
         # Check if the output location is already there. If not, do the conversion.
         if os.path.exists(weka_loc):
             conversion = 'existing'
+            converted_path = weka_loc
+        elif s3_path_exists(checkpoint_path, s3_resource):
+            conversion = 'existing'
+            converted_path = checkpoint_path + '-hf'
         else:
             conversion = 'new'
+            converted_path = weka_loc
+
             conversion_cmd = f"python hf_olmo/convert_olmo_to_hf.py --checkpoint-dir '{checkpoint_path}' --destination-dir '{weka_loc}' --keep-olmo-artifacts --tokenizer 'allenai/gpt-neox-olmo-dolma-v1_5.json'"
-
-
 
             if SANITY_CHECK:
                 print(conversion_cmd)
@@ -40,21 +48,41 @@ def convert_checkpoint(cps, load_dir="/data/input"):
 
         processed.append({
             'unproccessed_path': checkpoint_path,
-            'converted_path': weka_loc,
-            'convertion': conversion})
+            'converted_path': converted_path.replace(load_dir,'/weka'),
+            'convertion': conversion,
+            'date_time': time.strftime('%b-%d-%Y_%H%M', time.localtime())})
 
-    with open('/data/input/jenah/log.jsonl','a+') as fout:
+    print(processed)
+    with open('log.jsonl','a+') as fout:
         for p in processed:
             fout.write(json.dumps(p)+'\n')
 
-def expand_paths(cps):
+
+def s3_path_exists(cp, s3):
+    b = cp.split('/')[2]
+    bucket = s3.Bucket(b)
+    objs = list(bucket.objects.filter(Prefix=cp.replace('s3://'+b+'/', '') + '-hf'))
+    return True if (len(objs) > 0) else False
+
+
+def expand_paths(cps, s3):
     expanded = []
     for cp in cps:
+        bucket = cp.split('/')[2]
         segs = cp.split('*')
-        prefix = 's3://ai2-llm/'
-        cmd = f"aws s3 ls --recursive {segs[0]}"
-        all_dirs = subprocess.run(cmd, shell=True, check=True, capture_output=True, text = True).stdout
-        relevant_dirs = ['/'.join(d.split()[-1].split('/')[:-1]) for d in all_dirs.split() if 'model.pt' in d]
+
+        # cmd = f"aws s3 ls --recursive {segs[0]}"
+        # all_dirs = subprocess.run(cmd, shell=True, check=True, capture_output=True, text = True).stdout
+        # relevant_dirs = ['/'.join(d.split()[-1].split('/')[:-1]) for d in all_dirs.split() if 'model.pt' in d]
+
+        relevant_dirs = []
+        paginator = s3.get_paginator('list_objects_v2')
+        page_iterator = paginator.paginate(Bucket=bucket, Prefix=segs[0].replace('s3://'+bucket+'/', ''))
+        for page in page_iterator:
+            for obj in page['Contents']:
+                if 'model.pt' in obj["Key"]:
+                    relevant_dirs.append(obj["Key"].replace('/model.pt',''))
+
         search_segs = [seg for i, seg in enumerate(segs) if i > 0 and seg != ""]
 
         print(f"search segments: {search_segs}")
@@ -64,8 +92,7 @@ def expand_paths(cps):
             for s in search_segs:
                 temp_dirs = [d for d in temp_dirs if s in d]
 
-        exp = set([f"{prefix}{d}" for d in temp_dirs])
-        print(exp)
+        exp = set([f"s3://{bucket}/{d}" for d in temp_dirs])
 
         expanded += exp
     return expanded
