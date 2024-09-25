790ae811504c4678614e06b09315788d1daac93b
Jiacheng Liu <liujch1998@gmail.com>
2024-09-25 21:23:16 +0000

    Fix weka path of data and checkpoints

--- scripts/ladder_peteish.py
+++ scripts/ladder_peteish.py
@@ -188,7 +188,7 @@ def config_from_args(args: argparse.Namespace) -> TrainConfig:
             read_location = "/weka/oe-training-default/ai2-llm"
     read_location.rstrip("/")
 
-    remote_save_folder = f"s3://ai2-llm/checkpoints/OLMo-ladder/{run_name}"
+    remote_save_folder = f"{read_location}/checkpoints/OLMo-ladder/{run_name}"
     load_path = args.load_path
     if load_path is None:
         load_path = find_latest_checkpoint(remote_save_folder)
--- scripts/ladder_peteish.sh
+++ scripts/ladder_peteish.sh
@@ -1,27 +1,27 @@
-./scripts/beaker/ladder_peteish-launch.sh 4 --model 150M --data olmoe-mix-0924 --length 1xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 4 --model 320M --data olmoe-mix-0924 --length 1xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 530M --data olmoe-mix-0924 --length 1xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 680M --data olmoe-mix-0924 --length 1xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 1B --data olmoe-mix-0924 --length 1xC --name peteish --s3 --save_overwrite
-# ./scripts/beaker/ladder_peteish-launch.sh 8 --model 3B --data olmoe-mix-0924 --length 1xC --name peteish --s3 --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 4 --model 150M --data olmoe-mix-0924 --length 1xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 4 --model 320M --data olmoe-mix-0924 --length 1xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 530M --data olmoe-mix-0924 --length 1xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 680M --data olmoe-mix-0924 --length 1xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 1B --data olmoe-mix-0924 --length 1xC --name peteish --save_overwrite
+# ./scripts/beaker/ladder_peteish-launch.sh 8 --model 3B --data olmoe-mix-0924 --length 1xC --name peteish --save_overwrite
 
-./scripts/beaker/ladder_peteish-launch.sh 4 --model 150M --data olmoe-mix-0924 --length 2xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 4 --model 320M --data olmoe-mix-0924 --length 2xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 530M --data olmoe-mix-0924 --length 2xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 680M --data olmoe-mix-0924 --length 2xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 1B --data olmoe-mix-0924 --length 2xC --name peteish --s3 --save_overwrite
-# ./scripts/beaker/ladder_peteish-launch.sh 8 --model 3B --data olmoe-mix-0924 --length 2xC --name peteish --s3 --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 4 --model 150M --data olmoe-mix-0924 --length 2xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 4 --model 320M --data olmoe-mix-0924 --length 2xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 530M --data olmoe-mix-0924 --length 2xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 680M --data olmoe-mix-0924 --length 2xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 1B --data olmoe-mix-0924 --length 2xC --name peteish --save_overwrite
+# ./scripts/beaker/ladder_peteish-launch.sh 8 --model 3B --data olmoe-mix-0924 --length 2xC --name peteish --save_overwrite
 
-./scripts/beaker/ladder_peteish-launch.sh 4 --model 150M --data olmoe-mix-0924 --length 5xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 4 --model 320M --data olmoe-mix-0924 --length 5xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 530M --data olmoe-mix-0924 --length 5xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 680M --data olmoe-mix-0924 --length 5xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 1B --data olmoe-mix-0924 --length 5xC --name peteish --s3 --save_overwrite
-# ./scripts/beaker/ladder_peteish-launch.sh 8 --model 3B --data olmoe-mix-0924 --length 5xC --name peteish --s3 --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 4 --model 150M --data olmoe-mix-0924 --length 5xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 4 --model 320M --data olmoe-mix-0924 --length 5xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 530M --data olmoe-mix-0924 --length 5xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 680M --data olmoe-mix-0924 --length 5xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 1B --data olmoe-mix-0924 --length 5xC --name peteish --save_overwrite
+# ./scripts/beaker/ladder_peteish-launch.sh 8 --model 3B --data olmoe-mix-0924 --length 5xC --name peteish --save_overwrite
 
-./scripts/beaker/ladder_peteish-launch.sh 4 --model 150M --data olmoe-mix-0924 --length 10xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 4 --model 320M --data olmoe-mix-0924 --length 10xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 530M --data olmoe-mix-0924 --length 10xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 680M --data olmoe-mix-0924 --length 10xC --name peteish --s3 --save_overwrite
-./scripts/beaker/ladder_peteish-launch.sh 8 --model 1B --data olmoe-mix-0924 --length 10xC --name peteish --s3 --save_overwrite
-# ./scripts/beaker/ladder_peteish-launch.sh 8 --model 3B --data olmoe-mix-0924 --length 10xC --name peteish --s3 --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 4 --model 150M --data olmoe-mix-0924 --length 10xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 4 --model 320M --data olmoe-mix-0924 --length 10xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 530M --data olmoe-mix-0924 --length 10xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 680M --data olmoe-mix-0924 --length 10xC --name peteish --save_overwrite
+./scripts/beaker/ladder_peteish-launch.sh 8 --model 1B --data olmoe-mix-0924 --length 10xC --name peteish --save_overwrite
+# ./scripts/beaker/ladder_peteish-launch.sh 8 --model 3B --data olmoe-mix-0924 --length 10xC --name peteish --save_overwrite
