fb1f2031bad4681a2676209e74c34375203a07d6
Dirk Groeneveld <dirkg@allenai.org>
2024-09-26 15:49:16 -0700

    Settings that actually turn this off

--- scripts/beaker/peteish/peteish13-highlr-noib.sh
+++ scripts/beaker/peteish/peteish13-highlr-noib.sh
@@ -31,8 +31,8 @@ export TORCH_DIST_INIT_BARRIER=1
 export OLMO_SHARED_FS=1
 
 export NCCL_DEBUG=INFO
-export NCCL_IB_HCA="^=mlx"
-# export NCCL_SOCKET_IFNAME=en
+export NCCL_IB_HCA="^mlx"
+export NCCL_SOCKET_IFNAME="=bond0"
 # export NCCL_IB_GID_INDEX=0
 
 torchrun \
