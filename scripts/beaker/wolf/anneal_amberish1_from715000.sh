f2732789c36fb64790e5e5553b638d604c900911
Jiacheng Liu <liujch1998@gmail.com>
2024-09-27 00:32:37 +0000

    Update anneal

--- scripts/beaker/wolf/anneal_amberish1_from715000.sh
+++ scripts/beaker/wolf/anneal_amberish1_from715000.sh
@@ -58,7 +58,7 @@ gantry run \
     torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 8 --rdzv_id=20310 --rdzv_backend=static --rdzv_conf='read_timeout=1200' --rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400 --node_rank=\$BEAKER_REPLICA_RANK \
         scripts/train.py ${CONFIG_PATH} \
         --run_name=${RUN_NAME} \
-        --wandb.project=hb-wolf-olmo-2 --wandb.entity=liujch1998 \
+        --wandb.project=olmo-small --wandb.entity=ai2-llm \
         --save_folder=/weka/oe-training-default/ai2-llm/checkpoints/OLMo-small/${RUN_NAME} --save_overwrite=true --load_path=${LOAD_PATH} \
         --optimizer.learning_rate=${LEARNING_RATE} \
         --device_train_microbatch_size=4 \
--- scripts/unshard_and_convert.sh
+++ scripts/unshard_and_convert.sh
@@ -1,8 +1,8 @@
-MODEL_NAME="v2.7_v2.5_vera_no-infgram"
-CKPT="step11234"
+MODEL_NAME="anneal_v2.10_from715000"
+CKPT="step11931"
 CKPT_NAME="${MODEL_NAME}_${CKPT}"
 
 aws s3 --profile weka-aus cp --recursive s3://oe-training-default/wolf/ckpt/${MODEL_NAME}/${CKPT} ./ckpt_sharded/${CKPT_NAME}
 python scripts/unshard.py ./ckpt_sharded/${CKPT_NAME} ./ckpt_unsharded/${CKPT_NAME} --model-only
 python hf_olmo/convert_olmo_to_hf.py --checkpoint-dir ./ckpt_unsharded/${CKPT_NAME}
-python scripts/convert_olmo_to_hf_new.py --input_dir ./ckpt_unsharded/${CKPT_NAME} --output_dir ./ckpt_transformers/${CKPT_NAME} --tokenizer_json_path olmo_data/tokenizers/allenai_gpt-neox-olmo-dolma-v1_5.json
+# python scripts/convert_olmo_to_hf_new.py --input_dir ./ckpt_unsharded/${CKPT_NAME} --output_dir ./ckpt_transformers/${CKPT_NAME} --tokenizer_json_path olmo_data/tokenizers/allenai_gpt-neox-olmo-dolma-v1_5.json
