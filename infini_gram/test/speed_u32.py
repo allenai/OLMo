df16b44cb315d756ea87cc243c332c95a571b9f2
Jiacheng Liu <liujch1998@gmail.com>
2024-09-27 22:48:32 +0000

    Add speed test; Add peteish config and bump to V3

--- infini_gram/test/speed_u32.py
+++ infini_gram/test/speed_u32.py
@@ -6,25 +6,25 @@ import torch
 from olmo.config import InfgramConfig
 from infini_gram import InfiniGramEngine
 
-index_dir = '../hb-wolf/index/v5_dolma-v1_7-wiki_olmo'
-cfg = InfgramConfig(index_dir=index_dir)
+index_dir = '../hb-wolf/index/v5_olmoe-mix-0924-wiki_dolma2'
+cfg = InfgramConfig(index_dir=index_dir, dtype='u32')
 max_batch_size = 1024
 max_seq_len = 4096
 
-engine = InfiniGramEngine(cfg, eos_token_id=50279, max_batch_size_per_device=max_batch_size, max_seq_len=max_seq_len, local_rank=0, global_rank=0, local_world_size=1, world_size=1)
+engine = InfiniGramEngine(cfg, eos_token_id=100257, max_batch_size_per_device=max_batch_size, max_seq_len=max_seq_len, local_rank=0, global_rank=0, local_world_size=1, world_size=1)
 
 with open(os.path.join(index_dir, 'tokenized.0'), 'rb') as f:
     ds = f.read()
-tok_cnt = len(ds) // 2
+tok_cnt = len(ds) // 4
 
 for batch_size in [1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]:
     all_latency_ms = []
     for it in range(110):
         tok_offset = random.randint(0, tok_cnt - batch_size * max_seq_len)
-        input_buf = ds[tok_offset * 2:(tok_offset + batch_size * max_seq_len) * 2]
-        input_idss = np.frombuffer(input_buf, dtype=np.uint8).view(np.uint16).astype(np.int64).reshape(batch_size, max_seq_len)
+        input_buf = ds[tok_offset * 4:(tok_offset + batch_size * max_seq_len) * 4]
+        input_idss = np.frombuffer(input_buf, dtype=np.uint8).view(np.uint32).astype(np.int64).reshape(batch_size, max_seq_len)
         input_idss = torch.tensor(input_idss, dtype=torch.int64)
-        input_idss[input_idss == 65535] = 50279
+        input_idss[input_idss == 2**32-1] = 100257
         if 'v5' in index_dir:
             input_idss = input_idss.flip(1)
         start_time = time.time()
