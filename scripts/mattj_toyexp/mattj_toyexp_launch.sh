5a541b243d36d26486a47f31232c0c09522743b9
Matt Jordan <mattj@allenai.org>
2024-10-02 10:53:55 -0700

    mooore logs

--- scripts/mattj_toyexp/mattj_toyexp_launch.sh
+++ scripts/mattj_toyexp/mattj_toyexp_launch.sh
@@ -36,4 +36,4 @@ gantry run \
   --shared-memory 10GiB \
   --yes \
   --timeout=-1 \
-  -- echo $(git status); echo $(ls .); echo $(ls scripts/*); /bin/bash -c "scripts/mattj_toyexp/mattj_toyexp_setup.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
+  -- echo $(git status); echo $(ls .); echo $(ls scripts/*); echo $(printenv); /bin/bash -c "scripts/mattj_toyexp/mattj_toyexp_setup.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
