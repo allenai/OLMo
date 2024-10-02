9ae9f73679ad700b4b9b31a7c33002cb000b57c7
Matt Jordan <mattj@allenai.org>
2024-10-02 13:11:57 -0700

    trying saturn with en

--- scripts/mattj_toyexp/mattj_toyexp_setup.sh
+++ scripts/mattj_toyexp/mattj_toyexp_setup.sh
@@ -37,7 +37,7 @@ export OLMO_SHARED_FS=1
 
 export NCCL_DEBUG=INFO
 export NCCL_IB_HCA="^=mlx5_bond_0"
-export NCCL_SOCKET_IFNAME=eno0,eno1
+export NCCL_SOCKET_IFNAME=en
 # export NCCL_IB_GID_INDEX=0
 
 torchrun \
