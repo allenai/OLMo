a80782dcbf85ea9c19197a491e5b75bdd7840daf
Shane A <shanea@allenai.org>
2024-10-02 15:48:24 -0700

    Update LUMI scripts to use new container

--- scripts/pyspy_all_nodes.sh
+++ scripts/pyspy_all_nodes.sh
@@ -7,8 +7,6 @@ srun --overlap --jobid $1 \
     -B"$PROJECT_DIR:$PROJECT_DIR" \
     -B"$SCRATCH_DIR:$SCRATCH_DIR" \
     -B"$FLASH_DIR:$FLASH_DIR" \
-    -B /opt/cray:/opt/cray \
-    -B /usr/lib64/libcxi.so.1:/usr/lib64/libcxi.so.1 \
-    -B /usr/lib64/libjson-c.so.3:/usr/lib64/libjson-c.so.3 \
-    $PROJECT_DIR/containers/llm-lumi_latest.sif \
+    -B /var/spool/slurmd,/opt/cray/,/usr/lib64/libcxi.so.1,/usr/lib64/libjansson.so.4,/usr/lib64/libjson-c.so.3 \
+    $OLMO_CONTAINER \
       bash scripts/pyspy_all_processes.sh | sort -s -t: -k1,1
