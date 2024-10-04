47eb7c6a9943c63f3546203415f8372f1aef9964
Shane A <shanea@allenai.org>
2024-10-03 23:32:06 -0700

    Try fix test

--- tests/train_test.py
+++ tests/train_test.py
@@ -81,7 +81,7 @@ def _compare_module_output(
             mismatching_element_count == 0
         ), f"Number of {module_name} mis-matching inputs: {mismatching_element_count}"
 
-    if (norm := torch.linalg.vector_norm((new_input - original_input).float())) > 1e-8:
+    if (norm := torch.linalg.vector_norm((new_input - original_input).float())) > 0.0:
         logger.info("Difference of norm of %s input is non-trivial: %f", module_name, norm)
     assert_close(
         new_input, original_input, msg=lambda msg: f"{module_name} inputs are not sufficiently close.\n{msg}"
@@ -97,7 +97,7 @@ def _compare_module_output(
         assert (
             original_output.shape == new_output.shape
         ), f"{module_name} output shape is different for new model. Original {original_output.shape}, new {new_output.shape}"
-        if (norm := torch.linalg.vector_norm((new_output - original_output).float())) > 1e-8:
+        if (norm := torch.linalg.vector_norm((new_output - original_output).float())) > 0.0:
             logger.info("Difference of norm of %s output is non-trivial: %f", module_name, norm)
         assert_close(
             new_output,
