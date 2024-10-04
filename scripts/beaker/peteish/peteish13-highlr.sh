b699753d7dda38a159b169e15557a46f72d40d65
Dirk Groeneveld <dirkg@allenai.org>
2024-10-04 13:01:26 -0700

    compile.dynamic=false

--- scripts/beaker/peteish/peteish13-highlr.sh
+++ scripts/beaker/peteish/peteish13-highlr.sh
@@ -58,6 +58,7 @@ torchrun \
       --activation_checkpointing=null \
       --compile.mode=default \
       --compile.fullgraph=false \
+      --compile.dynamic=false \
       --fused_loss=true \
       --model.flash_attention=true \
       --data.num_workers=8 \
