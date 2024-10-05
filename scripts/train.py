d3d39a045642390d9337eede781969857d796d64
Dirk Groeneveld <dirkg@allenai.org>
2024-10-05 05:55:37 +0000

    Set device when initializing the process group

--- scripts/train.py
+++ scripts/train.py
@@ -326,7 +326,7 @@ if __name__ == "__main__":
 
     # Initialize process group.
     torch.cuda.set_device(f"cuda:{get_local_rank()}")   # Set this early to prevent GPU 0 from picking up a bunch of tensors it shouldn't have.
-    dist.init_process_group(backend="nccl", timeout=timedelta(minutes=30))
+    dist.init_process_group(backend="nccl", timeout=timedelta(minutes=30), device_id=get_local_rank())
     log.info("Process group initialized")
 
     prepare_cli_environment()
