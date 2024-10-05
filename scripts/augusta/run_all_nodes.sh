7cf11d857c3b36c75bc941b5cfa54b882c0fdb28
Dirk Groeneveld <dirkg@allenai.org>
2024-10-05 22:39:12 +0000

    Script to run something on all nodes

---
+++ scripts/augusta/run_all_nodes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NCCL_LIB_DIR=/var/lib/tcpxo/lib64 source /var/lib/tcpxo/lib64/nccl-env-profile.sh
+
+set -euxo pipefail
+
+HOST_VARS=$(sed 's/ \{1,\}/ -x /g' <<<"${!NCCL*} LD_LIBRARY_PATH")
+mpirun \
+  --mca btl self,tcp \
+  --mca btl_tcp_if_include enp0s12 \
+  --hostfile ~/hostfile \
+  -npernode 1 \
+  -x ${HOST_VARS} \
+  -x WANDB_ENTITY \
+  -x S3_ACCESS_KEY_ID \
+  -x S3_SECRET_ACCESS_KEY \
+  -x AWS_ACCESS_KEY_ID \
+  -x AWS_SECRET_ACCESS_KEY \
+  -x DEBIAN_FRONTEND=noninteractive \
+  "$@"
