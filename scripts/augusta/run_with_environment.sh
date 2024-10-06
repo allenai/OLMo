b52f2283f1ff6fa7f61caf3a63f51a2117c545bd
Dirk Groeneveld <dirkg@allenai.org>
2024-10-06 22:02:59 +0000

    Silence warning

--- scripts/augusta/run_with_environment.sh
+++ scripts/augusta/run_with_environment.sh
@@ -11,4 +11,5 @@ cd ~/OLMo
 NCCL_LIB_DIR=/var/lib/tcpxo/lib64 source /var/lib/tcpxo/lib64/nccl-env-profile.sh
 export NCCL_NET=FasTrak
 HOST_NODE_ADDR=augusta-vms-0001:12345
+export OMP_NUM_THREADS=16
 torchrun --nproc_per_node=8 --nnodes=$NUM_NODES --rdzv-backend=c10d --rdzv-endpoint=$HOST_NODE_ADDR scripts/train.py "$@"
