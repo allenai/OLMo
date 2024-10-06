59f2247b37e3da4720b77f6eccd09f68990d9437
Dirk Groeneveld <dirkg@allenai.org>
2024-10-06 21:25:35 +0000

    Specify the number of nodes on the command line

--- scripts/augusta/run_with_environment.sh
+++ scripts/augusta/run_with_environment.sh
@@ -2,9 +2,12 @@
 
 set -euxo pipefail
 
+NUM_NODES=$1
+shift
+
 cd ~/OLMo
 source OLMo/bin/activate
 NCCL_LIB_DIR=/var/lib/tcpxo/lib64 source /var/lib/tcpxo/lib64/nccl-env-profile.sh
 export NCCL_NET=FasTrak
 HOST_NODE_ADDR=augusta-vms-0001:12345
-torchrun --nproc_per_node=8 --nnodes=2 --rdzv-backend=c10d --rdzv-endpoint=$HOST_NODE_ADDR scripts/train.py "$@"
+torchrun --nproc_per_node=8 --nnodes=$NUM_NODES --rdzv-backend=c10d --rdzv-endpoint=$HOST_NODE_ADDR scripts/train.py "$@"
