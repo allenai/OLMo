def698750fbd0b2bdf6e2b51d9ae736fe3ad7d3d
Dirk Groeneveld <dirkg@allenai.org>
2024-10-06 22:06:46 +0000

    More informative logs

--- scripts/augusta/run_with_environment.sh
+++ scripts/augusta/run_with_environment.sh
@@ -12,4 +12,10 @@ NCCL_LIB_DIR=/var/lib/tcpxo/lib64 source /var/lib/tcpxo/lib64/nccl-env-profile.s
 export NCCL_NET=FasTrak
 HOST_NODE_ADDR=augusta-vms-0001:12345
 export OMP_NUM_THREADS=16
+
+# Redirect stdout and stderr so that we get a prefix with the node name
+export NODENAME=$(hostname -s)
+exec > >(trap "" INT TERM; sed -u "s/^/$NODENAME out: /")
+exec 2> >(trap "" INT TERM; sed -u "s/^/$NODENAME err: /" >&2)
+
 torchrun --nproc_per_node=8 --nnodes=$NUM_NODES --rdzv-backend=c10d --rdzv-endpoint=$HOST_NODE_ADDR scripts/train.py "$@"
