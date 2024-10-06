ea5ca3f9b48906246a1b0e2518d1ef865f0acd29
Dirk Groeneveld <dirkg@allenai.org>
2024-10-06 21:36:23 +0000

    Correct new path of the venv

--- scripts/augusta/run_with_environment.sh
+++ scripts/augusta/run_with_environment.sh
@@ -5,8 +5,9 @@ set -euxo pipefail
 NUM_NODES=$1
 shift
 
+source ~/venv/OLMo/bin/activate
+
 cd ~/OLMo
-source OLMo/bin/activate
 NCCL_LIB_DIR=/var/lib/tcpxo/lib64 source /var/lib/tcpxo/lib64/nccl-env-profile.sh
 export NCCL_NET=FasTrak
 HOST_NODE_ADDR=augusta-vms-0001:12345
