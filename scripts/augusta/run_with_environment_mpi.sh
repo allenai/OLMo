f8ef856169ff9a74b9ab1156b8aa1ff1232a408c
Dirk Groeneveld <dirkg@allenai.org>
2024-10-15 15:01:40 -0700

    Reshuffle for better logs

--- scripts/augusta/run_with_environment_mpi.sh
+++ scripts/augusta/run_with_environment_mpi.sh
@@ -4,14 +4,6 @@ set -exuo pipefail
 FIRST_HOST=$1
 shift
 
-source ~/venv/OLMo/bin/activate
-
-cd ~/OLMo
-NCCL_LIB_DIR=/var/lib/tcpxo/lib64 source /var/lib/tcpxo/lib64/nccl-env-profile.sh
-export NCCL_NET=FasTrak
-export OMP_NUM_THREADS=16
-export GOOGLE_CLOUD_PROJECT=h100-cluster-owner
-
 export NODENAME=$(hostname -s)
 export MASTER_ADDR=$FIRST_HOST
 export MASTER_PORT=39591
@@ -24,6 +16,14 @@ export NODE_RANK=$((($RANK - $LOCAL_RANK) / $LOCAL_WORLD_SIZE))
 # Redirect stdout and stderr so that we get a prefix with the node name
 exec > >(trap "" INT TERM; sed -u "s/^/$NODENAME:$LOCAL_RANK out: /")
 exec 2> >(trap "" INT TERM; sed -u "s/^/$NODENAME:$LOCAL_RANK err: /" >&2)
+
+source ~/venv/OLMo/bin/activate
+
+cd ~/OLMo
+NCCL_LIB_DIR=/var/lib/tcpxo/lib64 source /var/lib/tcpxo/lib64/nccl-env-profile.sh
+export NCCL_NET=FasTrak
+export OMP_NUM_THREADS=16
+export GOOGLE_CLOUD_PROJECT=h100-cluster-owner
 
 if [ $LOCAL_RANK -eq 0 ] ; then
   nvidia-smi --query-compute-apps=pid --format=csv,noheader | xargs -r kill
