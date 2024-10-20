ee6d55f0e54006fedc50d0fab29a457b12531e30
Jiacheng Liu <liujch1998@gmail.com>
2024-10-20 22:33:26 +0000

    Fix glob

--- scripts/eval.py
+++ scripts/eval.py
@@ -247,7 +247,7 @@ def main(cfg: TrainConfig) -> None:
             load_paths = [cfg.load_path]
         else:
             # This globbing does not work with remote paths.
-            load_paths = list(sorted(glob.glob(f"{cfg.save_folder}/step*"), key=lambda x: int(x.split('/')[-1].split('step')[-1])))
+            load_paths = list(sorted(glob.glob(f"{cfg.load_path}/step*"), key=lambda x: int(x.split('/')[-1].split('step')[-1])))
 
         for load_path in load_paths:
             step = int(load_path.split('/')[-1].split('step')[-1])
