36cd64037d2287401691cda265f997349077eb8b
Masha Iureva <mashai@Masha-Iurevas-MacBook-Pro.local>
2024-10-23 09:22:53 -0700

    mashai configs for training

---
+++ configs/intern-project/masha_cc_launch.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -ex
+
+NUM_NODES=2
+gantry run \
+    --workspace ai2/mashai \ #???
+    --task-name mashai-cc \
+    --description "masha's 1% cc experiment" \
+    --priority normal \
+    --preemptible \
+    --beaker-image petew/olmo-torch23-gantry \
+    --cluster ai2/saturn-cirrascale \
+    --gpus 8 \
+    --replicas "${NUM_NODES}" \
+    --leader-selection \
+    --host-networking \
+    --budget ai2/oe-data \
+    --no-nfs \
+    --weka oe-training-default:/weka/oe-training-default \ #???
+    --propagate-failure \
+    --propagate-preemption \
+    --synchronized-start-timeout 90m \
+    --no-python \
+    --env LOG_FILTER_TYPE=local_rank0_only \
+    --env OMP_NUM_THREADS=8 \
+    --env OLMO_TASK=model \
+    --env R2_PROFILE=R2 \
+    --env S3_PROFILE=S3 \
+    --env WEKA_PROFILE=WEKA \
+    --env-secret AWS_CONFIG=MATTJ_AWS_CONFIG \
+    --env-secret AWS_CREDENTIALS=MATTJ_AWS_CREDENTIALS \
+    --env-secret R2_ENDPOINT_URL=R2_ENDPOINT_URL \
+    --env-secret WEKA_ENDPOINT_URL=WEKA_ENDPOINT_URL \
+    --env-secret WANDB_API_KEY=MATTJ_WANDB_API_KEY \
+    --shared-memory 10GiB \
+    --yes \
+    --timeout=-1 \
+    -- /bin/bash -c "configs/intern-project/masha_cc_setup.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
