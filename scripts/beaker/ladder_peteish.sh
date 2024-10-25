efe2baccb04e760f7f16b1398d581a0065c9003a
Jiacheng Liu <liujch1998@gmail.com>
2024-10-25 21:56:38 +0000

    Fix ladder_peteish.sh

--- scripts/beaker/ladder_peteish.sh
+++ scripts/beaker/ladder_peteish.sh
@@ -17,6 +17,17 @@ export FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE
 pip install flash-attn==2.5.9.post1 --no-build-isolation
 pip install '.[train]'
 
+# Force processes to synchronize at init_process_group
+export TORCH_DIST_INIT_BARRIER=1
+
+# Tell OLMo all ranks share the same filesystem for checkpoints.
+export OLMO_SHARED_FS=1
+
+export NCCL_DEBUG=INFO
+export NCCL_IB_HCA="^=mlx5_bond_0"
+export NCCL_SOCKET_IFNAME=ib
+# export NCCL_IB_GID_INDEX=0
+
 torchrun \
   --nnodes ${NUM_NODES}:${NUM_NODES} \
   --nproc-per-node 8 \
