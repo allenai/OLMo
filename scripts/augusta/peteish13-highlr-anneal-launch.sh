5408e7e824997e674ff7b8c0de4e52a0539e7192
Dirk Groeneveld <dirkg@allenai.org>
2024-10-30 23:05:59 +0000

    Try to load from a checkpoint

--- scripts/augusta/peteish13-highlr-anneal-launch.sh
+++ scripts/augusta/peteish13-highlr-anneal-launch.sh
@@ -30,6 +30,7 @@ mkdir -p $SAVE_FOLDER
     --fsdp.sharding_strategy=HYBRID_SHARD \
     --fsdp.hybrid_sharding_num_model_replicas=$NUM_NODES \
     --save_folder=$SAVE_FOLDER \
+    '--load_path=${path.last_checkpoint:${remote_save_folder}}' \
     --device_train_microbatch_size=4 \
     --device_eval_batch_size=8 \
     --activation_checkpointing=whole_layer \
