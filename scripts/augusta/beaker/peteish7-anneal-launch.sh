acba910f33f102c3490cb17371a108339a5f98b9
Dirk Groeneveld <dirkg@allenai.org>
2024-11-01 15:57:14 -0700

    Set retries

--- scripts/augusta/beaker/peteish7-anneal-launch.sh
+++ scripts/augusta/beaker/peteish7-anneal-launch.sh
@@ -36,4 +36,5 @@ gantry run \
   --yes \
   --timeout=-1 \
   --allow-dirty \
+  --retries 10 \
   -- /bin/bash -c "scripts/augusta/beaker/peteish7-anneal.sh \$BEAKER_LEADER_REPLICA_HOSTNAME \$BEAKER_REPLICA_RANK"
