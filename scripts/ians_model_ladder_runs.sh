55d4871d7777f5cc4561f8e508f635f9c6308bbc
Ben Bogin <benb@allenai.org>
2024-11-12 10:03:19 -0500

    manually merging ian's branch

---
+++ scripts/ians_model_ladder_runs.sh
@@ -0,0 +1,25 @@
+# scripts/beaker/ladder-launch.sh 1 normal --model 150M --data dolma-v1-6-and-sources-baseline --length 5xC --name dolma-v1-6-and-sources-baseline --s3 --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 530M --data dolma-v1-6-and-sources-baseline --length 5xC --name dolma-v1-6-and-sources-baseline --s3 --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 300M --data dolma-v1-6-and-sources-baseline --length 5xC --name dolma-v1-6-and-sources-baseline --s3 --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 750M --data dolma-v1-6-and-sources-baseline --length 5xC --name dolma-v1-6-and-sources-baseline --s3 --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 1B --data dolma-v1-6-and-sources-baseline --length 5xC --name dolma-v1-6-and-sources-baseline --s3 --save_overwrite
+scripts/beaker/ladder-launch.sh 2 normal --model 1B --data DCLM-baseline --length 5xC --name DCLM-baseline  --save_overwrite
+scripts/beaker/ladder-launch.sh 1 normal --model 750M --data DCLM-baseline --length 5xC --name DCLM-baseline  --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 300M --data DCLM-baseline --length 5xC --name DCLM-baseline  --save_overwrite
+scripts/beaker/ladder-launch.sh 1 normal --model 530M --data DCLM-baseline --length 5xC --name DCLM-baseline  --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 150M --data DCLM-baseline --length 5xC --name DCLM-baseline  --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 150M --data dolma17-50p-DCLM-baseline-50p --length 5xC --name dolma17-50p-DCLM-baseline-50p  --save_overwrite
+scripts/beaker/ladder-launch.sh 1 normal --model 530M --data dolma17-50p-DCLM-baseline-50p --length 5xC --name dolma17-50p-DCLM-baseline-50p  --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 300M --data dolma17-50p-DCLM-baseline-50p --length 5xC --name dolma17-50p-DCLM-baseline-50p  --save_overwrite
+scripts/beaker/ladder-launch.sh 1 normal --model 750M --data dolma17-50p-DCLM-baseline-50p --length 5xC --name dolma17-50p-DCLM-baseline-50p  --save_overwrite
+scripts/beaker/ladder-launch.sh 2 normal --model 1B --data dolma17-50p-DCLM-baseline-50p --length 5xC --name dolma17-50p-DCLM-baseline-50p  --save_overwrite
+scripts/beaker/ladder-launch.sh 2 normal --model 1B --data dolma17-25p-DCLM-baseline-75p --length 5xC --name dolma17-25p-DCLM-baseline-75p   --save_overwrite
+scripts/beaker/ladder-launch.sh 1 normal --model 750M --data dolma17-25p-DCLM-baseline-75p --length 5xC --name dolma17-25p-DCLM-baseline-75p  --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 300M --data dolma17-25p-DCLM-baseline-75p --length 5xC --name dolma17-25p-DCLM-baseline-75p  --save_overwrite
+scripts/beaker/ladder-launch.sh 1 normal --model 530M --data dolma17-25p-DCLM-baseline-75p --length 5xC --name dolma17-25p-DCLM-baseline-75p  --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 150M --data dolma17-25p-DCLM-baseline-75p --length 5xC --name dolma17-25p-DCLM-baseline-75p  --save_overwrite
+# scripts/beaker/ladder-launch.sh 1 normal --model 150M --data dolma17-75p-DCLM-baseline-25p --length 5xC --name dolma17-75p-DCLM-baseline-25p  --save_overwrite
+scripts/beaker/ladder-launch.sh 1 normal --model 530M --data dolma17-75p-DCLM-baseline-25p --length 5xC --name dolma17-75p-DCLM-baseline-25p  --save_overwrite
+scripts/beaker/ladder-launch.sh 1 normal --model 300M --data dolma17-75p-DCLM-baseline-25p --length 5xC --name dolma17-75p-DCLM-baseline-25p  --save_overwrite
+scripts/beaker/ladder-launch.sh 1 normal --model 750M --data dolma17-75p-DCLM-baseline-25p --length 5xC --name dolma17-75p-DCLM-baseline-25p  --save_overwrite
+scripts/beaker/ladder-launch.sh 2 normal --model 1B --data dolma17-75p-DCLM-baseline-25p --length 5xC --name dolma17-75p-DCLM-baseline-25p   --save_overwrite
