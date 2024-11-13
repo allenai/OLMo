Fix bpb signs

olmo/eval/downstream.py
@@ -147,13 +147,13 @@ class ICLMetric(Metric):
         for doc_id in loglikelihood_dict:
             # each doc_id might have a different number of continuation
             num_continuations = len(loglikelihood_dict[doc_id].keys())
-            bpbs = torch.tensor([-float("inf")] * num_continuations)
+            bpbs = torch.tensor([float("inf")] * num_continuations)
             loglikelihoods = torch.tensor([-float("inf")] * num_continuations)
 
             skip_document = False
             for cont_id in loglikelihood_dict[doc_id]:
                 try:
-                    bpbs[cont_id] = loglikelihood_dict[doc_id][cont_id]
+                    bpbs[cont_id] = -loglikelihood_dict[doc_id][cont_id]
                     loglikelihoods[cont_id] = loglikelihood_dict[doc_id][cont_id] / normalizer_dict[doc_id][cont_id]
                 except IndexError:
                     # We didn't process all of the continuations, so skip this document.