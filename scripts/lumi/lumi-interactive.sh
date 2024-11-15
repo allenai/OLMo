200bd1fc620c3df9392831cd3d2fd584b710683a
Dirk Groeneveld <dirkg@allenai.org>
2024-11-15 11:46:58 -0800

    Delete all the LUMI scripts

--- scripts/lumi/lumi-interactive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run an interactive shell in our singularity image on LUMI.
-
-singularity shell \
-  -B"$PROJECT_DIR:$PROJECT_DIR" \
-  -B"$SCRATCH_DIR:$SCRATCH_DIR" \
-  -B"$FLASH_DIR:$FLASH_DIR" \
-  -B /var/spool/slurmd,/opt/cray/,/usr/lib64/libcxi.so.1,/usr/lib64/libjansson.so.4,/usr/lib64/libjson-c.so.3 \
-  $OLMO_CONTAINER
