53f8eaadc5fc3dfb45276814d5eb539d205c5cca
Ian <magnusson.i@northeastern.edu>
2024-11-15 14:04:20 -0800

    new project space

--- scripts/ladder.py
+++ scripts/ladder.py
@@ -138,7 +138,7 @@ def config_from_args(args: argparse.Namespace) -> TrainConfig:
         permanent_data_prefix = "s3://ai2-llm"
     permanent_data_prefix.rstrip("/")
 
-    remote_save_folder = f"s3://ai2-llm/checkpoints/ianm/optimizing-ladder/{run_name}"
+    remote_save_folder = f"s3://ai2-llm/checkpoints/ianm/optimizing-ladder-experiments/{run_name}"
     load_path = args.load_path
     if load_path is None:
         load_path = find_latest_checkpoint(remote_save_folder)
@@ -188,7 +188,7 @@ def config_from_args(args: argparse.Namespace) -> TrainConfig:
         wandb=None if not args.wandb else WandbConfig(
             name=run_name,
             group=run_name,
-            project="ianm-optimizing-ladder"
+            project="ianm-optimizing-ladder-experiments"
         ),
         model=model_config,
         ddp=DDPConfig(),  # defaults are fine
