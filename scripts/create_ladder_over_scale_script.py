5ee5202f4d7e8234283e8ed43375d52a1819d714
Ian <magnusson.i@northeastern.edu>
2024-11-15 14:49:51 -0800

    account for s3 locations

--- scripts/create_ladder_over_scale_script.py
+++ scripts/create_ladder_over_scale_script.py
@@ -14,20 +14,22 @@ scripts/beaker/ladder-launch.sh 1 normal --model 1B --data dolma17-75p-DCLM-base
 """
 
 import argparse
-import os
-import sys
+import json
 
 def main(args):
     with open(args.scales, 'r') as scales_file:
         scales = scales_file.readlines()
+    data_mixes = []
     with open(args.data_mixes, 'r') as data_mixes_file:
-        data_mixes = data_mixes_file.readlines()
+        for line in data_mixes_file:
+            data_mixes.append(json.loads(line))
     
     for scale in scales:
         scale = scale.strip()
-        for data_mix in data_mixes:
-            data_mix = data_mix.strip()
-            print(f"scripts/beaker/ladder-launch.sh 1 normal --model {scale} --data {data_mix} --length {args.length} --name {data_mix}-{args.name_suffix}")
+        for data_mix_info in data_mixes:
+            data_mix = data_mix_info['name'].strip()
+            s3 = data_mix_info['s3_only']
+            print(f"scripts/beaker/ladder-launch.sh 1 normal --model {scale} --data {data_mix} --length {args.length} --name {data_mix}-{args.name_suffix}" + (" --s3" if s3 else ""))
 
 
 
@@ -35,7 +37,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create a ladder of runs over the scales and data mixes provided')
     parser.add_argument('--scales', type=str, help='Path to the file containing the scales')
-    parser.add_argument('--data-mixes', type=str, help='Path to the file containing the data mixes')
+    parser.add_argument('--data-mixes', type=str, help='Path to the file containing the data mixes (jsonl)')
     parser.add_argument('--name-suffix', type=str, help='suffix (after data-mix) of the experiment')
     parser.add_argument('--length', type=str, help='Length of the experiment')
     args = parser.parse_args()
