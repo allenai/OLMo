b75595fe524bb380fa6de14c2616dcabb56538bf
CodeCreator <wettig.alexander@gmail.com>
2024-11-20 16:13:49 -0500

    Ladder eval annealed 7b model

--- scripts/beaker/peteish/ladder-eval-launch.sh
+++ scripts/beaker/peteish/ladder-eval-launch.sh
@@ -29,41 +29,41 @@ checkpoints=(
     peteish-final-760M-5xC
 )
 
-for checkpoint in ${checkpoints[@]}; do
-    gantry run \
-    --allow-dirty \
-    --name ladder-eval-${checkpoint} \
-    --workspace ai2/OLMo-tiny \
-    --task-name ladder-eval-${checkpoint} \
-    --description "Ladder eval backfill for ${checkpoint}" \
-    --priority high \
-    --preemptible \
-    --beaker-image petew/olmo-torch23-gantry \
-    --cluster ai2/jupiter-cirrascale-2 \
-    --gpus "${NUM_GPUS}" \
-    --replicas "${NUM_NODES}" \
-    --leader-selection \
-    --host-networking \
-    --propagate-failure \
-    --propagate-preemption \
-    --budget ai2/oe-training \
-    --no-nfs \
-    --weka oe-training-default:/weka/oe-training-default \
-    --no-python \
-    --env LOG_FILTER_TYPE=local_rank0_only \
-    --env OMP_NUM_THREADS=${NUM_GPUS} \
-    --env OLMO_TASK=model \
-    --env-secret WANDB_API_KEY=WANDB_API_KEY \
-    --env BACKFILL_SUFFIX=${BACKFILL_SUFFIX} \
-    --env CHECKPOINT=/weka/oe-training-default/ai2-llm/checkpoints/OLMo-ladder/${checkpoint} \
-    --env NUM_CHECKPOINTS=1 \
-    --shared-memory 10GiB \
-    --yes \
-    --timeout=-1 \
-    -- /bin/bash -c "scripts/beaker/peteish/ladder-eval.sh \$BEAKER_LEADER_REPLICA_HOSTNAME \$BEAKER_REPLICA_RANK ${NUM_NODES} ${NUM_GPUS}" &
-done
+# for checkpoint in ${checkpoints[@]}; do
+#     gantry run \
+#     --allow-dirty \
+#     --name ladder-eval-${checkpoint} \
+#     --workspace ai2/OLMo-tiny \
+#     --task-name ladder-eval-${checkpoint} \
+#     --description "Ladder eval backfill for ${checkpoint}" \
+#     --priority high \
+#     --preemptible \
+#     --beaker-image petew/olmo-torch23-gantry \
+#     --cluster ai2/jupiter-cirrascale-2 \
+#     --gpus "${NUM_GPUS}" \
+#     --replicas "${NUM_NODES}" \
+#     --leader-selection \
+#     --host-networking \
+#     --propagate-failure \
+#     --propagate-preemption \
+#     --budget ai2/oe-training \
+#     --no-nfs \
+#     --weka oe-training-default:/weka/oe-training-default \
+#     --no-python \
+#     --env LOG_FILTER_TYPE=local_rank0_only \
+#     --env OMP_NUM_THREADS=${NUM_GPUS} \
+#     --env OLMO_TASK=model \
+#     --env-secret WANDB_API_KEY=WANDB_API_KEY \
+#     --env BACKFILL_SUFFIX=${BACKFILL_SUFFIX} \
+#     --env CHECKPOINT=/weka/oe-training-default/ai2-llm/checkpoints/OLMo-ladder/${checkpoint} \
+#     --env NUM_CHECKPOINTS=1 \
+#     --shared-memory 10GiB \
+#     --yes \
+#     --timeout=-1 \
+#     -- /bin/bash -c "scripts/beaker/peteish/ladder-eval.sh \$BEAKER_LEADER_REPLICA_HOSTNAME \$BEAKER_REPLICA_RANK ${NUM_NODES} ${NUM_GPUS}" &
+# done
 
-checkpoint=peteish7
+checkpoint=peteish7-anneal-from-928646-50B-no-warmup
 gantry run \
     --allow-dirty \
     --name ladder-eval-${checkpoint} \
