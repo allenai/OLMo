b517a73927b7937141044e93aad6d9e416d22adc
Ian <magnusson.i@northeastern.edu>
2024-11-26 16:42:23 -0500

    micro batch 4

--- scripts/ladder.py
+++ scripts/ladder.py
@@ -163,7 +163,7 @@ def config_from_args(args: argparse.Namespace) -> TrainConfig:
     # device batch size based on the hardware we're running on.
     device_batch_size = {
         "150M": 32,
-        "300M": 1,
+        "300M": 4,
         "530M": 8,
         "750M": 8,
         "1B": 2,
