337c3f52cd9d23a66e24eebd65b70e92981816e8
Shane A <shanea@allenai.org>
2024-12-11 12:11:38 -0800

    Add 2 retries

--- scripts/augusta/beaker/peteish1-anneal-launch.sh
+++ scripts/augusta/beaker/peteish1-anneal-launch.sh
@@ -43,4 +43,5 @@ gantry run \
   --yes \
   --timeout=-1 \
   --allow-dirty \
+  --retries 2 \
   -- /bin/bash -c "scripts/augusta/beaker/peteish1-anneal.sh \$BEAKER_LEADER_REPLICA_HOSTNAME \$BEAKER_REPLICA_RANK $ORIGINAL_WANDB_RUN_ID $START_STEP $LENGTH"
