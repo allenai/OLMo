df8cf3930ec7cce04ad85b6324f97dda6dfb8d7e
Kevin Farhat <kevinfarhat@kevin-farhat-macbook-pro.tail8a69.ts.net>
2025-01-10 11:47:39 -0800

    credentials + env setup

---
+++ kevin-scripts/peteish7-anneal-launch.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# CONFIG_NAME="peteish7-anneal"
+# CONFIG_NAME="peteish7-anneal-B34v0x10"
+# CONFIG_NAME="peteish7-anneal-B34v0.1x50"
+
+# bash sewon-scripts/peteish7-anneal-launch.ch peteish7-anneal-from-1T-scitech_sw
+# bash sewon-scripts/peteish7-anneal-launch.ch peteish7-anneal-from-1T-edu
+# bash sewon-scripts/peteish7-anneal-launch.ch peteish7-anneal-from-1T-health
+
+# bash sewon-scripts/peteish7-anneal-launch.ch peteish7-anneal-from-1T-history
+# bash sewon-scripts/peteish7-anneal-launch.ch peteish7-anneal-from-1T-ent_sports
+# bash sewon-scripts/peteish7-anneal-launch.ch peteish7-anneal-from-1T-lit
+
+# bash sewon-scripts/peteish7-anneal-launch.ch peteish7-anneal-from-1T-politics
+# bash sewon-scripts/peteish7-anneal-launch.ch peteish7-anneal-from-1T-fin
+# bash sewon-scripts/peteish7-anneal-launch.ch peteish7-anneal-from-1T-others
+
+CONFIG_NAME=$1
+NUM_NODES=8
+
+gantry run \
+  --workspace ai2/kevin_onboard_test \
+  --task-name ${CONFIG_NAME} \
+  --description ${CONFIG_NAME} \
+  --priority low \
+  --preemptible \
+  --beaker-image petew/olmo-torch23-gantry \
+  --cluster ai2/jupiter-cirrascale-2 \
+  --gpus 8 \
+  --replicas "${NUM_NODES}" \
+  --leader-selection \
+  --host-networking \
+  --budget ai2/oe-training \
+  --no-nfs \
+  --weka oe-training-default:/weka/oe-training-default \
+  --propagate-failure \
+  --propagate-preemption \
+  --synchronized-start-timeout 90m \
+  --no-python \
+  --env LOG_FILTER_TYPE=local_rank0_only \
+  --env OMP_NUM_THREADS=8 \
+  --env OLMO_TASK=model \
+  --env S3_PROFILE=default \
+  --env WEKA_PROFILE=default \
+  --env-secret AWS_CONFIG=KEVINFARHAT_AWS_CONFIG \
+  --env-secret AWS_CREDENTIALS=KEVINFARHAT_AWS_CREDENTIALS \
+  --env-secret WANDB_API_KEY=KEVINFARHAT_WANDB_API_KEY \
+  --shared-memory 10GiB \
+  --yes \
+  --timeout=600 \
+  -- /bin/bash -c "sewon-scripts/peteish7.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK ${CONFIG_PATH}"
