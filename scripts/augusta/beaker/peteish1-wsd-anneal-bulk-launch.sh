a952b5b1c241d9770212bda8a26f58f4b25af39a
Shane A <shanea@allenai.org>
2025-01-10 14:57:44 -0800

    Try make a bulk launch script

---
+++ scripts/augusta/beaker/peteish1-wsd-anneal-bulk-launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+
+NUM_NODES=$1
+shift
+
+LOAD_STEP=$1
+shift
+
+ANNEAL_STEPS=$1
+shift
+
+for LR in 1.56e-2
+do
+  ./peteish1-wsd-anneal-launch.sh $NUM_NODES $LR $LOAD_STEP $ANNEAL_STEPS ${@}
+done
