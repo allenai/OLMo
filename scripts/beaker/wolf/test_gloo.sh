ff6db384a34a467bfadde82988e042f0cf1fce7d
Jiacheng Liu <liujch1998@gmail.com>
2025-01-13 23:10:21 +0000

    test gloo

---
+++ scripts/beaker/wolf/test_gloo.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -ex
+
+NUM_NODES=2
+RUN_NAME="test_gloo"
+
+gantry run \
+  --allow-dirty \
+  --name ${RUN_NAME} \
+  --task-name ${RUN_NAME} \
+  --description ${RUN_NAME} \
+  --workspace ai2/infini-llm \
+  --budget ai2/oe-training \
+  --beaker-image petew/olmo-torch23-gantry \
+  --cluster ai2/jupiter-cirrascale-2 \
+  --priority high \
+  --no-nfs \
+  --weka oe-training-default:/weka/oe-training-default \
+  --gpus 2 \
+  --shared-memory 10GiB \
+  --replicas "${NUM_NODES}" \
+  --host-networking \
+  --leader-selection \
+  --propagate-failure \
+  --propagate-preemption \
+  --synchronized-start-timeout 48h \
+  --no-python \
+  --yes \
+  -- /bin/bash -c "\
+    set -exuo pipefail; \
+    IFS=$'\n\t'; \
+    conda shell.bash activate base; \
+    pip install -U torch==2.5.1; \
+    export TORCH_DIST_INIT_BARRIER=1; \
+    torchrun --nnodes ${NUM_NODES}:${NUM_NODES} --nproc-per-node 2 --rdzv_id=20310 --rdzv_backend=static --rdzv_conf='read_timeout=1200' --rdzv_endpoint=\$BEAKER_LEADER_REPLICA_HOSTNAME:29400 --node_rank=\$BEAKER_REPLICA_RANK \
+        scripts/test_gloo.py \
+  "
---
+++ scripts/test_gloo.py
@@ -0,0 +1,77 @@
+"""Run this script with 'torchrun'."""
+
+import gzip
+import logging
+import os
+import sys
+from datetime import timedelta
+from pathlib import Path
+from typing import Optional, TextIO
+
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import wandb
+from packaging import version
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp import ShardingStrategy
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+from olmo.config import (
+    CheckpointType,
+    DDPGradSyncMode,
+    DistributedStrategy,
+    TrainConfig,
+)
+from olmo.data import build_train_dataloader
+from olmo.eval import build_evaluators
+from olmo.exceptions import OLMoCliError, OLMoConfigurationError
+from olmo.model import OLMo
+from olmo.optim import BoltOnWarmupScheduler, build_optimizer, build_scheduler
+from olmo.torch_util import (
+    barrier,
+    get_default_device,
+    get_global_rank,
+    get_local_rank,
+    get_local_world_size,
+    get_world_size,
+    peak_gpu_memory,
+    seed_all,
+)
+from olmo.train import Trainer
+from olmo.util import (
+    add_cached_path_clients,
+    clean_opt,
+    find_latest_checkpoint,
+    log_extra_field,
+    prepare_cli_environment,
+)
+
+log = logging.getLogger("train")
+
+
+if __name__ == "__main__":
+    try:
+        mp.set_start_method("spawn", force=True)
+    except RuntimeError as e:
+        print(f"failed to set multiprocessing start method: {e}")
+    log.info(f"Multiprocessing start method set to '{mp.get_start_method()}'")
+
+    # Set CUDA device.
+    torch.cuda.set_device(f"cuda:{get_local_rank()}")
+
+    # Initialize process group.
+    device_as_string = f"cuda:{get_local_rank()}"
+    torch.cuda.set_device(
+        device_as_string
+    )  # Set this early to prevent GPU 0 from picking up a bunch of tensors it shouldn't have.
+    dist.init_process_group(
+        backend="cpu:gloo,cuda:nccl", timeout=timedelta(minutes=1440), device_id=torch.device(device_as_string)
+    )
+    log.info("Process group initialized")
+
+    input = torch.arange(4) + rank * 4
+    input = list(input.chunk(4))
+    output = list(torch.empty([4], dtype=torch.int64).chunk(4))
+    dist.all_to_all(output, input)
+    print(f"Rank {get_global_rank()}, output: {output}")
