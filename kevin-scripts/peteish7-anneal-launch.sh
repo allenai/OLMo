048174f4ff39bd725c71808bcb5b2753321945be
Kevin Farhat <kevinfarhat@allenai.org>
2025-01-17 17:32:23 -0800

    cluster

--- kevin-scripts/peteish7-anneal-launch.sh
+++ kevin-scripts/peteish7-anneal-launch.sh
@@ -27,7 +27,7 @@ gantry run \
   --description ${CONFIG_NAME} \
   --priority high \
   --beaker-image petew/olmo-torch23-gantry \
-  --cluster ai2/saturn-cirrascale \
+  --cluster ai2/jupiter-cirrascale-2 \
   --gpus 8 \
   --replicas "${NUM_NODES}" \
   --leader-selection \
