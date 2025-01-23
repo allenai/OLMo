cf07b0da4bad7b28b4e2027fec4075de9b68af7a
Kevin Farhat <kevinfarhat@allenai.org>
2025-01-23 13:43:22 -0800

    adding conversion to my branch

---
+++ kevin-scripts/convert.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+# Function to check if S3 path exists
+check_s3_path() {
+  aws s3 ls "$1" > /dev/null 2>&1
+  return $?
+}
+# might need to export path here
+base_dir="s3://ai2-kevinfarhat/checkpoints"
+
+#for m in peteish7-anneal-B3x50 peteish7-init ; do \
+#for m in peteish7-anneal-dclmx1_booksx50 peteish7-anneal-dclmx1_mathx30 peteish7-anneal-dclmx1_codex20 ; do \
+
+for m in peteish7-lr-0.00041414-anneal-from-477000-50B-dclm_baseline ; do \
+
+	CHECKPOINT="${base_dir}/$m/step11931"
+
+	# Check if the provided path exists
+	if check_s3_path "$CHECKPOINT"; then
+	  echo "Path exists: $CHECKPOINT"
+	else
+	    # Raise error and exit if neither path exists
+	    echo "Error: Path does not exist: $CHECKPOINT"
+	    exit 1
+	fi
+
+	python hf_olmo/convert_olmo_to_hf.py \
+	    --checkpoint-dir $CHECKPOINT \
+	    --destination-dir ${CHECKPOINT}-hf \
+	    --keep-olmo-artifact \
+	    --tokenizer allenai/dolma2-tokenizer
+
+done
+
+#cd ../oe-eval-internal
+#bash sewon-scripts/run.sh
+
+
+
+# #!/usr/bin/env bash
+
+# echo "Starting the script..."
+
+# # add this to avoid 
+# #   File "/Users/kevinfarhat/ai2/OLMo/hf_olmo/convert_olmo_to_hf.py", line 12, in <module>
+# #     from hf_olmo.configuration_olmo import OLMoConfig
+# # ModuleNotFoundError: No module named 'hf_olmo'
+# #Set PYTHONPATH to include the directory containing hf_olmo
+# export PYTHONPATH=/Users/kevinfarhat/ai2/OLMo:$PYTHONPATH
+
+# # Function to check if S3 path exists
+# check_s3_path() {
+#   aws s3 ls "$1" > /dev/null 2>&1
+#   return $?
+# }
+
+# base_dir="s3://ai2-kevinfarhat/checkpoints"
+
+# #for m in peteish7-anneal-B3x50 peteish7-init ; do \
+# #for m in peteish7-anneal-dclmx1_booksx50 peteish7-anneal-dclmx1_mathx30 peteish7-anneal-dclmx1_codex20 ; do \
+# for m in peteish7-medlr-anneal-dclmish-baseline-seed-1209-20241221 ; do \
+
+# # for m in peteish7-medlr-anneal-from-477000-50B-dclm_baseline ; do \
+
+# 	CHECKPOINT="${base_dir}/$m/step23852"
+# 	# CHECKPOINT="${base_dir}/$m/latest"
+
+# 	# Check if the provided path exists
+# 	if check_s3_path "$CHECKPOINT"; then
+# 	    echo "Path exists: $CHECKPOINT"
+# 	else
+# 	    # Raise error and exit if neither path exists
+# 	    echo "Error: Path does not exist: $CHECKPOINT"
+# 	    exit 1
+# 	fi
+
+# 	python hf_olmo/convert_olmo_to_hf.py \
+# 	    --checkpoint-dir $CHECKPOINT \
+# 	    --destination-dir ${CHECKPOINT}-hf \
+# 	    --keep-olmo-artifact \
+# 	    --tokenizer allenai/dolma2-tokenizer
+
+# done
+
+# #cd ../oe-eval-internal
+# #bash sewon-scripts/run.sh
+
+
