c13a025c28b23cbf8d93d46b45bf448df70bf3bc
Kevin Farhat <kevinfarhat@allenai.org>
2025-01-24 14:36:57 -0800

    conversion

--- kevin-scripts/convert.sh
+++ kevin-scripts/convert.sh
@@ -11,9 +11,9 @@ base_dir="s3://ai2-kevinfarhat/checkpoints"
 #for m in peteish7-anneal-B3x50 peteish7-init ; do \
 #for m in peteish7-anneal-dclmx1_booksx50 peteish7-anneal-dclmx1_mathx30 peteish7-anneal-dclmx1_codex20 ; do \
 
-for m in peteish7-lr-0.00041414-anneal-from-477000-50B-dclm_baseline ; do \
+for m in peteish7-lr-0.00041414-anneal-from-477000-100B-dclm_baseline ; do \
 
-	CHECKPOINT="${base_dir}/$m/step11931"
+	CHECKPOINT="${base_dir}/$m/step23852"
 
 	# Check if the provided path exists
 	if check_s3_path "$CHECKPOINT"; then
@@ -38,15 +38,7 @@ done
 
 
 # #!/usr/bin/env bash
-
-# echo "Starting the script..."
-
-# # add this to avoid 
-# #   File "/Users/kevinfarhat/ai2/OLMo/hf_olmo/convert_olmo_to_hf.py", line 12, in <module>
-# #     from hf_olmo.configuration_olmo import OLMoConfig
-# # ModuleNotFoundError: No module named 'hf_olmo'
-# #Set PYTHONPATH to include the directory containing hf_olmo
-# export PYTHONPATH=/Users/kevinfarhat/ai2/OLMo:$PYTHONPATH
+# export PYTHONPATH=/weka/oe-training-default/kevinf/OLMo:$PYTHONPATH
 
 # # Function to check if S3 path exists
 # check_s3_path() {
@@ -58,23 +50,21 @@ done
 
 # #for m in peteish7-anneal-B3x50 peteish7-init ; do \
 # #for m in peteish7-anneal-dclmx1_booksx50 peteish7-anneal-dclmx1_mathx30 peteish7-anneal-dclmx1_codex20 ; do \
-# for m in peteish7-medlr-anneal-dclmish-baseline-seed-1209-20241221 ; do \
 
-# # for m in peteish7-medlr-anneal-from-477000-50B-dclm_baseline ; do \
+# for m in peteish7-lr-0.00041414-anneal-from-477000-50B-dclm_baseline ; do \
 
-# 	CHECKPOINT="${base_dir}/$m/step23852"
-# 	# CHECKPOINT="${base_dir}/$m/latest"
+# 	CHECKPOINT="${base_dir}/$m/step11931"
 
 # 	# Check if the provided path exists
 # 	if check_s3_path "$CHECKPOINT"; then
-# 	    echo "Path exists: $CHECKPOINT"
+# 	  echo "Path exists: $CHECKPOINT"
 # 	else
 # 	    # Raise error and exit if neither path exists
 # 	    echo "Error: Path does not exist: $CHECKPOINT"
 # 	    exit 1
 # 	fi
 
-# 	python hf_olmo/convert_olmo_to_hf.py \
+# 	python -m hf_olmo.convert_olmo_to_hf\
 # 	    --checkpoint-dir $CHECKPOINT \
 # 	    --destination-dir ${CHECKPOINT}-hf \
 # 	    --keep-olmo-artifact \
@@ -84,5 +74,9 @@ done
 
 # #cd ../oe-eval-internal
 # #bash sewon-scripts/run.sh
+
+
+
+
 
 
