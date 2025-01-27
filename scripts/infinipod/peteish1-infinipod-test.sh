60d9721397dd6d3228ff5df1fc74639fb422e1ff
Shane A <shanea@allenai.org>
2025-01-27 15:37:38 -0800

    Turn off shared FS

--- scripts/infinipod/peteish1-infinipod-test.sh
+++ scripts/infinipod/peteish1-infinipod-test.sh
@@ -40,7 +40,7 @@ export NCCL_TUNER_PLUGIN="none"
 export NVIDIA_PYTORCH_VERSION=24.03
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 # Tell OLMo all ranks share the same filesystem for checkpoints.
-export OLMO_SHARED_FS=1
+export OLMO_SHARED_FS=0
 # Redirect stdout and stderr so that we get a prefix with the node name
 export NODENAME=$(hostname -s)
 exec > >(trap "" INT TERM; sed -u "s/^/$NODENAME:$SLURM_LOCALID out: /")
