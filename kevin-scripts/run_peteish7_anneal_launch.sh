30b1d671c7ed9df3bf19a76d0a55bee362a6a146
Kevin Farhat <kevinfarhat@allenai.org>
2025-01-27 10:49:20 -0800

    new config

--- kevin-scripts/run_peteish7_anneal_launch.sh
+++ kevin-scripts/run_peteish7_anneal_launch.sh
@@ -1,1 +1,1 @@
-bash kevin-scripts/peteish7-anneal-launch.sh peteish7-lr-0.00041414-anneal-from-477000-100B-lb-v1p0
+bash kevin-scripts/peteish7-anneal-launch.sh peteish7-lr-0.00041414-anneal-from-477000-100B-lb-v0
