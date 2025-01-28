f53fb42dd7dcbf7e6e168f2c38eda7036314be17
Shane A <shanea@allenai.org>
2025-01-28 10:29:29 -0800

    Fix the fs local id

--- scripts/infinipod/peteish1-invsqrt.sh
+++ scripts/infinipod/peteish1-invsqrt.sh
@@ -30,9 +30,9 @@ export NCCL_SHIMNET_SHIM_LAYERS="unused"
 export NCCL_TUNER_PLUGIN="none"
 export NVIDIA_PYTORCH_VERSION=24.03
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-export FS_LOCAL_RANK=$SLURM_PROCID
 # Tell OLMo all ranks share the same filesystem for checkpoints.
 export OLMO_SHARED_FS=0
+export FS_LOCAL_RANK=$SLURM_LOCALID
 # Redirect stdout and stderr so that we get a prefix with the node name
 export NODENAME=$(hostname -s)
 exec > >(trap "" INT TERM; sed -u "s/^/$NODENAME:$SLURM_LOCALID out: /")
