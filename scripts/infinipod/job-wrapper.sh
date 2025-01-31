7ff4542678ffc5803de232d789328e8ca691e57e
Shane A <shanea@allenai.org>
2025-01-30 16:36:30 -0800

    Revert "Add node id to log filename"
    This reverts commit f8935d076477e21fdef1061cc51981843a449c18.

--- scripts/infinipod/job-wrapper.sh
+++ scripts/infinipod/job-wrapper.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=peteish1-wsd-anneal
+#SBATCH --output=/home/common/shanea/logs/%u_%j_%n.log
 #SBATCH --nodes=16              # Total number of nodes
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=8       # Allocate one gpu per MPI rank
@@ -7,4 +8,4 @@
 #SBATCH --time=96:00:00
 #SBATCH --mem=0			# All memory on the node
 
-srun --mpi=pmi2 --nodes="$SLURM_NNODES" --output="/home/common/shanea/logs/%u_%j_%n.log" ${@}
+srun --mpi=pmi2 --nodes="$SLURM_NNODES" ${@}
