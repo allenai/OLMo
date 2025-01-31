f8935d076477e21fdef1061cc51981843a449c18
Shane A <shanea@allenai.org>
2025-01-30 16:15:30 -0800

    Add node id to log filename

--- scripts/infinipod/job-wrapper.sh
+++ scripts/infinipod/job-wrapper.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=peteish1-wsd-anneal
-#SBATCH --output=/home/common/shanea/logs/%u_%j_%n.log
 #SBATCH --nodes=16              # Total number of nodes
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=8       # Allocate one gpu per MPI rank
@@ -8,4 +7,4 @@
 #SBATCH --time=96:00:00
 #SBATCH --mem=0			# All memory on the node
 
-srun --mpi=pmi2 --nodes="$SLURM_NNODES" ${@}
+srun --mpi=pmi2 --nodes="$SLURM_NNODES" --output="/home/common/shanea/logs/%u_%j_%n.log" ${@}
