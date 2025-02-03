6f64adc3b11d7fc1c065cbd882b3a9361c7293df
David Heineman <dheineman3@gatech.edu>
2025-02-02 20:01:18 -0800

    disable device_id sillyness

--- scripts/train.py
+++ scripts/train.py
@@ -372,7 +372,7 @@ if __name__ == "__main__":
         device_as_string
     )  # Set this early to prevent GPU 0 from picking up a bunch of tensors it shouldn't have.
     dist.init_process_group(
-        backend="nccl", timeout=timedelta(minutes=30), device_id=torch.device(device_as_string)
+        backend="nccl", timeout=timedelta(minutes=30),  # device_id=torch.device(device_as_string)
     )
     log.info("Process group initialized")
 
