a9e256cb3fc39e4ddde5358e343e75bc89cc295e
Ananya Harsh Jha <ananyahjha93@gmail.com>
2025-02-08 22:52:52 -0800

    warmup experiments

--- scripts/beaker/tiny/torchrun-script.sh
+++ scripts/beaker/tiny/torchrun-script.sh
@@ -26,13 +26,13 @@ torchrun \
   --nproc-per-node 4 \
   --rdzv_id=101 \
   --rdzv_backend=c10d \
-  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29402 \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29403 \
   scripts/train.py \
     $CONFIG_PATH \
       --run_name=$TASK_NAME \
       --wandb.name=$TASK_NAME \
       --wandb.group=$TASK_NAME \
       --wandb.project=olmo-optimizers \
-      --optimizer.learning_rate=6e-4 \
-      --scheduler.t_warmup=100 \
+      --optimizer.learning_rate=3e-3 \
+      --scheduler.t_warmup=0 \
       --save_overwrite
