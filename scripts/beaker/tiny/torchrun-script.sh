3d09c4f306a1b60bb7c059e3a69b339a1f57236d
Ananya Harsh Jha <ananyahjha93@gmail.com>
2025-02-08 22:21:22 -0800

    lr runs

--- scripts/beaker/tiny/torchrun-script.sh
+++ scripts/beaker/tiny/torchrun-script.sh
@@ -26,12 +26,12 @@ torchrun \
   --nproc-per-node 4 \
   --rdzv_id=101 \
   --rdzv_backend=c10d \
-  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29401 \
   scripts/train.py \
     $CONFIG_PATH \
       --run_name=$TASK_NAME \
       --wandb.name=$TASK_NAME \
       --wandb.group=$TASK_NAME \
       --wandb.project=olmo-optimizers \
-      --optimizer.learning_rate=6e-4 \
+      --optimizer.learning_rate=1.2e-3 \
       --save_overwrite
