11efc911b76e295320f0f1a472ad6ac45e1527ab
Kevin Farhat <kevinfarhat@allenai.org>
2025-02-11 15:16:34 -0800

    local voncersion working

--- kevin-scripts/convert-local.sh
+++ kevin-scripts/convert-local.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+# Function to check if local path exists
+check_local_path() {
+  [ -d "$1" ] && return 0 || return 1
+}
 
-CHECKPOINT="/weka/oe-training-default/ai2-llm/checkpoints/Spicy-OLMo/peteish7-lr-0.00041414-anneal-from-477000-100B-lb-v0/latest"
+# might need to export path here
+CHECKPOINT="/weka/oe-training-default/kevinf/checkpoints/private-olmo/peteish7-medlr-anneal-from-477000-5B-openMathInstruct2/latest"
+
+# Check if the provided path exists
+if check_local_path "$CHECKPOINT"; then
+    echo "Path exists: $CHECKPOINT"
+else
+    # Raise error and exit if neither path exists
+    echo "Error: Path does not exist: $CHECKPOINT"
+    exit 1
+fi
 
 python hf_olmo/convert_olmo_to_hf.py \
     --checkpoint-dir $CHECKPOINT \
     --destination-dir ${CHECKPOINT}-hf \
     --keep-olmo-artifact \
     --tokenizer allenai/dolma2-tokenizer
-
-
-
-
-
-
