af0269cca26d04affb305b8e8bb22104af0c4664
Ananya Harsh Jha <ananyahjha93@gmail.com>
2025-02-19 18:46:38 +0000

    muon runs

--- scripts/beaker/optimizer/torchrun-script.sh
+++ scripts/beaker/optimizer/torchrun-script.sh
@@ -38,7 +38,7 @@ torchrun \
   --nproc-per-node 4 \
   --rdzv_id=101 \
   --rdzv_backend=c10d \
-  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29400 \
+  --rdzv_endpoint=$BEAKER_LEADER_REPLICA_HOSTNAME:29401 \
   scripts/train.py \
     $CONFIG_PATH \
       --run_name=$TASK_NAME \
