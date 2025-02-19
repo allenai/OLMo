1acb28c5b5181344b8cfcff3d9d3c8177bf7b37a
Ananya Harsh Jha <ananyahjha93@gmail.com>
2025-02-19 23:23:46 +0000

    schedule free runs

--- scripts/beaker/optimizer/gantry-script.sh
+++ scripts/beaker/optimizer/gantry-script.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-SOCKET=29402
+SOCKET=29403
 NUM_NODES=1
-TASK_NAME=olmo-150M-optimizer-schedule-free-adamw-lr-3e-3-wd-0.1
+TASK_NAME=olmo-150M-optimizer-schedule-free-adamw-lr-6e-3-wd-0.1
 CONFIG_PATH=configs/optimizers/OLMo-150M.yaml
 
 OPTIMIZER=schedule_free_adamw
-LR=3e-3
+LR=6e-3
 WD=0.1
 
 gantry run \
