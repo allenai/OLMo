03d6b8516616dc7bc8c6285cacfc50228c70a390
Ananya Harsh Jha <ananyahjha93@gmail.com>
2025-02-19 10:52:15 -0800

    muon runs

--- scripts/beaker/optimizer/gantry-script.sh
+++ scripts/beaker/optimizer/gantry-script.sh
@@ -3,11 +3,11 @@
 set -ex
 
 NUM_NODES=1
-TASK_NAME=olmo-150M-optimizer-muon-lr-5e-3-wd-0.1-momentum-0.9
+TASK_NAME=olmo-150M-optimizer-muon-lr-1e-3-wd-0.1-momentum-0.9
 CONFIG_PATH=configs/optimizers/OLMo-150M.yaml
 
 OPTIMIZER=muon
-MUON_LR=5e-3
+MUON_LR=1e-3
 MUON_WEIGHT_DECAY=0.1
 MUON_MOMENTUM=0.9
 
