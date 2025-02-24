17e892cc03330c34838b3e4b48d4d1d62032b2f4
Ananya Harsh Jha <ananyahjha93@gmail.com>
2025-02-24 00:26:57 +0000

    muon schedule runs

--- scripts/beaker/optimizer/muon-gantry-script.sh
+++ scripts/beaker/optimizer/muon-gantry-script.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-SOCKET=29402
+SOCKET=29403
 NUM_NODES=1
-TASK_NAME=olmo-150M-optimizer-muon-lr-3e-3-wd-0.1-cosine
+TASK_NAME=olmo-150M-optimizer-muon-lr-6e-3-wd-0.1-cosine
 CONFIG_PATH=configs/optimizers/OLMo-150M.yaml
 
 OPTIMIZER=muon
-MUON_LR=3e-3
+MUON_LR=6e-3
 MUON_WEIGHT_DECAY=0.1
 
 gantry run \
