c010ae1cf973618a64a42c092eb5c5fa2203fc93
Shane A <shanea@allenai.org>
2025-02-25 17:29:47 -0800

    Set up cuda stuff

--- olmo/scaling/mup_olmo/mup_coord_check.py
+++ olmo/scaling/mup_olmo/mup_coord_check.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
 
         sys.exit()
 
-    if args.cuda:
+    if torch.cuda.is_available():
         # Set CUDA device.
         torch.cuda.set_device(f"cuda:{get_local_rank()}")
 
