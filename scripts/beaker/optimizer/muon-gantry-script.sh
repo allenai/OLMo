2fa2cc21e9e4eb4a53cff6024bf56e5809a83366
Ananya Harsh Jha <ananyahjha93@gmail.com>
2025-03-15 14:00:22 -0700

    muon sweeps

--- scripts/beaker/optimizer/muon-gantry-script.sh
+++ scripts/beaker/optimizer/muon-gantry-script.sh
@@ -4,11 +4,11 @@ set -ex
 
 SOCKET=29400
 NUM_NODES=1
-TASK_NAME=olmo-1B-optimizer-muon-lr-3e-3-wd-0.1-cosine-adamw-1e-3
+TASK_NAME=olmo-1B-optimizer-muon-lr-6e-3-wd-0.1-cosine-adamw-1e-3
 CONFIG_PATH=configs/optimizers/OLMo-1B.yaml
 
 OPTIMIZER=muon
-MUON_LR=3e-3
+MUON_LR=6e-3
 MUON_WEIGHT_DECAY=0.1
 
 gantry run \
