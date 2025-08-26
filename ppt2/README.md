31ecb8c1ff9a8eb366552b21a7ffd89df3d5bd45
Will Merrill <willm@allenai.org>
2025-08-26 00:44:54 +0000

    Add ppt2 to OLMo

---
+++ ppt2/README.md
@@ -0,0 +1,31 @@
+# PPT2 Experiments
+
+To create a Beaker session with WEKA mounted:
+
+```shell
+# Create beaker session with WEKA mounted
+beaker session create --budget ai2/allennlp --bare --mount weka://oe-training-default=/weka/oe-training-default
+```
+
+To launch training runs:
+
+```shell
+export GPUS=4
+
+# To launch the control.
+ppt2/scripts/launch.sh ppt2/configs/peteish1-control.yaml
+
+# To launch phase 0 on shuffle Dyck.
+# TODO: Read shuffle-dyck from Google Cloud
+scripts/launch.sh ppt2/configs/peteish1-phase0.yaml
+
+# Launch phase1 on checkpoint 500, which is step-matched.
+# TODO: Read phase0 checkpoint 250/500 from WEKA
+scripts/launch.sh ppt2/configs/peteish1-phase1.yaml
+```
+
+Beaker error messages are now more informative around why things don't launch. Budget issues, etc.
+
+* `ai2/willm-ppt2` has max priority Normal
+* `ai2/oe-base` instead of `ai2/oe-training`
+* Clusters: `ai2/neptune-cirrascale`, `ai2/jupiter-cirrascale-2`, etc.
---
+++ ppt2/scripts/launch.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# set -ex
+
+CONFIG_PATH=${1:-"configs/peteish1-control.yaml"}
+GPUS=${GPUS:-8}
+
+NODES=1
+# For multinode training, add:
+#   --replicas "${NODES}" \
+#   --synchronized-start-timeout 90m \
+
+gantry run \
+  --allow-dirty \
+  --workspace ai2/willm-ppt2 \
+  --budget ai2/oe-base \
+  --task-name ppt2-control \
+  --description "PPT2 control" \
+  --priority normal \
+  --cluster ai2/neptune-cirrascale \
+  --preemptible \
+  --beaker-image petew/olmo-torch23-gantry \
+  --gpus $GPUS \
+  --leader-selection \
+  --host-networking \
+  --weka oe-training-default:/weka/oe-training-default \
+  --propagate-failure \
+  --propagate-preemption \
+  --no-python \
+  --env LOG_FILTER_TYPE=local_rank0_only \
+  --env OMP_NUM_THREADS=8 \
+  --env OLMO_TASK=model \
+  --env R2_PROFILE=R2 \
+  --env S3_PROFILE=S3 \
+  --env WEKA_PROFILE=WEKA \
+  --env-secret AWS_CONFIG=WILLM_AWS_CONFIG \
+  --env-secret AWS_CREDENTIALS=WILLM_AWS_CREDENTIALS \
+  --env-secret WANDB_API_KEY=WILLM_WANDB_API_KEY \
+  --env-secret R2_ENDPOINT_URL=R2_ENDPOINT_URL \
+  --env-secret WEKA_ENDPOINT_URL=WEKA_ENDPOINT_URL \
+  --shared-memory 10GiB \
+  --yes \
+  --timeout=-1 \
+  -- /bin/bash -c "ppt2/scripts/peteish1.sh ${NODES} ${CONFIG_PATH} \$BEAKER_LEADER_REPLICA_HOSTNAME \$BEAKER_REPLICA_RANK"
